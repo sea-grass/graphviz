955fbc0805f22ac892bf370fb6ca882d04ff035f
Bob Apthorpe <bob.apthorpe@gmail.com>
2020-06-25 22:01:20 -0500

    Organized man pages and installed PDFs

--- cmake/package_info.cmake
+++ cmake/package_info.cmake
@@ -20,3 +20,90 @@ if(NSIS_FOUND)
     set(CPACK_NSIS_MODIFY_PATH              ON                                              )
     LIST(APPEND CPACK_GENERATOR NSIS)
 endif()
+
+# Only install man pages on non-Windows systems
+if(NOT WIN32)
+    # Specify man pages to be installed
+    install(
+        FILES ${CMAKE_CURRENT_SOURCE_DIR}/graphviz.7
+        DESTINATION ${MAN7_INSTALL_DIR}
+    )
+endif()
+
+# Install PDFs somewhere sensible
+install(FILES
+    ${CMAKE_CURRENT_SOURCE_DIR}/cmd/dot/dot.1.pdf
+    ${CMAKE_CURRENT_SOURCE_DIR}/cmd/dot/osage.1.pdf
+    ${CMAKE_CURRENT_SOURCE_DIR}/cmd/dot/patchwork.1.pdf
+    ${CMAKE_CURRENT_SOURCE_DIR}/doc/dotguide.pdf
+    ${CMAKE_CURRENT_SOURCE_DIR}/doc/dottyguide.pdf
+    ${CMAKE_CURRENT_SOURCE_DIR}/doc/leftyguide.pdf
+    ${CMAKE_CURRENT_SOURCE_DIR}/doc/libgraph/Agraph.pdf
+    ${CMAKE_CURRENT_SOURCE_DIR}/doc/libguide/libguide.pdf
+    ${CMAKE_CURRENT_SOURCE_DIR}/doc/neatoguide.pdf
+    ${CMAKE_CURRENT_SOURCE_DIR}/doc/odt/graphviz_plugins.pdf
+    ${CMAKE_CURRENT_SOURCE_DIR}/doc/oldlibguide.pdf
+    ${CMAKE_CURRENT_SOURCE_DIR}/doc/smyrna/smyrna.pdf
+    DESTINATION doc/pdf
+)
+
+# Only install PDF versions of application and library man pages on Windows systems
+# Note: These are all translated from nroff to PDF via pandoc and xelatex
+# (part of LaTeX) - see doc/mantopdf/README.md
+if(WIN32)
+    # Application manual pages
+    install(FILES
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/graphviz.7.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/acyclic.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/bcomps.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/ccomps.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/cluster.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/diffimg.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/dijkstra.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/dot.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/dotty.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/edgepaint.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/gc.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/gml2gv.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/graphml2gv.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/gvcolor.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/gvedit.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/gvgen.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/gvmap.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/gvmap.sh.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/gvpack.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/gvpr.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/gxl2gv.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/inkpot.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/lefty.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/lneato.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/mingle.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/mm2gv.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/nop.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/osage.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/patchwork.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/prune.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/sccmap.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/smyrna.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/tred.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/unflatten.1.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/vimdot.1.pdf
+        DESTINATION doc/manual_pdf
+    )
+
+    # Library manual pages
+    install(FILES
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/agraph.3.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/cdt.3.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/cgraph.3.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/expr.3.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/gvc.3.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/gvpr.3.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/inkpot.3.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/lab_gamut.3.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/pack.3.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/pathplan.3.pdf
+        ${CMAKE_CURRENT_SOURCE_DIR}/doc/mantopdf/xdot.3.pdf
+        DESTINATION doc/library_pdf
+    )
+endif()
