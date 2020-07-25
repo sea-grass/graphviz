04d338f2cf064cbb4053f51af5941c872aa97077
Bob Apthorpe <bob.apthorpe@gmail.com>
2020-07-25 17:53:11 -0500

    Added logging and command-line arguments

--- rtest2/rtest2.py
+++ rtest2/rtest2.py
@@ -1,10 +1,50 @@
 #!/usr/bin/env python
+"""
+Test generator and runner for detecting differences in text and images produced by dot
+
+Directions for use:
+- Create reference directory and populate it with reference data.
+- Create "test roster", a JSON-formatted file consisting of a
+  dict of test definitions. Default name is "TESTS.json"
+  - key is test label
+  - value is a dict
+    - required key "graph", value is a string, the backslash-escaped 
+      gv commands to run through dot
+    - required key "format", value is a list of strings which can
+      be accepted by dot's -T option. At least one format must be
+      listed; list cannot be empty
+    - any other keys are ignored
+  - Optionally, create input and output directories which are
+    writable by this script.
+- Run the script, specifying the path to the dot executable, the test
+  definition file, and the reference directory, and any optional
+  parameters (verbosity, input and output directories, default layout)
+
+Interpreting results:
+- Output is in TAP format; see https://testanything.org/
+  - The range of subtests for a given test is displayed as "1..<number of subtests>"
+  - Each subtest will display either "ok" or "not ok" followed by the
+    subtest ID and a descriptive message on stdout.
+- The test script exits with an integer status code:
+  - If all tests succeed, the script exits with status code 0.
+  - If any subtest fails, the script exits with status code 1
+  - If no tests can run (usually due to misconfiguration),
+    the script exits with status code 2
+- For failing text comparisons, a unified diff of the results and
+  reference files is produced in the results directory. Text
+  difference files are named <test>.<subtest>.<format>.diff
+- For failing image comparisons, an image difference file is created
+  from the results and reference image files in the results
+  directory. Image difference files are named
+  <test>.<subtest>.diff.<format>
+"""
 from __future__ import absolute_import, division, print_function
+import argparse
 from collections import defaultdict, OrderedDict
 from difflib import unified_diff
 from filecmp import cmp
 import json
-# import logging
+import logging
 from os import mkdir, remove
 from os.path import abspath, exists, join
 from PIL import Image
@@ -12,43 +52,16 @@ from pixelmatch.contrib.PIL import pixelmatch
 import subprocess
 import sys
 
-# Directions for use:
-# - Create reference directory and populate it with reference data.
-#   See basedn, rdn, and reference_dir in script body
-# - Create "test roster", a JSON-formatted file consisting of a
-#   dict of test definitions. Default name is "TESTS.json"
-#   - key is test label
-#   - value is a dict
-#     - required key "graph", value is a string, the \-escaped 
-#       gv commands to run through dot
-#     - required key "format", value is a list of strings which can
-#       be accepted by dot's -T option. At least one format must be
-#       listed; list cannot be empty
-#     - any other keys are ignored
-#   - Optionally, create input and output directories which are
-#     writable by this script. See basedn, idn, graph_dir, odn,
-#     and results_dir in script body
-# - Run the script, specifying the full path to the dot executable,
-#   the input, output, and reference directories, and any optional
-#   parameters (verbosity, default layout)
-#
-# Interpreting results:
-# - Output is in TAP format; see https://testanything.org/
-#   - The range of subtests for a given test is displayed as "1..<number of subtests>"
-#   - Each subtest will display either "ok" or "not ok" followed by the
-#     subtest ID and a descriptive message on stdout.
-# - The test script exits with an integer status code:
-#   - If all tests succeed, the script exits with status code 0.
-#   - If any subtest fails, the script exits with status code 1
-#   - If no tests can run (usually due to missing reference data),
-#     the script exits with status code 2
-# - For failing text comparisons, a unified diff of the results and
-#   reference files is produced in the results directory. Text
-#   difference files are named <test>.<subtest>.<format>.diff
-# - For failing image comparisons, an image difference file is created
-#   from the results and reference image files in the results
-#   directory. Image difference files are named
-#   <test>.<subtest>.diff.<format>
+# Set default logging handler to avoid "No handler found" warnings.
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+_logger = logging.getLogger(__name__)
+_logger.addHandler(NullHandler())
 
 # Global results comparison selector
 # Ensure any format that appears in TESTS.json is categorized as
@@ -60,6 +73,90 @@ for rt in [ "png" ]:
 for rt in [ "cmapx", "gv", "ps", "svg", "xdot" ]:
     fmttest[rt] = "text"
 
+
+def parse_args(args):
+    """Parse command line parameters
+
+    Args:
+      args ([str]): command line parameters as list of strings
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    parser = argparse.ArgumentParser(
+        description="rtest2 test runner")
+    parser.add_argument(
+        '-t',
+        '--test-definitions',
+        dest="test_json",
+        help="set JSON test definition file",
+        default=join(abspath("."), "TESTS.json"),
+        metavar="TESTS_JSON",
+        action='store')
+    parser.add_argument(
+        '-x',
+        '--dot-executable',
+        dest="dot_exe",
+        help="set dot executable",
+        default=join(abspath("."), "dot"),
+        metavar="DOT_EXECUTABLE",
+        action='store')
+    parser.add_argument(
+        '-i',
+        '--input-dir',
+        dest="graph_dir",
+        help="set directory to contain generated input files",
+        default=join(abspath("."), "test_graphs"),
+        action='store')
+    parser.add_argument(
+        '-o',
+        '--output-dir',
+        dest="results_dir",
+        help="set directory to contain generated results",
+        default=join(abspath("."), "test_results"),
+        action='store')
+    parser.add_argument(
+        '-r',
+        '--reference-dir',
+        dest="reference_dir",
+        help="set directory containing reference results",
+        default=join(abspath("."), "test_reference"),
+        action='store')
+    parser.add_argument(
+        '-L',
+        '--layout',
+        dest="layout",
+        help="set layout engine",
+        default="dot",
+        action='store')
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        dest="loglevel",
+        help="set loglevel to INFO",
+        action='store_const',
+        const=logging.INFO)
+    parser.add_argument(
+        '-vv',
+        '--very-verbose',
+        dest="loglevel",
+        help="set loglevel to DEBUG",
+        action='store_const',
+        const=logging.DEBUG)
+    return parser.parse_args(args)
+
+
+def setup_logging(loglevel):
+    """Setup basic logging
+
+    Args:
+      loglevel (int): minimum loglevel for emitting messages
+    """
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(level=loglevel, stream=sys.stdout,
+                        format=logformat, datefmt="%Y-%m-%d %H:%M:%S")
+
+
 def main(args):
     """Main entry point allowing external calls
 
@@ -69,48 +166,79 @@ def main(args):
     Returns:
         int: Status code - 0 = success, non-zero = failure
     """
-    # Note: dot_exe, tfn, and layout should be specifiable from the command line
-    dot_exe = "dot"
-#    dot_exe = "/usr/bin/dot"
-#    dot_exe = r"C:\Program Files (x86)\Graphviz2.38\bin\dot.exe"
-
-    tfn = "TESTS.json"
-
-    layout = "dot"
-
-    # Also, either (basedn, idn, odn, rdn) or (graph_dir, results_dir, reference_dir)
-    # should be specificiable so CMake/CTest can run this as a test
-    basedn = "."
-    idn = "test_graphs"
-    odn = "test_results"
-    rdn = "test_reference"
-
-    graph_dir = abspath(join(basedn, idn))
-    results_dir = abspath(join(basedn, odn))
-    reference_dir = abspath(join(basedn, rdn))
-
-    # Compatibility flag: set to False for original test.tcl behavior
-    use_common_input = True
-
-    is_verbose = False
 
     # Overall test suite status: zero indicates success and non-zero indicates failure
     # Initiallize to 0, set to 1 if any test or subtest fails, set to 2 if no tests can be run
     iok = 0
 
-    # Fail if reference data directory cannot be found
-    print("1..1")
-    if exists(reference_dir):
-        print("ok 1 - Test 0: Reference directory check # Found {0:s}".format(reference_dir))
+    args = parse_args(args)
+
+    setup_logging(args.loglevel)
+    _logger.info("Starting rtest2")
+
+    # Note: dot_exe, tfn, and layout should be specifiable from the command line
+    print("1..6")
+    pretest_id = 0
+
+    pretest_id += 1
+    if exists(args.dot_exe):
+        dot_exe = abspath(args.dot_exe)
+        print("ok {0:d} - Dot executable # Found at {1:s}".format(pretest_id, dot_exe))
     else:
-        print("not ok 1 - Test 0: Reference directory check # Cannot find {0:s}".format(reference_dir))
+        print("not ok {0:d} - Dot executable # Cannot find {1:s}".format(pretest_id, args.dot_exe))
         iok = 2
+
+    pretest_id += 1
+    if exists(args.test_json):
+        tfn = abspath(args.test_json)
+        print("ok {0:d} - Test definitions # Cannot find {1:s}".format(pretest_id, tfn))
+    else:
+        print("not ok {0:d} - Test definitions # Cannot find {1:s}".format(pretest_id, args.test_json))
+        iok = 2
+
+    pretest_id += 1
+    graph_dir = abspath(args.graph_dir)
+    if not exists(graph_dir):
+        mkdir(graph_dir)
+    if exists(graph_dir):
+        print("ok {0:d} - Input directory # Found {1:s}".format(pretest_id, graph_dir))
+    else:
+        print("not ok {0:d} - Input directory # Cannot create {1:s}".format(pretest_id, args.graph_dir))
+        iok = 2
+
+    pretest_id += 1
+    results_dir = abspath(args.results_dir)
+    if not exists(results_dir):
+        mkdir(results_dir)
+    if exists(results_dir):
+        print("ok {0:d} - Results directory # Found {1:s}".format(pretest_id, results_dir))
+    else:
+        print("not ok {0:d} - Results directory # Cannot create {1:s}".format(pretest_id, args.results_dir))
+        iok = 2
+
+    pretest_id += 1
+    if exists(args.reference_dir):
+        reference_dir = abspath(args.reference_dir)
+        print("ok {0:d} - Reference directory # Found {1:s}".format(pretest_id, reference_dir))
+    else:
+        print("not ok {0:d} - Reference directory # Cannot find {1:s}".format(pretest_id, args.reference_dir))
+        iok = 2
+
+    pretest_id += 1
+    layout = args.layout.strip().lower()
+    if len(layout) > 0:
+        print("ok {0:d} - Layout engine # Setting to {1:s}".format(pretest_id, layout))
+    else:
+        print("not ok {0:d} - Input directory # Cannot find {1:s}".format(pretest_id, args.layout))
+        iok = 2
+
+    # Quit if pretests fail
+    if iok != 0:
+        _logger.info("Prematurely ending rtest2")
         return iok
 
-    # Build input and output directories if they don't already exist
-    for tdn in [graph_dir, results_dir]:
-        if not exists(tdn):
-            mkdir(tdn)
+    # Compatibility flag: set to False for original test.tcl behavior
+    use_common_input = True
 
     # Import test roster from JSON input
     with open(tfn, 'r') as ifh:
@@ -123,14 +251,14 @@ def main(args):
         nsubtests = len(test_item["format"])
         # TAP output: Print range of subtests
         print("1..{0:d}".format(nsubtests))
-        if is_verbose:
-            print("Test {0:03d}:".format(test_id))
-            print("  Label: {0:s}".format(test_label))
-            print("  Code:  {0:s}".format(test_item["code"]))
-            if len(test_item["format"]) == 1:
-                print("  Subtest:")
-            else:
-                print("  Subtests:")
+
+        _logger.info("Test {0:03d}:".format(test_id))
+        _logger.info("  Label: {0:s}".format(test_label))
+        _logger.info("  Code:  {0:s}".format(test_item["code"]))
+        if len(test_item["format"]) == 1:
+            _logger.info("  Subtest:")
+        else:
+            _logger.info("  Subtests:")
 
         # Common input file to all subtests of test test_id
         # Write headcmt followed by test_item["code"] to graph_fn
@@ -197,32 +325,29 @@ def main(args):
             cmd_args.append(results_fn)
 
             # Display command
-            if is_verbose:
-                cmd = " ".join(cmd_args)
-                print("    {0:s}: {1:s}".format(tag, cmd))
+            _logger.info("    {0:s}: {1:s}".format(tag, " ".join(cmd_args)))
 
             # Run the test
             pstatus = subprocess.run(cmd_args)
 
-            if is_verbose:
-                if (pstatus.returncode != 0):
-                    print("Subtest {0:s} errored out with status code {1:d}:"
-                        .format(tag, pstatus.returncode))
-                else:
-                    print("Subtest {0:s} completed successfully"
-                        .format(tag))
+            if (pstatus.returncode != 0):
+                _logger.info("Subtest {0:s} errored out with status code {1:d}:"
+                    .format(tag, pstatus.returncode))
+            else:
+                _logger.info("Subtest {0:s} completed successfully"
+                    .format(tag))
 
-                if (pstatus.stdout):
-                    print("stdout: {0:s}".format(pstatus.stdout))
+            if (pstatus.stdout):
+                _logger.info("stdout: {0:s}".format(pstatus.stdout))
 
-                if (pstatus.stderr):
-                    print("stderr: {0:s}".format(pstatus.stderr))
+            if (pstatus.stderr):
+                _logger.info("stderr: {0:s}".format(pstatus.stderr))
 
             # Analyze results
             if exists(results_fn) and exists(reference_fn):
-                if is_verbose:
-                    print("Found reference file {0:s} corresponding to results file {1:s}"
-                        .format(reference_fn, results_fn))
+                _logger.info("Found reference file {0:s} corresponding to results file {1:s}"
+                    .format(reference_fn, results_fn))
+
                 if cmp(reference_fn, results_fn):
                     subtest_comment = " # Results are trivially similar"
 
@@ -281,9 +406,7 @@ def main(args):
             print("{0:s} {1:d} - {2:s}{3:s}"
                 .format(subtest_status, subtest_id, subtest_label, subtest_comment))
 
-        if is_verbose:
-            print()
-
+    _logger.info("Ending rtest2")
     return iok
 
 
