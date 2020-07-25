68bb9a5b756d50d3f44232929fe0221212256d58
Bob Apthorpe <bob.apthorpe@gmail.com>
2020-07-24 21:42:55 -0500

    Added text and image differences

--- rtest2/rtest2.py
+++ rtest2/rtest2.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 from __future__ import absolute_import, division, print_function
-from collections import OrderedDict
+from collections import defaultdict, OrderedDict
+from difflib import unified_diff
+from filecmp import cmp
 import json
 # import logging
 from os import mkdir, remove
 from os.path import abspath, exists, join
+from PIL import Image
+from pixelmatch.contrib.PIL import pixelmatch
 import subprocess
 import sys
+
+# Global results comparison selector
+fmttest = defaultdict(lambda : "unknown")
+for rt in [ "png" ]:
+    fmttest[rt] = "image"
+for rt in [ "cmapx", "gv", "ps", "svg", "xdot" ]:
+    fmttest[rt] = "text"
 
 def main(args):
     """Main entry point allowing external calls
@@ -118,8 +129,9 @@ def main(args):
             cmd_args.append(results_fn)
 
             # Display command
-            cmd = " ".join(cmd_args)
-            print("    {0:03d}.{1:03d}: {2:s}".format(ict, jct, cmd))
+            if is_verbose:
+                cmd = " ".join(cmd_args)
+                print("    {0:s}: {1:s}".format(basefn, cmd))
 
             pstatus = subprocess.run(cmd_args)
             if (pstatus.returncode != 0):
@@ -129,19 +141,55 @@ def main(args):
                 print("Subtest {0:s} completed successfully"
                       .format(basefn))
 
-            if (pstatus.stdout):
-                print("stdout: {0:s}".format(pstatus.stdout))
+            if is_verbose:
+                if (pstatus.stdout):
+                    print("stdout: {0:s}".format(pstatus.stdout))
 
-            if (pstatus.stderr):
-                print("stderr: {0:s}".format(pstatus.stderr))
+                if (pstatus.stderr):
+                    print("stderr: {0:s}".format(pstatus.stderr))
 
             if exists(results_fn) and exists(reference_fn):
-                print("Found reference file {0:s} corresponding to results file {1:s}"
-                      .format(reference_fn, results_fn))
+                if is_verbose:
+                    print("Found reference file {0:s} corresponding to results file {1:s}"
+                        .format(reference_fn, results_fn))
                 # TODO Perform all results comparisons and tests here
+                if cmp(reference_fn, results_fn):
+                    print("OK - Results from subtest {0:s} are trivially similar".format(basefn))
+                else:
+                    if fmttest[tfmt] == "text":
+                        print("FAIL - Text results from subtest {0:s} differ from reference:".format(basefn))
+                        if is_verbose:
+                            with open(reference_fn) as ff:
+                                reference_data = ff.readlines()
+                            with open(results_fn) as tf:
+                                results_data = tf.readlines()
+                            textdiff = unified_diff(reference_data, results_data, reference_fn, results_fn)
+                            sys.stdout.writelines(textdiff)
+                    elif fmttest[tfmt] == "image":
+                        # Compare images
+                        ref_img = Image.open(reference_fn)
+                        results_img = Image.open(results_fn)
+                        # TODO: Add simple histogram checks
+                        if ref_img.size != results_img.size:
+                            print("FAIL - Subtest {0:s} image size ({1:d}, {2:d}) differs from reference size ({3:d}, {4:d})"
+                                  .format(basefn, results_img.size[0], results_img.size[1], ref_img.size[0], ref_img.size[1], ))
+                        else:
+                            img_diff = Image.new("RGBA", ref_img.size)
+                            idiff_fn = join(results_dir, "{0:s}.diff.{1:s}".format(basefn, tfmt))
+                            mismatch = pixelmatch(ref_img, results_img, img_diff, includeAA=True)
+                            img_diff.save(idiff_fn)
+                            if mismatch > 0:
+                                print("FAIL - Image results from subtest {0:s} differ from reference by {1:d} pixels"
+                                    .format(basefn, mismatch))
+                            else:
+                                print("FAIL - Image results from subtest {0:s} differ from reference but pixels seem to match (alpha channel?)"
+                                    .format(basefn))
+                    else:
+                        print("FAIL - Unknown format results from subtest {0:s} differ from reference:".format(basefn))
+
             else:            
                 # TODO Report subtest skip or failure
-                pass
+                print("FAIL - Subtest {0:s} did not produce any results".format(basefn))
 
         print()
 
