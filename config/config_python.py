34d14bf93fca92660285323197cc893f837d6b72
swaldhoer <34184299+swaldhoer@users.noreply.github.com>
2021-02-27 19:21:26 +0100

    apply main convention and use argparse instead of sys.argv

--- config/config_python.py
+++ config/config_python.py
@@ -1,12 +1,27 @@
-import sys
+#!/usr/bin/env python3
+
+import argparse
 from distutils import sysconfig
 
-if sys.argv[1] == "archlib":
-    print(sysconfig.get_python_lib(1,1))
-elif sys.argv[1] == "lib":
-    print(sysconfig.get_python_lib(0,1))
-elif sys.argv[1] == "archsitelib":
-    print(sysconfig.get_python_lib(1,0))
-elif sys.argv[1] == "sitelib":
-    print(sysconfig.get_python_lib(0,0))
 
+def main():
+    parser = argparse.ArgumentParser(description="Prints specified library directory")
+    parser.add_argument(
+        "libdir",
+        choices=["archlib", "archsitelib", "lib", "sitelib"],
+        action="store",
+        help="Library directory path to get",
+    )
+    args = parser.parse_args()
+    if args.libdir == "archlib":
+        print(sysconfig.get_python_lib(1, 1))
+    elif args.libdir == "lib":
+        print(sysconfig.get_python_lib(0, 1))
+    elif args.libdir == "archsitelib":
+        print(sysconfig.get_python_lib(1, 0))
+    elif args.libdir == "sitelib":
+        print(sysconfig.get_python_lib(0, 0))
+
+
+if __name__ == "__main__":
+    main()
