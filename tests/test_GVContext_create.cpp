675639fcd14ead40762dfc8922e0a5927afee8be
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2021-04-04 00:10:58 +0200

    add test of GVContext create with built-in plugins
    Destroy is also implicitly tested since we would detect potential
    memory leaks with ASan.

---
+++ tests/test_GVContext_create.cpp
@@ -0,0 +1,11 @@
+#include <catch2/catch.hpp>
+
+#include <gvc/GVContext.h>
+
+TEST_CASE("GVContext can be constructed with built-in plugins and it's "
+          "underlying C data structure can be retrieved") {
+  const auto demand_loading = false;
+  auto gvc = GVC::GVContext::create_with_builtins(lt_preloaded_symbols,
+                                                  demand_loading);
+  REQUIRE(gvc->c_struct() != nullptr);
+}
