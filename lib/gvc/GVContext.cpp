eaf30ee7ccb2d8d78b42e4f36ec207985b6d124f
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2021-04-04 00:10:58 +0200

    add a GVContext class with creation with built-in plugins

---
+++ lib/gvc/GVContext.cpp
@@ -0,0 +1,18 @@
+#include <stdexcept>
+
+#include <gvc/GVContext.h>
+
+namespace GVC {
+
+GVContext::GVContext(GVC_t *gvc) : m_gvc(gvc) {}
+
+std::unique_ptr<GVContext>
+GVContext::create_with_builtins(const lt_symlist_t *builtins,
+                                bool demand_loading) {
+  return std::make_unique<GVContext>(
+      gvContextPlugins(builtins, demand_loading));
+}
+
+GVContext::~GVContext() { gvFreeContext(m_gvc); }
+
+} // namespace GVC
---
+++ lib/gvc/GVContext.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include <cgraph/AGraph.h>
+#include <gvc/gvc.h>
+
+#ifdef _WIN32
+#if gvc_EXPORTS
+#define GVCONTEXT_API __declspec(dllexport)
+#else
+#define GVCONTEXT_API __declspec(dllimport)
+#endif
+#else
+#define GVCONTEXT_API /* nothing */
+#endif
+
+/**
+ * @brief The GVContext class represents a Graphviz context
+ */
+
+namespace GVC {
+
+class GVCONTEXT_API GVContext {
+public:
+  // named constructors
+  static std::unique_ptr<GVContext>
+  create_with_builtins(const lt_symlist_t *builtins, bool demand_loading);
+
+  // do not use this constructor directly. Use a named constructor instead
+  explicit GVContext(GVC_t *gvc);
+  ~GVContext();
+  // disallow copy for now since we manage a C struct using a raw pointer
+  GVContext(GVContext &) = delete;
+  GVContext &operator=(GVContext &) = delete;
+  GVC_t *c_struct() const { return m_gvc; }
+
+private:
+  // the underlying C data structure
+  GVC_t *const m_gvc = nullptr;
+};
+
+} // namespace GVC
