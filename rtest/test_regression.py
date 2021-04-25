155aa5630d99e1e18674000e371997bb4eca33ea
Saleem Abdulrasool <compnerd@compnerd.org>
2021-04-25 03:21:55 +0000

    GVC: export `gvToolTred`
    This is a temporary bandaid solution for a missing export from the GVC
    library.  The general solution of fixing the annotation on the
    declarations is better suited for Windows.  That would allow the
    compiler to make appropriate optimizations.  Modern MinGW should also be
    able to handle the exports through the attribute, avoiding the need to
    maintain the extra def file.
    
    The attributes could also be further generalized to aid on Unix
    platforms.
    
    This partially addresses #2507.

--- rtest/test_regression.py
+++ rtest/test_regression.py
@@ -769,12 +769,16 @@ def test_1931():
   assert "line 3\nline 4" in xdot
   assert "line 5\nline 6" in xdot
 
-@pytest.mark.xfail()
 def test_2057():
   """
   gvToolTred should be usable by user code
   https://gitlab.com/graphviz/graphviz/-/issues/2057
   """
+
+  # FIXME: Remove skip when
+  # https://gitlab.com/graphviz/graphviz/-/issues/1777 is fixed
+  if os.getenv("build_system") == "msbuild":
+    pytest.skip("Windows MSBuild release does not contain any header files (#1777)")
 
   # find co-located test source
   c_src = (Path(__file__).parent / "2057.c").resolve()
