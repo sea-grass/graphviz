1f47241185da71a033997c1cff6b3bd24bb26c41
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-01-29 17:27:49 -0700

    cdt dtclose: remove now unnecessary 'ev' local

--- lib/cdt/dtclose.c
+++ lib/cdt/dtclose.c
@@ -8,7 +8,6 @@
 int dtclose(Dt_t* dt)
 {
 	Dtdisc_t	*disc;
-	int		ev = 0;
 
 	if(!dt || dt->nview > 0 ) /* can't close if being viewed */
 		return -1;
@@ -18,19 +17,18 @@ int dtclose(Dt_t* dt)
 	if(dt->view)	/* turn off viewing */
 		dtview(dt,NULL);
 
-	if(ev == 0) /* release all allocated data */
-	{	(void)dt->meth->searchf(dt, NULL, DT_CLEAR);
-		if(dtsize(dt) > 0)
-			return -1;
+	/* release all allocated data */
+	(void)dt->meth->searchf(dt, NULL, DT_CLEAR);
+	if(dtsize(dt) > 0)
+		return -1;
 
-		if(dt->data->ntab > 0)
-			dt->memoryf(dt, dt->data->htab, 0, disc);
-		dt->memoryf(dt, dt->data, 0, disc);
-	}
+	if(dt->data->ntab > 0)
+		dt->memoryf(dt, dt->data->htab, 0, disc);
+	dt->memoryf(dt, dt->data, 0, disc);
 
 	if(dt->type == DT_MALLOC)
 		free(dt);
-	else if(ev == 0 && dt->type == DT_MEMORYF)
+	else if (dt->type == DT_MEMORYF)
 		dt->memoryf(dt, dt, 0, disc);
 
 	return 0;
