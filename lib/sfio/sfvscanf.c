sea-grass/graphviz

commit 0af0ffb98f569969a2a20939f52744c02c21b5d2

SFIO sfvscanf: stop reusing 'accept' array
This array was being used for both accepting floats of varying precision as well
as accepting characters of a user-defined class. With a modern compiler, there
is no need to reuse an array like this and separating it leads to easier to read
code.

lib/sfio/sfvscanf.c
@@ -599,9 +599,10 @@ int sfvscanf(FILE *f, const char *form, va_list args)
 			*argv.s++ = inp;
 		} while (--width > 0 && SFGETC(f, inp) >= 0);
 	    } else {		/* if(fmt == '[') */
-		form = setclass((char *) form, accept);
+		char accepted[UCHAR_MAX];
+		form = setclass((char *) form, accepted);
 		do {
-		    if (!accept[inp]) {
+		    if (!accepted[inp]) {
 			if (n > 0 || (flags & SFFMT_ALTER))
 			    break;
 			else {

commit f10c1ee4b9ac2be8aed07423ca8258672e8b8e78

sfio sfvscanf: fix buffer overflow on 'accepted' array
MSVC correctly notices:

  C:\GitLab-Runner\builds\graphviz\graphviz\lib\sfio\sfvscanf.c(40): warning
    C4789: buffer 'accepted' of size 255 bytes will be overrun; 256 bytes will
    be written starting at offset 0
    [C:\GitLab-Runner\builds\graphviz\graphviz\lib\sfio\sfio.vcxproj]

This bug was introduced in 0af0ffb98f569969a2a20939f52744c02c21b5d2. No
changelog entry because there has been no release since that commit.

lib/sfio/sfvscanf.c
@@ -602,7 +602,7 @@ int sfvscanf(FILE *f, const char *form, va_list args)
 			*argv.s++ = inp;
 		} while (--width > 0 && SFGETC(f, inp) >= 0);
 	    } else {		/* if(fmt == '[') */
-		bool accepted[UCHAR_MAX];
+		bool accepted[UCHAR_MAX + 1];
 		form = setclass(form, accepted);
 		do {
 		    if (!accepted[inp]) {