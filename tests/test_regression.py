613bdf96ab6b0ce4fb2775371953701abc060f7a
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-09-12 01:52:42 -0700

    port 'dot -V?' test cases to Pytest
    This drops the `BUILDDATE` aspect of this test because locating builddate.h is
    non-trivial (or even impossible in the case of testing an installation without
    the build directory available). But this seems of limited value anyway.
    
    Gitlab: #2443

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -3233,3 +3233,25 @@ def test_dot_randomV():
     assert proc.returncode != 0, "malformed options were accepted"
 
     assert proc.stderr == expected, "unexpected usage info"
+
+
+@pytest.mark.skipif(
+    os.getenv("build_system") == "msbuild",
+    reason="https://gitlab.com/graphviz/graphviz/-/issues/1777",
+)
+def test_dot_Vquestionmark():
+    """
+    test the output from two short options combined
+    """
+
+    proc = subprocess.run(
+        ["dot", "-V?"], stderr=subprocess.PIPE, universal_newlines=True, check=True
+    )
+
+    c_src = (Path(__file__).parent / "get-package-version.c").resolve()
+    assert c_src.exists(), "missing test case"
+    package_version, _ = run_c(c_src)
+
+    assert proc.stderr.startswith(
+        f"dot - graphviz version {package_version.strip()} ("
+    ), "unexpected -V info"
--- tests/unit_tests/lib/common/command_line.c
+++ tests/unit_tests/lib/common/command_line.c
@@ -85,38 +85,3 @@ Test(command_line, dash_Vrandom_output,
 
 	cr_assert_stderr_eq_str(expected_stderr);
 }
-
-/**
- * Exit and output tests for `dot -V?`
- */
-Test(command_line, dash_V_questionmark_exit,
-		.init = redirect_all_std,
-		.exit_code = 0)
-{
-	GVC_t *Gvc = gvContextPlugins(lt_preloaded_symbols, DEMAND_LOADING);
-	GvExitOnUsage = 1;
-	int argc = 2;
-	char* argv[] = {"dot", "-V?"};
-
-	gvParseArgs(Gvc, argc, argv);
-
-	// Fail this test if the function above does not call exit.
-	cr_assert(false);
-}
-
-Test(command_line, dash_V_questionmark_output,
-		.init = redirect_all_std)
-{
-	GVC_t *Gvc = gvContextPlugins(lt_preloaded_symbols, DEMAND_LOADING);
-	GvExitOnUsage = 0;
-	int argc = 2;
-	char* argv[] = {"dot", "-V?"};
-
-	gvParseArgs(Gvc, argc, argv);
-
-	char expected_stderr[100];
-	snprintf(expected_stderr, sizeof(expected_stderr),
-	         "dot - graphviz version %s (%s)\n", PACKAGE_VERSION, BUILDDATE);
-
-	cr_assert_stderr_eq_str(expected_stderr);
-}
