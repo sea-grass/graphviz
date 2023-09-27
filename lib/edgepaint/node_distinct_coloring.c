ellson/MOTHBALLED-graphviz

commit aadee957012e676c9aaf36ec40cb2860c178e269

main edgepaint (also know as clarify) routines for coloring nodes of a graph to maximize color distances along edges

lib/edgepaint/node_distinct_coloring.c
@@ -1,0 +1,306 @@
+#include "general.h"
+#include "SparseMatrix.h"
+#include "QuadTree.h"
+#include "node_distinct_coloring.h"
+#include "lab.h"
+#include "furtherest_point.h"
+#include "color_palette.h"
+
+inline static real mydist(int dim, real *x, real *y){
+  int k;
+  real d = 0;
+  for (k = 0; k < dim; k++) d += (x[k] - y[k])*(x[k]-y[k]);
+  return sqrt(d);
+}
+
+
+static void node_distinct_coloring_internal2(int scheme, QuadTree qt, int weightedQ, SparseMatrix A, int cdim, real accuracy, int iter_max, int seed, real *colors, 
+					     real *color_diff0, real *color_diff_sum0){
+  /* here we assume the graph is connected. And that the matrix is symmetric */
+  int i, j, *ia, *ja, n, k = 0;
+  int max_level;
+  real center[3];
+  real width;
+  real *a = NULL;
+  real *x;
+  real dist_max;
+  real color_diff = 0, color_diff_old;
+  real color_diff_sum = 0, color_diff_sum_old, *cc;
+  int iter = 0;
+  real cspace_size = 0.7;
+  real red[3], black[3], min;
+  int flag = 0, imin;
+  color_lab lab;
+  color_rgb rgb;
+  real *wgt = NULL;
+  //int iter2 = 0, iter_max2;
+
+  assert(accuracy > 0);
+  max_level = MAX(1, -log(accuracy)/log(2.));
+  max_level = MIN(30, max_level);
+
+  rgb.r = 255*0.5; rgb.g = 0; rgb.b = 0;
+  lab = RGB2LAB(rgb);
+  red[0] = lab.l; red[1] = lab.a; red[2] = lab.b;
+
+  n = A->m;
+  if (n == 1){
+    if (scheme == COLOR_LAB){
+      assert(qt);
+      QuadTree_get_nearest(qt, black, colors, &imin, &min, &flag);
+      assert(!flag);
+      LAB2RGB_real_01(colors);
+      *color_diff0 = 1000; *color_diff_sum0 = 1000;
+    } else {
+      for (i = 0; i < cdim; i++) colors[i] = 0;
+      *color_diff0 = sqrt(cdim); *color_diff_sum0 = sqrt(cdim); 
+    }
+    return;
+  } else if (n == 2){
+    if (scheme == COLOR_LAB){
+      assert(qt);
+      QuadTree_get_nearest(qt, black, colors, &imin, &min, &flag);
+      assert(!flag);
+      LAB2RGB_real_01(colors);
+
+      QuadTree_get_nearest(qt, red, colors+cdim, &imin, &min, &flag);
+      assert(!flag);
+      LAB2RGB_real_01(colors+cdim);
+      *color_diff0 = 1000; *color_diff_sum0 = 1000;
+
+    } else {
+      for (i = 0; i < cdim; i++) colors[i] = 0;
+      for (i = 0; i < cdim; i++) colors[cdim+i] = 0;
+      colors[cdim] = 0.5;
+      *color_diff0 = sqrt(cdim); *color_diff_sum0 = sqrt(cdim); 
+    }
+     return;
+  }
+  assert(n == A->m);
+  ia = A->ia;
+  ja = A->ja;
+  if (A->type == MATRIX_TYPE_REAL && A->a){
+    a = (real*) A->a;
+  } 
+
+  /* cube [0, cspace_size]^3: only uised if not LAB */
+  center[0] = center[1] = center[2] = cspace_size*0.5;
+  width = cspace_size*0.5;
+
+  /* randomly assign colors first */
+  srand(seed);
+  for (i = 0; i < n*cdim; i++) colors[i] = cspace_size*drand();
+
+  /* better way... not using it for now till paper is out */
+  /*
+  if (scheme != COLOR_LAB){
+    for (i = 0; i < n*cdim; i++) colors[i] = cspace_size*drand();
+  } else {
+    for (i = 0; i < n; i++) {
+      colors[3*i] = 100*drand();
+      colors[3*i+1] = 256*drand() - 128;
+      colors[3*i+2] = 256*drand() - 128;
+    }
+  }
+  */
+
+  x = MALLOC(sizeof(real)*cdim*n);
+  if (weightedQ) wgt = MALLOC(sizeof(real)*n);
+
+  color_diff = 0; color_diff_old = -1;
+  color_diff_sum = 0; color_diff_sum_old = -1;
+
+  /*
+  {FILE *fp;
+    char buf[10000];
+    fp = fopen("/tmp/count","r");
+    fgets(buf, 10000, fp);
+    sscanf(buf,"%d",&iter_max2);
+    fprintf(stderr,"count=%d\n", iter_max2);
+  }
+  */
+
+  while (iter++ < iter_max && (color_diff > color_diff_old || (color_diff == color_diff_old && color_diff_sum > color_diff_sum_old))){
+    color_diff_old = color_diff;
+    color_diff_sum_old = color_diff_sum;
+    for (i = 0; i < n; i++){
+      //      if (iter2++ >= iter_max2) goto CONT;
+      k = 0;
+      for (j = ia[i]; j < ia[i+1]; j++){
+	if (ja[j] == i) continue;
+	MEMCPY(&(x[k*cdim]), &(colors[ja[j]*cdim]), sizeof(real)*cdim);
+	if (wgt && a) wgt[k] = a[j];
+	k++;
+      }
+      cc = &(colors[i*cdim]);
+      if (scheme == COLOR_LAB){
+	furtherest_point_in_list(k, cdim, wgt, x, qt, max_level, mydist, &dist_max, &cc);
+      } else if (scheme == COLOR_RGB || scheme == COLOR_GRAY){
+	furtherest_point(k, cdim, wgt, x, center, width, max_level, mydist, &dist_max, &cc);
+      } else {
+	assert(0);
+      }
+      if (i == 0){
+	color_diff = dist_max;
+	color_diff_sum = dist_max;
+      } else {
+	color_diff = MIN(dist_max, color_diff);
+	color_diff_sum += dist_max;
+      }
+    }
+
+    if (Verbose) fprintf(stderr,"iter ---- %d ---, color_diff = %f, color_diff_sum = %f\n", iter, color_diff, color_diff_sum);
+  }
+
+  // CONT:
+
+  if (scheme == COLOR_LAB){
+    /* convert from LAB to RGB */
+    color_rgb rgb;
+    color_lab lab;
+    for (i = 0; i < n; i++){
+      lab = color_lab_init(colors[i*cdim], colors[i*cdim+1], colors[i*cdim+2]);
+      rgb = LAB2RGB(lab);
+      colors[i*cdim] = (rgb.r)/255;
+      colors[i*cdim+1] = (rgb.g)/255;
+      colors[i*cdim+2] = (rgb.b)/255;
+    }
+  }
+  *color_diff0 = color_diff;
+  *color_diff_sum0 = color_diff_sum;
+  FREE(x);
+  
+}
+ 
+static void node_distinct_coloring_internal(int scheme, QuadTree qt, int weightedQ,  SparseMatrix A, int cdim, real accuracy, int iter_max, int seed, real *colors, real *color_diff0, real *color_diff_sum0){
+  int i;
+  if (seed < 0) {
+    /* do multiple iterations and pick the best */
+    int iter, seed_max = -1;
+    real color_diff_max = -1;
+    srand(123);
+    iter = -seed;
+    for (i = 0; i < iter; i++){
+      seed = irand(100000);
+      node_distinct_coloring_internal2(scheme, qt, weightedQ, A, cdim, accuracy, iter_max, seed, colors, color_diff0, color_diff_sum0);
+      if (color_diff_max < *color_diff0){
+	seed_max = seed; color_diff_max = *color_diff0;
+      }
+    }
+    seed = seed_max;
+  } 
+  node_distinct_coloring_internal2(scheme, qt, weightedQ, A, cdim, accuracy, iter_max, seed, colors, color_diff0, color_diff_sum0);
+ 
+}
+
+void node_distinct_coloring(char *color_scheme, char *lightness, int weightedQ, SparseMatrix A0, real accuracy, int iter_max, int seed, int *cdim0, real **colors, real *color_diff0,
+			    real *color_diff_sum0, int *flag){
+  /* 
+     for a graph A, get a distinctive color of its nodes so that the color distance among all neighboring nodes are maximized. Here
+     color distance on a node is defined as the minimum of color differences between a node and its neighbors (or the minimum of weighted color differences if weightedQ = true,
+     where weights are stored as entries of A0.
+     accuracy is the threshold given so that when finding the coloring for each node, the optimal is
+     with in "accuracy" of the true global optimal. 
+     color_scheme: rgb, gray, lab, or one of the color palettes in color_palettes.h, or a list of hex rgb colors separaterd by comma like "#ff0000,#00ff00"
+     lightness: of the form 0,70, specifying the range of lightness of LAB color. Ignored if scheme is not COLOR_LAB.
+     .          if NULL, 0,70 is assumed
+     A: the graph of n nodes
+     accuracy: how accurate to find the optimal
+     seed: random_seed. If negative, consider -seed as the number of random start iterations
+     iter_max: max number of 
+     cdim: dimension of the color space
+     color: On input an array of size n*cdim, if NULL, will be allocated. On exit the final color assignment for node i is [cdim*i,cdim*(i+1)), in RGB (between 0 to 1)
+     color_diff: the minuimum color difference across all edges
+     color_diff_sum: the sum of min color dfference across all nodes
+  */ 
+  SparseMatrix B, A = A0;
+  int ncomps, *comps = NULL, *comps_ptr = NULL;
+  int nn, n;
+  real *ctmp;
+  int i, j, jj, nnodes = 0;
+  real color_diff = 0, color_diff_sum = 0;
+  QuadTree qt = NULL;
+  int cdim;
+  int scheme = COLOR_LAB;
+  int maxcolors = 10000, max_qtree_level = 10, r, g, b;
+  char *color_list = NULL;
+
+  if (iter_max < 0) iter_max = 100;
+
+  color_list = color_palettes_get(color_scheme);
+  if (color_list) color_scheme = color_list;
+
+  cdim = *cdim0 = 3;
+  if (strcmp(color_scheme, "lab") == 0){
+    fprintf(stderr,"lab\n");
+    scheme =  COLOR_LAB;
+    qt = lab_gamut_quadtree("lab_gamut", lightness, max_qtree_level);
+    if (!qt){
+      fprintf(stderr," can not open file \"lab_gamut\"\n");
+      *flag = ERROR_BAD_LAB_GAMUT_FILE;
+      return;
+    }
+  } else if (strcmp(color_scheme, "rgb") == 0){
+    fprintf(stderr,"rgb\n");
+    scheme = COLOR_RGB;
+  } else if (strcmp(color_scheme, "gray") == 0){
+    scheme = COLOR_GRAY;
+    cdim = *cdim0 = 1;
+  } else if (sscanf(color_scheme,"#%02X%02X%02X", &r, &g, &b) == 3 ){
+    double *colors = NULL;
+    scheme = COLOR_LAB;
+    color_blend_rgb2lab(color_scheme, maxcolors, &colors);
+    assert(colors);
+    qt = QuadTree_new_from_point_list(cdim, maxcolors, max_qtree_level, colors, NULL);
+    assert(qt);
+  } else {
+    *flag = ERROR_BAD_COLOR_SCHEME;
+    return;
+  }
+
+
+  *color_diff0 = *color_diff_sum0 = -1;
+  if (accuracy <= 0) accuracy = 0.0001;
+
+  *flag = 0;
+  n = A->m;
+  if (n != A->n) {
+    *flag = -1;
+    return;
+  }
+ 
+  if (!(*colors)) {
+    *colors = MALLOC(sizeof(real)*cdim*n);
+  }
+  ctmp = MALLOC(sizeof(real)*cdim*n);
+
+  B = SparseMatrix_symmetrize(A, FALSE);
+  A = B;
+
+  SparseMatrix_weakly_connected_components(A, &ncomps, &comps, &comps_ptr); 
+  
+  *color_diff_sum0 = 0;
+  for (i = 0; i < ncomps; i++){
+    nn = comps_ptr[i+1] - comps_ptr[i];
+    B = SparseMatrix_get_submatrix(A, nn, nn, &(comps[comps_ptr[i]]), &(comps[comps_ptr[i]]));
+    node_distinct_coloring_internal(scheme, qt, weightedQ, B, cdim, accuracy, iter_max, seed, ctmp, &color_diff, &color_diff_sum);
+    if (i == 0){
+      *color_diff0 = color_diff;
+    } 
+    *color_diff0 = MIN(*color_diff0, color_diff); 
+    if (B->m > 2) {
+      *color_diff_sum0 = *color_diff_sum0 + color_diff_sum;
+      nnodes += B->m;
+    }
+
+    for (j = comps_ptr[i]; j < comps_ptr[i+1]; j++){
+      jj = j - comps_ptr[i];
+      MEMCPY(&((*colors)[comps[j]*cdim]), &(ctmp[jj*cdim]), cdim*sizeof(real));
+    }
+    SparseMatrix_delete(B);
+  }
+  FREE(ctmp);
+  *color_diff_sum0 /= nnodes;
+
+  if (A != A0) SparseMatrix_delete(A);
+}

lib/edgepaint/Makefile.am
@@ -1,0 +1,21 @@
+# $Id$Revision: 
+## Process this file with automake to produce Makefile.in
+
+AM_CPPFLAGS = \
+	-I$(top_srcdir) \
+	-I$(top_srcdir)/lib/common \
+	-I$(top_srcdir)/lib/sparse \
+	-I$(top_srcdir)/lib/gvc \
+	-I$(top_srcdir)/lib/pathplan \
+	-I$(top_srcdir)/lib/sfdpgen \
+	-I$(top_srcdir)/lib/cgraph \
+	-I$(top_srcdir)/lib/cdt $(ANN_CFLAGS)
+
+noinst_HEADERS =  edge_distinct_coloring.h  furtherest_point.h  intersection.h  lab_gamut.h  lab.h  node_distinct_coloring.h
+
+noinst_LTLIBRARIES = libedgepaint_C.la
+
+libedgepaint_C_la_SOURCES = edge_distinct_coloring.c  furtherest_point.c  intersection.c  lab.c  node_distinct_coloring.c
+
+EXTRA_DIST = edgepaintlib.vcproj
+ 

lib/edgepaint/edge_distinct_coloring.c
@@ -1,0 +1,307 @@
+#include "general.h"
+#include "time.h"
+#include "SparseMatrix.h"
+#include "node_distinct_coloring.h"
+#include "DotIO.h"
+#include "intersection.h"
+#include "QuadTree.h"
+
+static int splines_intersect(int dim, int u1, int v1, int u2, int v2, 
+			     real cos_critical, int check_edges_with_same_endpoint, 
+			     real *x, char *xsplines1, char *xsplines2){
+  /* u1, v2 an u2, v2: the node index of the two edn points of two edges.
+     cos_critical: cos of critical angle
+     check_edges_with_same_endpoint: whether need to treat two splines from
+     .     the same end point specially in ignoring splines that exit/enter the same end pont at around 180
+     x: the coordinates of nodes
+     xsplines1,xsplines2: the first and second splines corresponding to two edges
+
+  */
+  int itmp;
+  int len1 = 100, len2 = 100;
+  real *x1, *x2;
+  int ns1 = 0, ns2 = 0;
+  int i, j, iter1 = 0, iter2 = 0;
+  real cos_a, tmp[2];
+  int endp1 = 0, endp2 = 0;
+
+  tmp[0] = tmp[1] = 0;
+  x1 = MALLOC(sizeof(real)*len1);
+  x2 = MALLOC(sizeof(real)*len2);
+
+  assert(dim <= 3);
+  /* if two end points are the same, make sure they are the first in each edge */
+  if (u1 == v2){/* switch u2 and v2 */
+    itmp = u2; u2 = v2; v2 = itmp;
+  } else if (v1 == u2){/* switch u1 and v1 */
+    itmp = u1; u1 = v1; v1 = itmp;
+  } else if (v1 == v2){/* switch both */
+    itmp = u2; u2 = v2; v2 = itmp;
+    itmp = u1; u1 = v1; v1 = itmp;
+  }
+
+  /* origonally I though the two end points has to be included. But now I think it does not need to since
+     the splie might start from the border of the label box
+  MEMCPY(x1, &(x[dim*u1]), sizeof(real)*dim);
+  MEMCPY(x2, &(x[dim*u2]), sizeof(real)*dim);
+  ns1++; ns2++;
+  */
+
+  /* splines could be a list of 
+     1. 3n points
+     2. of the form "e,x,y" followed by 3n points, where x,y is really padded to the end of the 3n points
+     3. of the form "s,x,y" followed by 3n points, where x,y is padded to the start of the 3n points
+  */
+  if (xsplines1){
+    if(strstr(xsplines1, "e,")){
+      endp1 = 1;
+      xsplines1 = strstr(xsplines1, "e,") + 2;
+    } else if (strstr(xsplines2, "s,")){
+      xsplines1 = strstr(xsplines1, "s,") + 2;
+    }
+  }
+  while (xsplines1 && sscanf(xsplines1,"%lf,%lf", &(x1[ns1*dim]), &x1[ns1*dim + 1]) == 2){
+    if (endp1 && iter1 == 0){
+      tmp[0] = x1[ns1*dim]; tmp[1] = x1[ns1*dim + 1];
+    } else {
+      ns1++;
+    }
+    iter1++;
+    xsplines1 = strstr(xsplines1, " ");
+    if (!xsplines1) break;
+    xsplines1++;
+    if (ns1*dim >= len1){
+      len1 = ns1*dim + MAX(10, 0.2*ns1*dim);
+      x1 = REALLOC(x1, sizeof(real)*len1);
+    }
+  }
+  if (endp1){/* pad the end point at the last position */
+    ns1++;
+    if (ns1*dim >= len1){
+      len1 = ns1*dim + MAX(10, 0.2*ns1*dim);
+      x1 = REALLOC(x1, sizeof(real)*len1);
+    }
+    x1[(ns1-1)*dim] = tmp[0];  x1[(ns1-1)*dim + 1] = tmp[1]; 
+  }
+
+
+  /* splines could be a list of 
+     1. 3n points
+     2. of the form "e,x,y" followed by 3n points, where x,y is really padded to the end of the 3n points
+     3. of the form "s,x,y" followed by 3n points, where x,y is padded to the start of the 3n points
+  */
+  if (xsplines2){
+    if(strstr(xsplines2, "e,")){
+      endp2 = 1;
+      xsplines2 = strstr(xsplines2, "e,") + 2;
+    } else if (strstr(xsplines2, "s,")){
+      xsplines2 = strstr(xsplines2, "s,") + 2;
+    }
+  }
+  while (xsplines2 && sscanf(xsplines2,"%lf,%lf", &(x2[ns2*dim]), &x2[ns2*dim + 1]) == 2){
+    if (endp2 && iter2 == 0){
+      tmp[0] = x2[ns2*dim]; tmp[1] = x2[ns2*dim + 1];
+    } else {
+      ns2++;
+    }
+    iter2++;
+    xsplines2 = strstr(xsplines2, " ");
+    if (!xsplines2) break;
+    xsplines2++;
+    if (ns2*dim >= len2){
+      len2 = ns2*dim + MAX(10, 0.2*ns2*dim);
+      x2 = REALLOC(x2, sizeof(real)*len2);
+    }
+  }
+  if (endp2){/* pad the end point at the last position */
+    ns2++;
+    if (ns2*dim >= len2){
+      len2 = ns2*dim + MAX(10, 0.2*ns2*dim);
+      x2 = REALLOC(x2, sizeof(real)*len2);
+    }
+    x2[(ns2-1)*dim] = tmp[0];  x2[(ns2-1)*dim + 1] = tmp[1]; 
+  }
+
+  /* origonally I though the two end points has to be included. But now I think it does not need to since
+     the splie might start from the border of the label box
+  MEMCPY(&(x2[dim*ns2]), &(x[dim*v2]), sizeof(real)*dim);
+  ns2++;
+  MEMCPY(&(x1[dim*ns1]), &(x[dim*v1]), sizeof(real)*dim);
+  ns1++;
+  */
+
+for (i = 0; i < ns1 - 1; i++){
+    for (j = 0; j < ns2 - 1; j++){
+      cos_a = intersection_angle(&(x1[dim*i]), &(x1[dim*(i + 1)]), &(x2[dim*j]), &(x2[dim*(j+1)]));
+      if (!check_edges_with_same_endpoint && cos_a >= -1) cos_a = ABS(cos_a);
+      if (cos_a > cos_critical) {
+	return 1;
+      }
+
+    }
+  }
+
+  FREE(x1);
+  FREE(x2);
+  return 0;
+}
+
+
+Agraph_t* edge_distinct_coloring(char *color_scheme, char *lightness, Agraph_t* g, real angle, real accuracy, real check_edges_with_same_endpoint, int seed){
+  /* color the edges of a graph so that conflicting edges are as dinstrinct in color as possibl.
+     color_scheme: rgb, lab, gray, or a list of comma separaterd RGB colors in hex, like #ff0000,#00ff00
+     lightness: of the form 0,70, specifying the range of lightness of LAB color. Ignored if scheme is not COLOR_LAB.
+     .          if NULL, 0,70 is assumed
+     g: the graph
+     angle: if two edges cross at an angle < "angle", consider they as conflict
+     accuracy: how accurate when finding  color of an edge to be as different from others
+     check_edges_with_same_endpoint: if TRUE, we will check edges with same end point and only consider them as conflict if
+     .                   their angle is very small. Edges that share an end point and is close to 180 degree
+     .                   are not consider conflict.
+     seed: random_seed. If negative, consider -seed as the number of random start iterations
+  */
+  real *x = NULL;
+  int dim = 2;
+  SparseMatrix A, B, C;
+  int *irn, *jcn, nz, nz2 = 0;
+  real cos_critical = cos(angle/180*3.14159), cos_a;
+  int u1, v1, u2, v2, i, j;
+  real *colors = NULL, color_diff, color_diff_sum;
+  int flag, ne;
+  char **xsplines = NULL;
+  int cdim;
+
+  A = SparseMatrix_import_dot(g, dim, NULL, &x, NULL, NULL, FORMAT_COORD, NULL);
+  if (!x){
+    fprintf(stderr,"The gv file contains no or not proper 2D coordinates\n");
+    return NULL;
+  }
+
+
+  irn = A->ia; jcn = A->ja;
+  nz = A->nz;
+
+  /* get rid of self edges */
+  for (i = 0; i < nz; i++){
+    if (irn[i] != jcn[i]){
+      irn[nz2] = irn[i];
+      jcn[nz2++] = jcn[i];
+    }
+  }
+
+  fprintf(stderr,"cos = %f, nz2 = %d\n", cos_critical, nz2);
+  /* now find edge collision */
+  nz = 0;
+  B = SparseMatrix_new(nz2, nz2, 1, MATRIX_TYPE_REAL, FORMAT_COORD);
+
+  if (Import_dot_splines(g, &ne, &xsplines)){
+#ifdef TIME
+    clock_t start = clock();
+#endif
+    assert(ne == nz2);
+    cos_a = 1.;/* for splines we exit conflict check as soon as we find an conflict, so the anle may not be representitive, hence set to constant */
+    for (i = 0; i < nz2; i++){
+      u1 = irn[i]; v1 = jcn[i];
+      for (j = i+1; j < nz2; j++){
+	u2 = irn[j]; v2 = jcn[j];
+	if (splines_intersect(dim, u1, v1, u2, v2, cos_critical, check_edges_with_same_endpoint, x, xsplines[i], xsplines[j])){
+	  B = SparseMatrix_coordinate_form_add_entries(B, 1, &i, &j, &cos_a);
+	}
+      }
+    }
+#ifdef TIME
+    fprintf(stderr, "cpu for dual graph =%10.3f", ((real) (clock() - start))/CLOCKS_PER_SEC);
+#endif
+    
+  } else {
+    /* no splines, justsimple edges */
+#ifdef TIME
+    clock_t start = clock();
+#endif
+    
+    
+    for (i = 0; i < nz2; i++){
+      u1 = irn[i]; v1 = jcn[i];
+      for (j = i+1; j < nz2; j++){
+	u2 = irn[j]; v2 = jcn[j];
+	cos_a = intersection_angle(&(x[dim*u1]), &(x[dim*v1]), &(x[dim*u2]), &(x[dim*v2]));
+	if (!check_edges_with_same_endpoint && cos_a >= -1) cos_a = ABS(cos_a);
+	if (cos_a > cos_critical) {
+	  B = SparseMatrix_coordinate_form_add_entries(B, 1, &i, &j, &cos_a);
+	}
+      }
+    }
+#ifdef TIME
+    fprintf(stderr, "cpu for dual graph (splines) =%10.3f\n", ((real) (clock() - start))/CLOCKS_PER_SEC);
+#endif
+  } 
+  C = SparseMatrix_from_coordinate_format(B);
+  if (B != C) SparseMatrix_delete(B);
+  
+  {
+#ifdef TIME
+    clock_t start = clock();
+#endif
+    int weightedQ = FALSE;
+    int iter_max = 100;
+    node_distinct_coloring(color_scheme, lightness, weightedQ, C, accuracy, iter_max, seed, &cdim, &colors, &color_diff, &color_diff_sum, &flag);
+    if (flag) goto RETURN;
+#ifdef TIME
+    fprintf(stderr, "cpu for color assignmment =%10.3f\n", ((real) (clock() - start))/CLOCKS_PER_SEC);
+#endif
+  }
+
+  /* for printing dual*/
+#if 0
+  {
+    FILE*fp;
+    fp = fopen("/tmp/dual.gv","w");
+    fprintf(fp,"graph {\n");
+    for (i = 0; i < nz2; i++){
+      u1 = irn[i]; v1 = jcn[i];
+      for (j = i+1; j < nz2; j++){
+	u2 = irn[j]; v2 = jcn[j];
+	cos_a = intersection_angle(&(x[dim*u1]), &(x[dim*v1]), &(x[dim*u2]), &(x[dim*v2]));
+	if (!check_edges_with_same_endpoint && cos_a >= -1) cos_a = ABS(cos_a);
+	if (cos_a > cos_critical) {
+	  fprintf(fp,"\"%d_%d\" -- \"%d_%d\"\n", u1,v1,u2,v2);
+	}
+      }
+    }
+    for (i = 0; i < nz2; i++){
+      u1 = irn[i]; v1 = jcn[i];
+      if (cdim == 3) {
+	fprintf(fp,"\"%d_%d\" [label2=%d pos=\"%f,%f\", color=\"#%02x%02x%02x\"]\n", u1,v1, i, 0.5*(x[dim*u1] + x[dim*v1]), 0.5*(x[dim*u1+1] + x[dim*v1+1]), 
+		MIN((unsigned int)(colors[i*cdim]*255),255), MIN((unsigned int) (colors[i*cdim+1]*255), 255), MIN((unsigned int)(colors[i*cdim+2]*255), 255));
+      } else if (cdim == 2){
+	fprintf(fp,"\"%d_%d\" [label2=%d pos=\"%f,%f\", color=\"#%02x%02x%02x\"]\n", u1,v1, i, 0.5*(x[dim*u1] + x[dim*v1]), 0.5*(x[dim*u1+1] + x[dim*v1+1]), 
+		MIN((unsigned int)(colors[i*cdim]*255),255), MIN((unsigned int) (colors[i*cdim+1]*255), 255), 255);
+      } else {
+	fprintf(fp,"\"%d_%d\" [label2=%d pos=\"%f,%f\", color=\"#%02x%02x%02x\"]\n", u1,v1, i, 0.5*(x[dim*u1] + x[dim*v1]), 0.5*(x[dim*u1+1] + x[dim*v1+1]), 
+		MIN((unsigned int)(colors[i*cdim]*255),255), MIN((unsigned int) (colors[i*cdim]*255), 255), MIN((unsigned int)(colors[i*cdim]*255), 255));
+      }
+    }
+    fprintf(fp,"}\n");
+  }
+#endif
+
+  fprintf(stderr,"The edge conflict graph has %d nodes and %d edges, final color_diff=%f color_diff_sum = %f\n",
+	  C->m, C->nz, color_diff, color_diff_sum);
+
+  attach_edge_colors(g, cdim, colors);
+
+ RETURN:
+  SparseMatrix_delete(A);
+  SparseMatrix_delete(C);
+  FREE(colors);
+  FREE(x);
+  if (xsplines){
+    for (i = 0; i < ne; i++){
+      if (xsplines[i]) FREE(xsplines[i]);
+    }
+    FREE(xsplines);
+  }
+  return g;
+
+}
+

lib/edgepaint/edge_distinct_coloring.h
@@ -1,0 +1,1 @@
+Agraph_t* edge_distinct_coloring(char *color_scheme, char *lightness, Agraph_t* g, real angle, real accuracy, real check_edges_with_same_endpoint, int seed);

sea-grass/graphviz

commit 72d5111ef65ae6c68656e0a058dba1b92d78090d

fix 'qt' memory leaks
Coverity notes:

  Error: RESOURCE_LEAK (CWE-772): [#def101]
  graphviz-2.40.1/lib/edgepaint/node_distinct_coloring.c:249: alloc_fn: Storage
    is returned from allocation function "lab_gamut_quadtree".
  graphviz-2.40.1/lib/edgepaint/lab.c:256:3: alloc_fn: Storage is returned from
    allocation function "QuadTree_new_from_point_list".
  graphviz-2.40.1/lib/sparse/QuadTree.c:383:3: alloc_fn: Storage is returned
    from allocation function "QuadTree_new".
  graphviz-2.40.1/lib/sparse/QuadTree.c:405:3: alloc_fn: Storage is returned
    from allocation function "gmalloc".
  graphviz-2.40.1/lib/common/memory.c:47:5: alloc_fn: Storage is returned from
    allocation function "malloc". [Note: The source code implementation of the
    function has been overridden by a builtin model.]
  graphviz-2.40.1/lib/common/memory.c:47:5: var_assign: Assigning: "rv" =
    "malloc(nbytes)".
  graphviz-2.40.1/lib/common/memory.c:51:5: return_alloc: Returning allocated
    memory "rv".
  graphviz-2.40.1/lib/sparse/QuadTree.c:405:3: var_assign: Assigning: "q" =
    "gmalloc(80UL)".
  graphviz-2.40.1/lib/sparse/QuadTree.c:418:3: return_alloc: Returning allocated
    memory "q".
  graphviz-2.40.1/lib/sparse/QuadTree.c:383:3: var_assign: Assigning: "qt" =
    "QuadTree_new(dim, center, width, max_level)".
  graphviz-2.40.1/lib/sparse/QuadTree.c:387:7: identity_transfer: Passing "qt"
    as argument 1 to function "QuadTree_add", which returns that argument.
  graphviz-2.40.1/lib/sparse/QuadTree.c:555:11: return_parm: Returning parameter
    "q".
  graphviz-2.40.1/lib/sparse/QuadTree.c:387:7: noescape: Resource "qt" is not
    freed or pointed-to in function "QuadTree_add".
  graphviz-2.40.1/lib/sparse/QuadTree.c:554:32: noescape:
    "QuadTree_add(QuadTree, double *, double, int)" does not free or save its
    parameter "q".
  graphviz-2.40.1/lib/sparse/QuadTree.c:387:7: var_assign: Assigning: "qt" =
    "QuadTree_add(qt, &coord[i * dim], weight[i], i)".
  graphviz-2.40.1/lib/sparse/QuadTree.c:387:7: identity_transfer: Passing "qt"
    as argument 1 to function "QuadTree_add", which returns that argument.
  graphviz-2.40.1/lib/sparse/QuadTree.c:555:11: return_parm: Returning
    parameter "q".
  graphviz-2.40.1/lib/sparse/QuadTree.c:387:7: noescape: Resource "qt" is not
    freed or pointed-to in function "QuadTree_add".
  graphviz-2.40.1/lib/sparse/QuadTree.c:554:32: noescape:
    "QuadTree_add(QuadTree, double *, double, int)" does not free or save its
    parameter "q".
  graphviz-2.40.1/lib/sparse/QuadTree.c:387:7: var_assign: Assigning: "qt" =
    "QuadTree_add(qt, &coord[i * dim], weight[i], i)".
  graphviz-2.40.1/lib/sparse/QuadTree.c:399:3: return_alloc: Returning
    allocated memory "qt".
  graphviz-2.40.1/lib/edgepaint/lab.c:256:3: var_assign: Assigning: "qt" =
    "QuadTree_new_from_point_list(dim, n, max_qtree_level, x, NULL)".
  graphviz-2.40.1/lib/edgepaint/lab.c:260:3: return_alloc: Returning allocated
    memory "qt".
  graphviz-2.40.1/lib/edgepaint/node_distinct_coloring.c:249: var_assign:
    Assigning: "qt" = storage returned from "lab_gamut_quadtree("lab_gamut",
    lightness, max_qtree_level)".
  graphviz-2.40.1/lib/edgepaint/node_distinct_coloring.c:281: leaked_storage:
    Variable "qt" going out of scope leaks the storage it points to.
  #  279|     if (n != A->n) {
  #  280|       *flag = -1;
  #  281|->     return;
  #  282|     }
  #  283|

  Error: RESOURCE_LEAK (CWE-772): [#def102]
  graphviz-2.40.1/lib/edgepaint/node_distinct_coloring.c:292: alloc_arg:
    "SparseMatrix_weakly_connected_components" allocates memory that is stored
    into "comps_ptr".
  graphviz-2.40.1/lib/sparse/SparseMatrix.c:2566:22: alloc_fn: Storage is
    returned from allocation function "gmalloc".
  graphviz-2.40.1/lib/common/memory.c:47:5: alloc_fn: Storage is returned from
    allocation function "malloc". [Note: The source code implementation of the
    function has been overridden by a builtin model.]
  graphviz-2.40.1/lib/common/memory.c:47:5: var_assign: Assigning: "rv" =
    "malloc(nbytes)".
  graphviz-2.40.1/lib/common/memory.c:51:5: return_alloc: Returning allocated
    memory "rv".
  graphviz-2.40.1/lib/sparse/SparseMatrix.c:2566:22: var_assign: Assigning:
    "*comps_ptr" = "gmalloc(4UL * (size_t)(m + 1))".
  graphviz-2.40.1/lib/edgepaint/node_distinct_coloring.c:318: leaked_storage:
    Variable "comps_ptr" going out of scope leaks the storage it points to.
  #  316|
  #  317|     if (A != A0) SparseMatrix_delete(A);
  #  318|-> }

This memory leak seems to have been present ever since this code was introduced
in aadee957012e676c9aaf36ec40cb2860c178e269.

lib/edgepaint/node_distinct_coloring.c
@@ -239,6 +239,7 @@ int node_distinct_coloring(char *color_scheme, char *lightness, bool weightedQ,
 
   n = A->m;
   if (n != A->n) {
+    QuadTree_delete(qt);
     return -1;
   }
  
@@ -264,6 +265,7 @@ int node_distinct_coloring(char *color_scheme, char *lightness, bool weightedQ,
     SparseMatrix_delete(B);
   }
   free(ctmp);
+  QuadTree_delete(qt);
 
   if (A != A0) SparseMatrix_delete(A);
   return 0;