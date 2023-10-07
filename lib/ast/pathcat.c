e736297fc9821f2220e14af2a70b840c603b839f
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-10-07 16:08:51 +1100

    ast: clang-format pathcat.c

--- lib/ast/pathcat.c
+++ lib/ast/pathcat.c
@@ -1,5 +1,5 @@
 /*************************************************************************
- * Copyright (c) 2011 AT&T Intellectual Property 
+ * Copyright (c) 2011 AT&T Intellectual Property
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -18,22 +18,21 @@
 #include <ast/ast.h>
 #include <cgraph/agxbuf.h>
 
-const char *pathcat(agxbuf *path, const char *dirs,
-	      const char *a, const char *b)
-{
-    const char sep = ':';
+const char *pathcat(agxbuf *path, const char *dirs, const char *a,
+                    const char *b) {
+  const char sep = ':';
 
-    while (*dirs && *dirs != sep)
-	agxbputc(path, *dirs++);
-    if (agxblen(path) > 0)
-	agxbputc(path, '/');
-    if (a) {
-	agxbput(path, a);
-	if (b)
-	    agxbputc(path, '/');
-    } else if (!b)
-	b = ".";
+  while (*dirs && *dirs != sep)
+    agxbputc(path, *dirs++);
+  if (agxblen(path) > 0)
+    agxbputc(path, '/');
+  if (a) {
+    agxbput(path, a);
     if (b)
-	agxbput(path, b);
-    return *dirs ? ++dirs : 0;
+      agxbputc(path, '/');
+  } else if (!b)
+    b = ".";
+  if (b)
+    agxbput(path, b);
+  return *dirs ? ++dirs : 0;
 }
