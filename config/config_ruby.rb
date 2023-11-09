1941c3b4ead59b2d5773d7c6be933aa99d08c2df
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-11-08 23:29:33 -0800

    Autotools: generalize Ruby config script to handle lookup of any variable
    The `lib` query is updated to `libdir` query to match its config entry. It is
    unclear to me why 3a85bbcbcdbbb0968a806320e64b40c0fe3feb80 altered this to use
    a parameter name that differs from its config entry name, obscuring the intent.

--- config/config_ruby.rb
+++ config/config_ruby.rb
@@ -2,15 +2,4 @@ require 'rbconfig'
 
 CONFIG = RbConfig::MAKEFILE_CONFIG
 
-
-case ARGV[0]
-when "archdir"
-    puts RbConfig::expand(CONFIG["archdir"])
-when "lib"
-    puts RbConfig::expand(CONFIG["libdir"])
-when "vendorarchdir"
-    puts RbConfig::expand(CONFIG["vendorarchdir"])
-when "sitearchdir"
-    puts RbConfig::expand(CONFIG["sitearchdir"])
-end
-
+puts RbConfig::expand(CONFIG[ARGV[0]])
