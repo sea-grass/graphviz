01e40e2536adf6704eb66ff9c6a68a00299fb10e
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-11-14 17:10:47 +1100

    neatogen stress_majorization_with_hierarchy: fix another 'levels' memory leak
    Coverity says:
    
      Error: RESOURCE_LEAK (CWE-772): [#def122]
      graphviz-2.40.1/lib/neatogen/constrained_majorization.c:130: alloc_arg:
        "compute_hierarchy" allocates memory that is stored into "levels".
      graphviz-2.40.1/lib/neatogen/compute_hierarchy.c:118:2: alloc_fn: Storage is
        returned from allocation function "gmalloc".
      graphviz-2.40.1/lib/common/memory.c:47:5: alloc_fn: Storage is returned from
        allocation function "malloc". [Note: The source code implementation of the
        function has been overridden by a builtin model.]
      graphviz-2.40.1/lib/common/memory.c:47:5: var_assign: Assigning:
        "rv" = "malloc(nbytes)".
      graphviz-2.40.1/lib/common/memory.c:51:5: return_alloc: Returning allocated
        memory "rv".
      graphviz-2.40.1/lib/neatogen/compute_hierarchy.c:118:2: var_assign: Assigning:
        "levels" = "gmalloc(4UL)".
      graphviz-2.40.1/lib/neatogen/compute_hierarchy.c:118:2: var_assign: Assigning:
        "*levelsp" = "levels".
      graphviz-2.40.1/lib/neatogen/constrained_majorization.c:137: leaked_storage:
        Variable "levels" going out of scope leaks the storage it points to.
      #  135|   	if (num_levels < 1) {
      #  136|   	    /* no hierarchy found, use faster algorithm */
      #  137|-> 	    return stress_majorization_kD_mkernel(graph, n, nedges_graph,
      #  138|   						  d_coords, nodes, dim,
      #  139|   						  opts, model, maxi);
    
    Gitlab: #1464
    Reported-by: Jaroslav Škarvada <jskarvad@redhat.com>

--- lib/neatogen/constrained_majorization.c
+++ lib/neatogen/constrained_majorization.c
@@ -124,6 +124,7 @@ int stress_majorization_with_hierarchy(vtx_data * graph,	/* Input graph in spars
 	}
 	if (num_levels < 1) {
 	    /* no hierarchy found, use faster algorithm */
+	    free(levels);
 	    return stress_majorization_kD_mkernel(graph, n,
 						  d_coords, nodes, dim,
 						  opts, model, maxi);
