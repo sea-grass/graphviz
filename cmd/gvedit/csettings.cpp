sea-grass/graphviz

commit f299564dcec26d74837afbd2b9d1cf36b5ca294e

gvedit find_me: avoid 'PATH_MAX'
Unconditional use of `PATH_MAX` is causing compilation failures on Hurd, where
`PATH_MAX` is not defined. Moreover there are some critical problems with
relying on `PATH_MAX`.¹ As glibc’s `readlink` man page notes:

  Using a statically sized buffer might not provide enough room for the symbolic
  link contents. The required size for the buffer can be obtained from the
  `stat.st_size` value returned by a call to `lstat(2)` on the link. However,
  the number of bytes written by `readlink()` and `readlinkat()` should be
  checked to make sure that the size of the symbolic link did not increase
  between the calls. Dynamically allocating the buffer for `readlink()` and
  `readlinkat()` also addresses a common portability problem when using
  `PATH_MAX` for the buffer size, as this constant is not guaranteed to be
  defined per POSIX if the system does not have such limit.

This commit is part of work to remove the use of `PATH_MAX`. Like
65430bd92a61ca924941b252b4e261ff668efa34 from where this code originated, this
change takes Smyrna code and adapts it to C++.

Gitlab: #2452
Reported-by: László Böszörményi (GCS) <gcs@debian.org>

¹ https://insanecoding.blogspot.com/2007/11/pathmax-simply-isnt.html

cmd/gvedit/csettings.cpp
@@ -47,18 +47,31 @@ typedef struct {
 } rdr_t;
 
 #if !defined(__APPLE__) && !defined(_WIN32)
-/// read the given symlink in the /proc file system
-static std::string read_proc(const std::string &path) {
+/// `readlink`-alike but dynamically allocates
+static std::string readln(const std::string &path) {
 
-  std::vector<char> buf(PATH_MAX + 1, '\0');
-  if (readlink(path.c_str(), buf.data(), buf.size()) < 0)
-    return "";
+  std::vector<char> buf(512, '\0');
 
-  // was the path too long?
-  if (buf.back() != '\0')
-    return "";
+  while (true) {
 
-  return buf.data();
+    // expand target buffer
+    buf.resize(buf.size() * 2);
+
+    // attempt to resolve
+    {
+      ssize_t written = readlink(path.c_str(), buf.data(), buf.size());
+      if (written < 0)
+        break;
+      if (static_cast<size_t>(written) < buf.size()) {
+        // success
+        buf[written] = '\0';
+        return buf.data();
+      }
+    }
+  }
+
+  // failed
+  return "";
 }
 #endif
 
@@ -83,12 +96,12 @@ static std::string find_me(void) {
     }
 
     // try to resolve any levels of symlinks if possible
-    while (true) {
-      std::vector<char> buf(PATH_MAX + 1, '\0');
-      if (readlink(path.data(), buf.data(), buf.size()) < 0)
-        return path.data();
+    for (std::string p = path.data();;) {
+      const std::string buf = readln(p);
+      if (buf == "")
+        return p;
 
-      path = buf;
+      p = buf;
     }
   }
 #elif defined(_WIN32)
@@ -115,17 +128,17 @@ static std::string find_me(void) {
 #else
 
   // Linux
-  std::string path = read_proc("/proc/self/exe");
+  std::string path = readln("/proc/self/exe");
   if (path != "")
     return path;
 
   // DragonFly BSD, FreeBSD
-  path = read_proc("/proc/curproc/file");
+  path = readln("/proc/curproc/file");
   if (path != "")
     return path;
 
   // NetBSD
-  path = read_proc("/proc/curproc/exe");
+  path = readln("/proc/curproc/exe");
   if (path != "")
     return path;
 
@@ -134,10 +147,21 @@ static std::string find_me(void) {
   {
     int mib[] = {CTL_KERN, KERN_PROC, KERN_PROC_PATHNAME, -1};
     static const size_t MIB_LENGTH = sizeof(mib) / sizeof(mib[0]);
-    std::vector<char> buf(PATH_MAX + 1, '\0');
-    size_t buf_size = buf.size();
-    if (sysctl(mib, MIB_LENGTH, buf.data(), &buf_size, NULL, 0) == 0)
-      return buf.data();
+
+    do {
+      // determine how long the path is
+      size_t buf_size = 0;
+      if (sysctl(mib, MIB_LENGTH, NULL, &buf_size, NULL, 0) < 0)
+        break;
+      assert(buf_size > 0);
+
+      // make enough space for the target path
+      std::vector<char> buf(buf_size, '\0');
+
+      // resolve it
+      if (sysctl(mib, MIB_LENGTH, buf.data(), &buf_size, NULL, 0) == 0)
+        return buf.data();
+    } while (0);
   }
 #endif
 #endif

commit b397164783763e56d6b77a2be05fa8d3bbabe7ec

gvedit: fix compilation on macOS
This was broken in f299564dcec26d74837afbd2b9d1cf36b5ca294e, but went undetected
because we no longer have macOS CI. No changelog entry because there has been no
release since then.

Gitlab: #2411

cmd/gvedit/csettings.cpp
@@ -46,7 +46,7 @@ typedef struct {
   int cur;
 } rdr_t;
 
-#if !defined(__APPLE__) && !defined(_WIN32)
+#ifndef _WIN32
 /// `readlink`-alike but dynamically allocates
 static std::string readln(const std::string &path) {
 