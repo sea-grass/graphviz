675eafd5d8c179e10132907298743ca2e95445a4
Ari Epstein <aepstein607@gmail.com>
2023-12-01 19:30:11 -0500

    Change license references to EPLv2

--- plugin.demo/xgtk/autogen.sh
+++ plugin.demo/xgtk/autogen.sh
@@ -17,11 +17,11 @@ autoreconf -v --install --force || exit 1
 # otherwise "make dist" fails.
 touch config/depcomp
 
-# ensure COPYING is based on epl-v10.txt
-#   epl-v10.txt was obtained from: http://www.eclipse.org/legal/epl-v10.html
+# ensure COPYING is based on epl-v20.txt
+#   epl-v20.txt was obtained from: http://www.eclipse.org/legal/epl-v20.html
 #   by using lynx to print to .txt.
 rm -f COPYING
-cp epl-v10.txt COPYING
+cp epl-v20.txt COPYING
 
 # don't use any old cache, but create a new one
 rm -f config.cache
