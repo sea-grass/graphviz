4f88c683bb053b2e9f477f0617e4665f5804a6e1
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-12-26 17:27:49 -0700

    edgepaint splines_intersect: use 'size_t' variables for array indexing
    Squashes 6 -Wsign-conversion warnings.

--- lib/edgepaint/edge_distinct_coloring.c
+++ lib/edgepaint/edge_distinct_coloring.c
@@ -17,7 +17,7 @@
 #include <edgepaint/intersection.h>
 #include <sparse/QuadTree.h>
 
-static int splines_intersect(int dim,
+static int splines_intersect(size_t dim,
 			     double cos_critical, int check_edges_with_same_endpoint, 
 			     char *xsplines1, char *xsplines2){
   /* cos_critical: cos of critical angle
@@ -26,10 +26,10 @@ static int splines_intersect(int dim,
      xsplines1,xsplines2: the first and second splines corresponding to two edges
 
   */
-  int len1 = 100, len2 = 100;
+  size_t len1 = 100, len2 = 100;
   double *x1, *x2;
-  int ns1 = 0, ns2 = 0;
-  int i, j, iter1 = 0, iter2 = 0;
+  size_t ns1 = 0, ns2 = 0;
+  int iter1 = 0, iter2 = 0;
   double cos_a, tmp[2];
   int endp1 = 0, endp2 = 0;
 
@@ -63,14 +63,14 @@ static int splines_intersect(int dim,
     if (!xsplines1) break;
     xsplines1++;
     if (ns1*dim >= len1){
-      len1 = ns1*dim + (int)MAX(10, 0.2*ns1*dim);
+      len1 = ns1 * dim + MAX(10u, ns1 * dim / 5);
       x1 = REALLOC(x1, sizeof(double)*len1);
     }
   }
   if (endp1){/* pad the end point at the last position */
     ns1++;
     if (ns1*dim >= len1){
-      len1 = ns1*dim + (int)MAX(10, 0.2*ns1*dim);
+      len1 = ns1 * dim + MAX(10u, ns1 * dim / 5);
       x1 = REALLOC(x1, sizeof(double)*len1);
     }
     x1[(ns1-1)*dim] = tmp[0];  x1[(ns1-1)*dim + 1] = tmp[1]; 
@@ -101,21 +101,21 @@ static int splines_intersect(int dim,
     if (!xsplines2) break;
     xsplines2++;
     if (ns2*dim >= len2){
-      len2 = ns2*dim + (int)MAX(10, 0.2*ns2*dim);
+      len2 = ns2 * dim + MAX(10u, ns2 * dim / 5);
       x2 = REALLOC(x2, sizeof(double)*len2);
     }
   }
   if (endp2){/* pad the end point at the last position */
     ns2++;
     if (ns2*dim >= len2){
-      len2 = ns2*dim + (int)MAX(10, 0.2*ns2*dim);
+      len2 = ns2 * dim + MAX(10u, ns2 * dim / 5);
       x2 = REALLOC(x2, sizeof(double)*len2);
     }
     x2[(ns2-1)*dim] = tmp[0];  x2[(ns2-1)*dim + 1] = tmp[1]; 
   }
 
-for (i = 0; i < ns1 - 1; i++){
-    for (j = 0; j < ns2 - 1; j++){
+  for (size_t i = 0; i < ns1 - 1; i++) {
+    for (size_t j = 0; j < ns2 - 1; j++) {
       cos_a = intersection_angle(&(x1[dim*i]), &(x1[dim*(i + 1)]), &(x2[dim*j]), &(x2[dim*(j+1)]));
       if (!check_edges_with_same_endpoint && cos_a >= -1) cos_a = fabs(cos_a);
       if (cos_a > cos_critical) {
@@ -186,7 +186,9 @@ Agraph_t* edge_distinct_coloring(char *color_scheme, char *lightness, Agraph_t* 
     cos_a = 1.;/* for splines we exit conflict check as soon as we find an conflict, so the anle may not be representitive, hence set to constant */
     for (i = 0; i < nz2; i++){
       for (j = i+1; j < nz2; j++){
-	if (splines_intersect(dim, cos_critical, check_edges_with_same_endpoint, xsplines[i], xsplines[j])){
+	if (splines_intersect((size_t)dim, cos_critical,
+	                      check_edges_with_same_endpoint, xsplines[i],
+	                      xsplines[j])) {
 	  B = SparseMatrix_coordinate_form_add_entry(B, i, j, &cos_a);
 	}
       }
