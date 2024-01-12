f011d4accd7863d10298da0704702c46285d76ea
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-01-12 17:28:54 +1100

    library-ize 'acyclic'
    Gitlab: closes #2194
    Reported-by: Ross Barnowski

--- cmd/tools/acyclic.c
+++ cmd/tools/acyclic.c
@@ -29,87 +29,15 @@
 #include <cgraph/unreachable.h>
 #include "openFile.h"
 
-typedef struct {
-    Agrec_t h;
-    int mark;
-    bool onstack: 1;
-} Agnodeinfo_t;
-
-#define ND_mark(n) (((Agnodeinfo_t*)((n)->base.data))->mark)
-#define ND_onstack(n) (((Agnodeinfo_t*)((n)->base.data))->onstack)
 #define graphName(g) (agnameof(g))
 
 #include <getopt.h>
 
 static FILE *inFile;
 
-typedef struct {
-  FILE *outFile;
-  bool doWrite;
-  bool Verbose;
-} graphviz_acyclic_options_t;
-
 typedef graphviz_acyclic_options_t opts_t;
 
 static char *cmd;
-
-/* addRevEdge:
- * Add a reversed version of e. The new edge has the same key.
- * We also copy the attributes, reversing the roles of head and 
- * tail ports.
- * This assumes we've already checked that such an edge does not exist.
- */
-static void addRevEdge(Agraph_t *g, Agedge_t *e)
-{
-    Agsym_t* sym;
-    Agedge_t* f = agedge (g, aghead(e), agtail(e), agnameof(e), 1);
-
-    agcopyattr (e, f);
-
-    sym = agattr (g, AGEDGE, TAILPORT_ID, 0);
-    if (sym) agsafeset (f, HEADPORT_ID, agxget (e, sym), "");
-    sym = agattr (g, AGEDGE, HEADPORT_ID, 0);
-    if (sym) agsafeset (f, TAILPORT_ID, agxget (e, sym), "");
-}
-
-/* dfs:
- * Return true if the graph has a cycle.
- *
- * \param num_rev [inout] running total of reversed edges
- */
-static bool dfs(Agraph_t *g, Agnode_t *t, bool hasCycle, size_t *num_rev) {
-    Agedge_t *e;
-    Agedge_t *f;
-    Agnode_t *h;
-
-    ND_mark(t) = 1;
-    ND_onstack(t) = true;
-    for (e = agfstout(g, t); e; e = f) {
-	f = agnxtout(g, e);
-	if (agtail(e) == aghead(e))
-	    continue;
-	h = aghead(e);
-	if (ND_onstack(h)) {
-	    if (agisstrict(g)) {
-		if (agedge(g, h, t, 0, 0) == 0) {
-		    addRevEdge(g, e);
-		    ++*num_rev;
-		}
-	    } else {
-		char* key = agnameof (e);
-		if (!key || agedge(g, h, t, key, 0) == 0) {
-		    addRevEdge(g, e);
-		    ++*num_rev;
-		}
-	    }
-	    agdelete(g, e);
-	    hasCycle = true;
-	} else if (ND_mark(h) == 0)
-	    hasCycle |= dfs(g, h, hasCycle, num_rev);
-    }
-    ND_onstack(t) = false;
-    return hasCycle;
-}
 
 static char *useString = "Usage: %s [-nv?] [-o outfile] <file>\n\
   -o <file> - put output in <file>\n\
@@ -166,21 +94,6 @@ static void init(opts_t *opts, int argc, char *argv[]) {
 	opts->outFile = stdout;
 }
 
-static bool acyclic(Agraph_t *g, const opts_t *opts, size_t *num_rev) {
-  bool has_cycle = false;
-  aginit(g, AGNODE, "info", sizeof(Agnodeinfo_t), true);
-  for (Agnode_t *n = agfstnode(g); n; n = agnxtnode(g, n)) {
-    if (ND_mark(n) == 0) {
-      has_cycle |= dfs(g, n, false, num_rev);
-    }
-  }
-  if (opts->doWrite) {
-    agwrite(g, opts->outFile);
-    fflush(opts->outFile);
-  }
-  return has_cycle;
-}
-
 int main(int argc, char *argv[])
 {
     Agraph_t *g;
@@ -192,7 +105,7 @@ int main(int argc, char *argv[])
 
     if ((g = agread(inFile, NULL)) != NULL) {
 	if (agisdirected (g)) {
-	    rv |= acyclic(g, &opts, &num_rev);
+	    rv |= graphviz_acyclic(g, &opts, &num_rev);
 	    if (opts.Verbose) {
 		if (rv)
 		    fprintf(stderr, "Graph \"%s\" has cycles; %" PRISIZE_T " reversed edges\n",
