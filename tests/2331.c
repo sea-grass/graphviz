d9a6a84f5a8c172992884fce086935841a628580
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-02-03 16:56:43 +1100

    XXX

--- tests/2331.c
+++ tests/2331.c
@@ -7,6 +7,14 @@
 #include <stdbool.h>
 #include <string.h>
 
+extern gvplugin_library_t gvplugin_dot_layout_LTX_library;
+extern gvplugin_library_t gvplugin_core_LTX_library;
+
+static const lt_symlist_t lt_preloaded_symbols[] = {
+    {"gvplugin_dot_layout_LTX_library", &gvplugin_dot_layout_LTX_library},
+    {"gvplugin_core_LTX_library", &gvplugin_core_LTX_library},
+    {0, 0}};
+
 int main(void) {
   GVC_t *gvc = gvContextPlugins(lt_preloaded_symbols, false);
 
