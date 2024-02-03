dbc2be8ce1b006e02c8c9e3f14c34adfa664fdb2
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-02-03 17:03:31 +1100

    XXX more tweaking

--- tests/2331.c
+++ tests/2331.c
@@ -10,7 +10,7 @@
 extern gvplugin_library_t gvplugin_dot_layout_LTX_library;
 extern gvplugin_library_t gvplugin_core_LTX_library;
 
-static const lt_symlist_t lt_preloaded_symbols[] = {
+lt_symlist_t lt_preloaded_symbols[] = {
     {"gvplugin_dot_layout_LTX_library", &gvplugin_dot_layout_LTX_library},
     {"gvplugin_core_LTX_library", &gvplugin_core_LTX_library},
     {0, 0}};
