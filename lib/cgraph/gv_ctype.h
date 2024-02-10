evilkim/gitlab-graphviz-mirror

commit 8636e5135e59aac476df5cc03f9c8648e3b714e7

remove use of an unsigned char in _agstrcanon
As far as I can see, nothing in this function depends on uc being unsigned.
Making it a standard char reduces some cast noise.

lib/cgraph/write.c
@@ -54,7 +54,7 @@ static int indent(Agraph_t * g, iochan_t * ofile)
 static char *_agstrcanon(char *arg, char *buf)
 {
     char *s, *p;
-    unsigned char uc;
+    char uc;
     int cnt = 0, dotcnt = 0;
     bool needs_quotes = false;
     int maybe_num;
@@ -70,7 +70,7 @@ static char *_agstrcanon(char *arg, char *buf)
     s = arg;
     p = buf;
     *p++ = '\"';
-    uc = *(unsigned char *) s++;
+    uc = *s++;
     maybe_num = isdigit(uc) || uc == '.' || uc == '-';
     while (uc) {
 	if (uc == '\"') {
@@ -97,8 +97,8 @@ static char *_agstrcanon(char *arg, char *buf)
 	}
 	else if (!ISALNUM(uc))
 	    needs_quotes = true;
-	*p++ = (char) uc;
-	uc = *(unsigned char *) s++;
+	*p++ = uc;
+	uc = *s++;
 	cnt++;
 	
 	/* If breaking long strings into multiple lines, only allow breaks after a non-id char, not a backslash, where the next char is an

sea-grass/graphviz

commit a30eaac814f1143f7f25ff86402ff7b3e587ccdd

fix: avoid 'isalnum' when processing possibly-UTF-8 input
On Windows platforms with a signed `char` type, this can cause an assertion
failure in the standard library:

  isctype.cpp:
  Expression c >= -1 && c <= 255

This problem was introduced in 8636e5135e59aac476df5cc03f9c8648e3b714e7. But
rather than revert that commit, this change does something hopefully more
robust. This alternative is neither locale- nor signedness-of-char-dependent.

Gitlab: fixes #2502
Reported-by: Chiel ten Brinke
Tested-by: Chiel ten Brinke

lib/cgraph/gv_ctype.h
@@ -30,3 +30,5 @@ static inline bool gv_isalpha(int c) {
 }
 
 static inline bool gv_isdigit(int c) { return c >= '0' && c <= '9'; }
+
+static inline bool gv_isalnum(int c) { return gv_isalpha(c) || gv_isdigit(c); }

lib/cgraph/write.c
@@ -48,7 +48,7 @@ static int indent(Agraph_t * g, iochan_t * ofile)
 
 // alphanumeric, '.', '-', or non-ascii; basically, chars used in unquoted ids
 static bool is_id_char(char c) {
-  return isalnum(c) || c == '.' || c == '-' || !isascii(c);
+  return gv_isalnum(c) || c == '.' || c == '-' || !isascii(c);
 }
 
 // is the prefix of this string a recognized Graphviz escape sequence?
@@ -140,7 +140,7 @@ static char *_agstrcanon(char *arg, char *buf)
 	    }
 	    part_of_escape = false;
 	}
-	else if (!(isalnum(uc) || uc == '_' || !isascii(uc))) {
+	else if (!(gv_isalnum(uc) || uc == '_' || !isascii(uc))) {
 	    needs_quotes = true;
 	    part_of_escape = false;
 	}