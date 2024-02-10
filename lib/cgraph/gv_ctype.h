evilkim/gitlab-graphviz-mirror

commit 8636e5135e59aac476df5cc03f9c8648e3b714e7

remove use of an unsigned char in _agstrcanon
As far as I can see, nothing in this function depends on uc being unsigned.
Making it a standard char reduces some cast noise.

lib/cgraph/write.c
@@ -54,7 +54,7 @@ static int indent(Agraph_t * g, iochan_t * ofile)
 static char *_agstrcanon(char *arg, char *buf)
 {
     char *s, *p;
-    unsigned char uc;
+    char uc;
     int cnt = 0, dotcnt = 0;
     bool needs_quotes = false;
     int maybe_num;
@@ -70,7 +70,7 @@ static char *_agstrcanon(char *arg, char *buf)
     s = arg;
     p = buf;
     *p++ = '\"';
-    uc = *(unsigned char *) s++;
+    uc = *s++;
     maybe_num = isdigit(uc) || uc == '.' || uc == '-';
     while (uc) {
 	if (uc == '\"') {
@@ -97,8 +97,8 @@ static char *_agstrcanon(char *arg, char *buf)
 	}
 	else if (!ISALNUM(uc))
 	    needs_quotes = true;
-	*p++ = (char) uc;
-	uc = *(unsigned char *) s++;
+	*p++ = uc;
+	uc = *s++;
 	cnt++;
 	
 	/* If breaking long strings into multiple lines, only allow breaks after a non-id char, not a backslash, where the next char is an

sea-grass/graphviz

commit 7addc1dca0f1fdfac7ac483d497aadd3bf06e3ec

fix: avoid 'isdigit' when processing possibly-UTF-8 input
On Windows platforms with a signed `char` type, this can cause an assertion
failure in the standard library:

  isctype.cpp:
  Expression c >= -1 && c <= 255

This problem was introduced in 8636e5135e59aac476df5cc03f9c8648e3b714e7. But
rather than revert that commit, this change does something hopefully more
robust. This alternative is neither locale- nor signedness-of-char-dependent.

As an aside, your author has worked on another software project where `isdigit`
calls inside a hot loop were identified as a performance issue. Surprisingly it
turned out that glibc’s implementation of this function indirects through a
locale-dependent table. This is despite `isdigit`, as documented, having
behavior that is _not_ locale dependent.

Gitlab: #2502
Reported-by: Chiel ten Brinke

lib/cgraph/CMakeLists.txt
@@ -21,6 +21,7 @@ add_library(cgraph
   cgraph.h
   clamp.h
   exit.h
+  gv_ctype.h
   ingraphs.h
   list.h
   overflow.h

lib/cgraph/Makefile.am
@@ -9,9 +9,9 @@ AM_CFLAGS = -DEXPORT_CGRAPH -DEXPORT_CGHDR
 endif
 
 pkginclude_HEADERS = cgraph.h
-noinst_HEADERS = agxbuf.h alloc.h bitarray.h cghdr.h clamp.h exit.h ingraphs.h \
-	list.h overflow.h prisize_t.h sort.h stack.h startswith.h strcasecmp.h \
-	streq.h strview.h tokenize.h unreachable.h unused.h
+noinst_HEADERS = agxbuf.h alloc.h bitarray.h cghdr.h clamp.h exit.h gv_ctype.h \
+	ingraphs.h list.h overflow.h prisize_t.h sort.h stack.h startswith.h \
+	strcasecmp.h streq.h strview.h tokenize.h unreachable.h unused.h
 noinst_LTLIBRARIES = libcgraph_C.la
 lib_LTLIBRARIES = libcgraph.la
 pkgconfig_DATA = libcgraph.pc

lib/cgraph/cgraph.vcxproj
@@ -104,6 +104,7 @@ win_flex --case-insensitive --outfile=scan.c scan.l</Command>
     <ClInclude Include="cgraph.h" />
     <ClInclude Include="clamp.h" />
     <ClInclude Include="exit.h" />
+    <ClInclude Include="gv_ctype.h" />
     <ClInclude Include="ingraphs.h" />
     <ClInclude Include="list.h" />
     <ClInclude Include="overflow.h" />

lib/cgraph/cgraph.vcxproj.filters
@@ -36,6 +36,9 @@
     <ClInclude Include="exit.h">
       <Filter>Header Files</Filter>
     </ClInclude>
+    <ClInclude Include="gv_ctype.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
     <ClInclude Include="ingraphs.h">
       <Filter>Header Files</Filter>
     </ClInclude>

lib/cgraph/gv_ctype.h
@@ -1,0 +1,24 @@
+/// \file
+/// \brief replacements for ctype.h functions
+///
+/// The behavior of the ctype.h functions is locale-dependent, while Graphviz
+/// code typically wants to ask about character data specifically interpreted as
+/// ASCII. The current locale is frequently irrelevant because Graphviz (1)
+/// supports input in encodings different than the user’s locale via the
+/// `charset` attribute and (2) is often producing output formats that are
+/// implicitly ASCII-only.
+///
+/// This discrepancy leads to misbehavior when trying to use the ctype.h
+/// functions as-is. For example, certain Windows environments with a signed
+/// `char` type crash when `isdigit` is called with a `char` that is part of a
+/// multi-byte unicode character and has its high bit set.
+///
+/// There are various solutions to this like using a full internationalization
+/// library or constructing an ASCII locale and calling the `*_l` variants. But
+/// for simplicity we just implement the exact discriminators we need.
+
+#pragma once
+
+#include <stdbool.h>
+
+static inline bool gv_isdigit(int c) { return c >= '0' && c <= '9'; }

lib/cgraph/write.c
@@ -15,6 +15,7 @@
 #include <stdio.h>		/* need sprintf() */
 #include <ctype.h>
 #include <cgraph/cghdr.h>
+#include <cgraph/gv_ctype.h>
 #include <cgraph/strcasecmp.h>
 #include <inttypes.h>
 
@@ -112,7 +113,7 @@ static char *_agstrcanon(char *arg, char *buf)
     p = buf;
     *p++ = '\"';
     uc = *s++;
-    maybe_num = isdigit(uc) != 0 || uc == '.' || uc == '-';
+    maybe_num = gv_isdigit(uc) || uc == '.' || uc == '-';
     while (uc) {
 	if (uc == '\"' && !part_of_escape) {
 	    *p++ = '\\';
@@ -133,7 +134,7 @@ static char *_agstrcanon(char *arg, char *buf)
 		    needs_quotes = true;
 		}
 	    }
-	    else if (!isdigit(uc)) {
+	    else if (!gv_isdigit(uc)) {
 		maybe_num = false;
 		needs_quotes = true;
 	    }