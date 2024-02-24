6f5e573d7f1846560fb6c2cf91a0ae8e6f369bd6
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-02-23 21:53:36 -0800

    common: remove use of 'putstr' global
    This is a step towards thread-safety in this code.

--- lib/common/output.c
+++ lib/common/output.c
@@ -28,46 +28,46 @@ double yDir (double y)
     return YDIR(y);
 }
 
-static int (*putstr) (void *chan, const char *str);
-
-static void agputs (const char* s, FILE* fp)
-{
+static void agputs(int (*putstr)(void *chan, const char *str), const char* s,
+                   FILE* fp) {
     putstr(fp, s);
 }
 
-static void agputc(char c, FILE *fp) {
+static void agputc(int (*putstr)(void *chan, const char *str), char c,
+                   FILE *fp) {
     static char buf[2] = {'\0','\0'};
     buf[0] = c;
     putstr(fp, buf);
 }
 
-static void printstring(FILE * f, char *prefix, char *s)
-{
-    if (prefix) agputs(prefix, f);
-    agputs(s, f);
+static void printstring(int (*putstr)(void *chan, const char *str), FILE *f,
+                        char *prefix, char *s) {
+  if (prefix) agputs(putstr, prefix, f);
+  agputs(putstr, s, f);
 }
 
-static void printint(FILE *f, char *prefix, size_t i) {
+static void printint(int (*putstr)(void *chan, const char *str), FILE *f,
+                     char *prefix, size_t i) {
     char buf[BUFSIZ];
     
-    if (prefix) agputs(prefix, f);
+  if (prefix) agputs(putstr, prefix, f);
     snprintf(buf, sizeof(buf), "%" PRISIZE_T, i);
-    agputs(buf, f);
+  agputs(putstr, buf, f);
 }
 
-static void printdouble(FILE * f, char *prefix, double v)
-{
+static void printdouble(int (*putstr)(void *chan, const char *str), FILE *f,
+                        char *prefix, double v) {
     char buf[BUFSIZ];
     
-    if (prefix) agputs(prefix, f);
+  if (prefix) agputs(putstr, prefix, f);
     snprintf(buf, sizeof(buf), "%.5g", v);
-    agputs(buf, f);
+  agputs(putstr, buf, f);
 }
 
-static void printpoint(FILE * f, pointf p)
-{
-    printdouble(f, " ", PS2INCH(p.x));
-    printdouble(f, " ", PS2INCH(YDIR(p.y)));
+static void printpoint(int (*putstr)(void *chan, const char *str), FILE *f,
+                       pointf p) {
+  printdouble(putstr, f, " ", PS2INCH(p.x));
+  printdouble(putstr, f, " ", PS2INCH(YDIR(p.y)));
 }
 
 /* setYInvert:
@@ -95,15 +95,16 @@ static char* canon (graph_t *g, char* s)
     return cs;
 }
 
-static void writenodeandport(FILE *f, node_t *node, char *portname) {
+static void writenodeandport(int (*putstr)(void *chan, const char *str),
+                             FILE *f, node_t *node, char *portname) {
     char *name;
     if (IS_CLUST_NODE(node))
 	name = canon (agraphof(node), strchr(agnameof(node), ':') + 1);
     else
 	name = agcanonStr (agnameof(node));
-    printstring(f, " ", name); /* slimey i know */
+    printstring(putstr, f, " ", name); /* slimey i know */
     if (portname && *portname)
-	printstring(f, ":", agcanonStr(portname));
+	printstring(putstr, f, ":", agcanonStr(portname));
 }
 
 void write_plain(GVJ_t *job, graph_t *g, FILE *f, bool extend) {
@@ -115,33 +116,33 @@ void write_plain(GVJ_t *job, graph_t *g, FILE *f, bool extend) {
     char *lbl;
     char* fillcolor;
 
-    putstr = g->clos->disc.io->putstr;
+    int (*putstr)(void *chan, const char *str) = g->clos->disc.io->putstr;
     setYInvert(g);
     pt = GD_bb(g).UR;
-    printdouble(f, "graph ", job->zoom);
-    printdouble(f, " ", PS2INCH(pt.x));
-    printdouble(f, " ", PS2INCH(pt.y));
-    agputc('\n', f);
+    printdouble(putstr, f, "graph ", job->zoom);
+    printdouble(putstr, f, " ", PS2INCH(pt.x));
+    printdouble(putstr, f, " ", PS2INCH(pt.y));
+    agputc(putstr, '\n', f);
     for (n = agfstnode(g); n; n = agnxtnode(g, n)) {
 	if (IS_CLUST_NODE(n))
 	    continue;
-	printstring(f, "node ", agcanonStr(agnameof(n)));
-	printpoint(f, ND_coord(n));
+	printstring(putstr, f, "node ", agcanonStr(agnameof(n)));
+	printpoint(putstr, f, ND_coord(n));
 	if (ND_label(n)->html)   /* if html, get original text */
 	    lbl = agcanonStr (agxget(n, N_label));
 	else
 	    lbl = canon(agraphof(n),ND_label(n)->text);
-        printdouble(f, " ", ND_width(n));
-        printdouble(f, " ", ND_height(n));
-        printstring(f, " ", lbl);
-	printstring(f, " ", late_nnstring(n, N_style, "solid"));
-	printstring(f, " ", ND_shape(n)->name);
-	printstring(f, " ", late_nnstring(n, N_color, DEFAULT_COLOR));
+        printdouble(putstr, f, " ", ND_width(n));
+        printdouble(putstr, f, " ", ND_height(n));
+        printstring(putstr, f, " ", lbl);
+	printstring(putstr, f, " ", late_nnstring(n, N_style, "solid"));
+	printstring(putstr, f, " ", ND_shape(n)->name);
+	printstring(putstr, f, " ", late_nnstring(n, N_color, DEFAULT_COLOR));
 	fillcolor = late_nnstring(n, N_fillcolor, "");
         if (fillcolor[0] == '\0')
 	    fillcolor = late_nnstring(n, N_color, DEFAULT_FILL);
-	printstring(f, " ", fillcolor);
-	agputc('\n', f);
+	printstring(putstr, f, " ", fillcolor);
+	agputc(putstr, '\n', f);
     }
     for (n = agfstnode(g); n; n = agnxtnode(g, n)) {
 	for (e = agfstout(g, n); e; e = agnxtout(g, e)) {
@@ -160,26 +161,26 @@ void write_plain(GVJ_t *job, graph_t *g, FILE *f, bool extend) {
 		    bz = ED_spl(e)->list[i];
 		    splinePoints += bz.size;
 		}
-		printstring(f, NULL, "edge");
-		writenodeandport(f, agtail(e), tport);
-		writenodeandport(f, aghead(e), hport);
-		printint(f, " ", splinePoints);
+		printstring(putstr, f, NULL, "edge");
+		writenodeandport(putstr, f, agtail(e), tport);
+		writenodeandport(putstr, f, aghead(e), hport);
+		printint(putstr, f, " ", splinePoints);
 		for (size_t i = 0; i < ED_spl(e)->size; i++) {
 		    bz = ED_spl(e)->list[i];
 		    for (size_t j = 0; j < bz.size; j++)
-			printpoint(f, bz.list[j]);
+			printpoint(putstr, f, bz.list[j]);
 		}
 	    }
 	    if (ED_label(e)) {
-		printstring(f, " ", canon(agraphof(agtail(e)),ED_label(e)->text));
-		printpoint(f, ED_label(e)->pos);
+		printstring(putstr, f, " ", canon(agraphof(agtail(e)),ED_label(e)->text));
+		printpoint(putstr, f, ED_label(e)->pos);
 	    }
-	    printstring(f, " ", late_nnstring(e, E_style, "solid"));
-	    printstring(f, " ", late_nnstring(e, E_color, DEFAULT_COLOR));
-	    agputc('\n', f);
+	    printstring(putstr, f, " ", late_nnstring(e, E_style, "solid"));
+	    printstring(putstr, f, " ", late_nnstring(e, E_color, DEFAULT_COLOR));
+	    agputc(putstr, '\n', f);
 	}
     }
-    agputs("stop\n", f);
+    agputs(putstr, "stop\n", f);
 }
 
 static void set_record_rects(node_t * n, field_t * f, agxbuf * xb)
