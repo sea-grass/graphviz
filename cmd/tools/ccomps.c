sea-grass/graphviz

commit 453d098db6a5ee9ea5cff99526538ad4ea57fde7

ccomps getName: remove long lived allocation
This code is not on a hot path, so we can afford to use clearer scoping and
ownership semantics.

Gitlab: #1950

cmd/tools/ccomps.c
@@ -23,6 +23,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <cgraph/agxbuf.h>
 #include <cgraph/alloc.h>
 #include <cgraph/cgraph.h>
 #include <cgraph/stack.h>
@@ -293,22 +294,18 @@ static int nodeInduce(Agraph_t * g, Agraph_t * eg)
 
 static char *getName(void)
 {
-    char *name;
-    static char *buf = 0;
+    agxbuf name = {0};
 
     if (sufcnt == 0)
-	name = outfile;
+	agxbput(&name, outfile);
     else {
-	if (!buf)
-	    buf = gv_alloc(strlen(outfile) + 20); // enough to handle '_number'
 	if (suffix)
-	    sprintf(buf, "%s_%d.%s", path, sufcnt, suffix);
+	    agxbprint(&name, "%s_%d.%s", path, sufcnt, suffix);
 	else
-	    sprintf(buf, "%s_%d", path, sufcnt);
-	name = buf;
+	    agxbprint(&name, "%s_%d", path, sufcnt);
     }
     sufcnt++;
-    return name;
+    return agxbuse(&name);
 }
 
 static void gwrite(Agraph_t * g)
@@ -325,8 +322,10 @@ static void gwrite(Agraph_t * g)
 	if (!outf) {
 	    fprintf(stderr, "Could not open %s for writing\n", name);
 	    perror("ccomps");
+	    free(name);
 	    graphviz_exit(EXIT_FAILURE);
 	}
+	free(name);
 	agwrite(g, outf);
 	fclose(outf);
     }

commit 1ee910c021c8ac2cefbc963c5d8ee1cf47fb0b52

ccomps: fix dangling pointer use
On Ubuntu 23.10, the compiler notices:

  ccomps.c: In function 'gwrite':
  ccomps.c:306:13: warning: dangling pointer to 'name' may be used [-Wdangling-pointer=]
    306 |             free(name);
        |             ^~~~~~~~~~
  ccomps.c:278:12: note: 'name' declared here
    278 |     agxbuf name = {0};
        |            ^~~~
  In file included from /usr/include/stdio.h:964,
                   from ccomps.c:23:
  In function 'fprintf',
      inlined from 'gwrite' at ccomps.c:304:6:
  /usr/include/x86_64-linux-gnu/bits/stdio2.h:79:10: warning: dangling pointer to 'name' may be used [-Wdangling-pointer=]
     79 |   return __fprintf_chk (__stream, __USE_FORTIFY_LEVEL - 1, __fmt,
        |          ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     80 |                         __va_arg_pack ());
        |                         ~~~~~~~~~~~~~~~~~
  ccomps.c: In function 'gwrite':
  ccomps.c:278:12: note: 'name' declared here
    278 |     agxbuf name = {0};
        |            ^~~~
  ccomps.c:309:9: warning: dangling pointer to 'name' may be used [-Wdangling-pointer=]
    309 |         free(name);
        |         ^~~~~~~~~~
  ccomps.c:278:12: note: 'name' declared here
    278 |     agxbuf name = {0};
        |            ^~~~
  ccomps.c:302:16: warning: dangling pointer to 'name' may be used [-Wdangling-pointer=]
    302 |         outf = fopen(name, "w");
        |                ^~~~~~~~~~~~~~~~
  ccomps.c:278:12: note: 'name' declared here
    278 |     agxbuf name = {0};
        |            ^~~~

The problem is that `getName` was returning a pointer to a buffer that is not
necessarily heap-allocated. The subsequent `free` calls would corrupt the heap
in this case. This bug was introduced in
453d098db6a5ee9ea5cff99526538ad4ea57fde7.

cmd/tools/ccomps.c
@@ -286,7 +286,7 @@ static char *getName(void)
 	    agxbprint(&name, "%s_%d", path, sufcnt);
     }
     sufcnt++;
-    return agxbuse(&name);
+    return agxbdisown(&name);
 }
 
 static void gwrite(Agraph_t * g)