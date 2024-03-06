2f42a518efb6d4818879d835e93afded971017e0
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-03-06 12:06:57 +1100

    neato layout plugin: clang-format gvplugin_neato_layout.c

--- plugin/neato_layout/gvplugin_neato_layout.c
+++ plugin/neato_layout/gvplugin_neato_layout.c
@@ -1,5 +1,5 @@
 /*************************************************************************
- * Copyright (c) 2011 AT&T Intellectual Property 
+ * Copyright (c) 2011 AT&T Intellectual Property
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -25,5 +25,5 @@ static gvplugin_api_t apis[] = {
 #define GVPLUGIN_NEATO_LAYOUT_API
 #endif
 
-GVPLUGIN_NEATO_LAYOUT_API gvplugin_library_t gvplugin_neato_layout_LTX_library = { "neato_layout", apis };
-
+GVPLUGIN_NEATO_LAYOUT_API gvplugin_library_t
+    gvplugin_neato_layout_LTX_library = {"neato_layout", apis};
