543dc02dc3c53fa39ea2efe38700a872fc3ed84e
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-03-06 12:06:57 +1100

    common isBox: return a C99 bool
    This makes the intent of this code slightly clearer.

--- lib/common/shapes.c
+++ lib/common/shapes.c
@@ -394,16 +394,13 @@ char *findFill(node_t * n)
     return findFillDflt(n, DEFAULT_FILL);
 }
 
-static int
-isBox (node_t* n)
-{
+static bool isBox(node_t *n) {
     polygon_t *p;
 
     if ((p = ND_shape(n)->polygon)) {
 	return p->sides == 4 && ROUND(p->orientation) % 90 == 0 && p->distortion == 0. && p->skew == 0.;
     }
-    else
-	return 0;
+    return false;
 }
 
 static int
