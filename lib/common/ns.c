ellson/MOTHBALLED-graphviz

commit 711b470ca8a17ee856fac529d309a340d33c1131

"Beware of bugs in the above code; I have only proved it correct, not tried it."--Donald Knuth

lib/common/ns.c
@@ -1,5 +1,6 @@
 /* $Id$ $Revision$ */
 /* vim:set shiftwidth=4 ts=8: */
+#define DEBUG 1
 
 /*************************************************************************
  * Copyright (c) 2011 AT&T Intellectual Property 
@@ -323,8 +324,16 @@ static subtree_t *STsetUnion(subtree_t *s0, subtree_t *s1)
 
   for (r0 = s0; r0->par && (r0->par != r0); r0 = r0->par);
   for (r1 = s1; r1->par && (r1->par != r1); r1 = r1->par);
-  if (r1->size <= r0->size) {r = r0; r1->par = r; r->size += r1->size;}
-  else {r = r1; r0->par = r; r->size += r0->size;}
+  if (r0 == r1) return r0;  /* safety code but shouldn't happen */
+  assert((r0->heap_index > -1) || (r1->heap_index > -1));
+  if (r1->heap_index == -1) r = r0;
+  else if (r0->heap_index == -1) r = r1;
+  else if (r1->size < r0->size) r = r0;
+  else r = r1;
+
+  r0->par = r1->par = r;
+  r->size = r0->size + r1->size;
+  assert(r->heap_index >= 0);
   return r;
 }
 
@@ -385,6 +394,7 @@ void STheapify(STheap_t *heap, int i)
         if ((left < heap->size) && (elt[left]->size < elt[i]->size)) smallest = left;
         else smallest = i;
         if ((right < heap->size) && (elt[right]->size < elt[smallest]->size)) smallest = right;
+        else smallest = i;
         if (smallest != i) {
             subtree_t *temp;
             temp = elt[i];
@@ -419,6 +429,7 @@ subtree_t *STextractmin(STheap_t *heap)
     rv = heap->elt[0];
     rv->heap_index = -1;
     heap->elt[0] = heap->elt[heap->size - 1];
+    heap->elt[0]->heap_index = 0;
     heap->elt[heap->size -1] = rv;    /* needed to free storage later */
     heap->size--;
     STheapify(heap,0);
@@ -457,7 +468,7 @@ subtree_t *merge_trees(Agedge_t *e)   /* entering tree edge */
 
   //fprintf(stderr,"merge trees of %d %d of %d, delta %d\n",t0->size,t1->size,N_nodes,delta);
 
-  if (t0->size < t1->size) {    // move t0
+  if (t0->heap_index == -1) {   // move t0
     delta = SLACK(e);
     tree_adjust(t0->rep,(Agnode_t*)0,delta);
   }
@@ -467,6 +478,7 @@ subtree_t *merge_trees(Agedge_t *e)   /* entering tree edge */
   }
   add_tree_edge(e);
   rv = STsetUnion(t0,t1);
+  
   return rv;
 }
 
@@ -504,7 +516,6 @@ int feasible_tree(void)
     ee = inter_tree_edge(tree0);
     tree1 = merge_trees(ee);
     STheapify(heap,tree1->heap_index);
-    //free(tree0);
   }
 
   free(heap);

sea-grass/graphviz

commit 1fc228eedbfcb4a969264ba9e4a9cc269cf56c1e

common STheapify: fix incorrect construction of heap
This bug was introduced in 711b470ca8a17ee856fac529d309a340d33c1131 and causes
certain inputs to not result in a correctly constructed heap. For example,
[10, 9, 13, 15, 17, 99, 3, 12, 21] is incorrectly “heapified” into
[3, 21, 12, 13, 99, 17, 15, 10, 9].

It is difficult to test internal code like this, so instead I extracted its
logic into a separate program.¹ Either Kimon’s fix or my own are sufficient to
address this problem, but I have committed my own here which I believe is an
improved simplification.

Gitlab: fixes #2391, fixes #2529
Reported-by: icktoofay
Reported-by: Kimon Papahadjopoulos

¹ The C program that follows:

    #include <assert.h>
    #include <stddef.h>
    #include <stdio.h>

    static void heapify(int *heap, int size, int i) {
      int left, right, smallest;
      do {
        left = 2 * (i + 1) - 1;
        right = 2 * (i + 1);
    #ifdef ORIGINAL
        if (left < size && heap[left] < heap[i])
          smallest = left;
        else
          smallest = i;
        if (right < size && heap[right] < heap[smallest])
          smallest = right;
        else
          smallest = i;
    #elif defined(KIMON_FIX)
        if (left < size && heap[left] < heap[i])
          smallest = left;
        else
          smallest = i;
        if (right < size && heap[right] < heap[smallest])
          smallest = right;
    #elif defined(MATT_FIX)
        smallest = i;
        if (left < size && heap[left] < heap[smallest])
          smallest = left;
        if (right < size && heap[right] < heap[smallest])
          smallest = right;
    #else
    #error "define one of ORIGINAL, KIMON_FIX, or MATT_FIX"
    #endif
        if (smallest != i) {
          int temp = heap[i];
          heap[i] = heap[smallest];
          heap[smallest] = temp;
          i = smallest;
        } else
          break;
      } while (i < size);
    }

    static void buildheap(int *heap, int size) {

lib/common/ns.c
@@ -430,10 +430,9 @@ void STheapify(STheap_t *heap, int i)
     do {
         left = 2*(i+1)-1;
         right = 2*(i+1);
-        if (left < heap->size && elt[left]->size < elt[i]->size) smallest = left;
-        else smallest = i;
+        smallest = i;
+        if (left < heap->size && elt[left]->size < elt[smallest]->size) smallest = left;
         if (right < heap->size && elt[right]->size < elt[smallest]->size) smallest = right;
-        else smallest = i;
         if (smallest != i) {
             subtree_t *temp;
             temp = elt[i];

tests/test_regression.py
@@ -3166,7 +3166,6 @@ def test_2390():
     dot("xdot", source=input)
 
 
-@pytest.mark.xfail(strict=True)  # FIXME
 def test_2391():
     """
     `nslimit1=0` should not cause Graphviz to crash
@@ -3181,7 +3180,6 @@ def test_2391():
     dot("svg", input)
 
 
-@pytest.mark.xfail(strict=True)  # FIXME
 def test_2391_1():
     """
     `nslimit1=0` with a label should not cause Graphviz to crash