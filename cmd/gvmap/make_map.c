d447eb971933ec30b6ab4bbed6a064ee41a9d76e
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-05-05 07:38:28 +1000

    gvmap make_map_internal: abbreviate 'area2' initialization

--- cmd/gvmap/make_map.c
+++ cmd/gvmap/make_map.c
@@ -1059,8 +1059,7 @@ static void make_map_internal(bool include_OK_points,
 	        fmax(boxsize[0] * 0.2, 2 * shore_depth_tol));
     }
     if (nrandom < 0) {
-      double area2;
-      area2 = (xmax[1] - xmin[1])*(xmax[0] - xmin[0]);
+      const double area2 = (xmax[1] - xmin[1]) * (xmax[0] - xmin[0]);
       const double n1 = floor(area2 / (shore_depth_tol * shore_depth_tol));
       const double n2 = n * floor(area2 / area);
       nrandom = fmax(n1, n2);
