77acff349b471fcd7052172c2c55817c49cb9328
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-05-05 16:43:42 +1000

    common star_size: replace 'MAX' with standard 'fmax'

--- lib/common/shapes.c
+++ lib/common/shapes.c
@@ -4047,11 +4047,11 @@ static void epsf_gencode(GVJ_t * job, node_t * n)
 static pointf star_size (pointf sz0)
 {
     pointf sz;
-    double r0, r, rx, ry;
+    double r, rx, ry;
 
     rx = sz0.x/(2*cos(alpha));
     ry = sz0.y/(sin(alpha) + sin(alpha3));
-    r0 = MAX(rx,ry);
+    const double r0 = fmax(rx, ry);
     r = r0 * sin(alpha4) * cos(alpha2) / (cos(alpha) * cos(alpha4));
 
     sz.x = 2*r*cos(alpha);
