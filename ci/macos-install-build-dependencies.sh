df5d64133b399fcaeccefac891089cfe1441cfe0
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-05-08 18:55:28 +1000

    CI: install DevIL on macOS, enabling Graphviz DevIL plugin

--- ci/macos-install-build-dependencies.sh
+++ ci/macos-install-build-dependencies.sh
@@ -19,7 +19,7 @@ brew install autogen || brew upgrade autogen
 brew install automake || brew upgrade automake
 brew install cmake || brew upgrade cmake
 brew install bison || brew upgrade bison
-
+brew install devil || brew upgrade devil
 brew install gtk+ || brew upgrade gtk+
 brew install gts || brew upgrade gts
 
