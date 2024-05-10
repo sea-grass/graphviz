086fe586b424b538899a01573b5ec5fd76722987
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-05-10 17:02:19 +1000

    smyrna: squash -Wmissing-prototypes warnings

--- cmd/smyrna/gui/frmobjectui.h
+++ cmd/smyrna/gui/frmobjectui.h
@@ -19,6 +19,8 @@ _BB void on_txtAttr_changed(GtkWidget * widget, gpointer user_data);
 _BB void on_attrApplyBtn_clicked (GtkWidget * widget, gpointer user_data);
 _BB void on_attrAddBtn_clicked (GtkWidget * widget, gpointer user_data);
 _BB void on_attrSearchBtn_clicked (GtkWidget * widget, gpointer user_data);
+_BB void on_attrRB0_clicked(GtkWidget *widget, gpointer user_data);
+_BB void on_attrProg_toggled(GtkWidget *widget, gpointer user_data);
 
 extern attr_list* load_attr_list(Agraph_t* g);
 extern void showAttrsWidget(void);
