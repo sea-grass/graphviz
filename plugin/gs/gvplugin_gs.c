46f20008a72eebc45fe544b821e947776077ca1c
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-06-08 17:20:03 +1000

    fix missing 'dllexport' annotations on GS, Kitty, VT plugins
    Gitlab: fixes #2548
    Reported-by: Silvan Scherrer

--- plugin/gs/gvplugin_gs.c
+++ plugin/gs/gvplugin_gs.c
@@ -17,4 +17,10 @@ static gvplugin_api_t apis[] = {
     {(api_t)0, 0},
 };
 
-gvplugin_library_t gvplugin_gs_LTX_library = { "gs", apis };
+#ifdef GVDLL
+#define GVPLUGIN_GS_API __declspec(dllexport)
+#else
+#define GVPLUGIN_GS_API
+#endif
+
+GVPLUGIN_GS_API gvplugin_library_t gvplugin_gs_LTX_library = {"gs", apis};
