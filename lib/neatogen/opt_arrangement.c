sea-grass/graphviz

commit 3d72a3e5908340a3459bf4bb569aae7486721dee

neatogen: use 'size_t' for 'vtx_data.nedges'
More progress towards using `size_t` for counting elements.

lib/neatogen/opt_arrangement.c
@@ -13,6 +13,7 @@
 #ifdef DIGCOLA
 #include <neatogen/matrix_ops.h>
 #include <neatogen/conjgrad.h>
+#include <stddef.h>
 
 static void construct_b(vtx_data * graph, int n, double *b)
 {
@@ -22,7 +23,7 @@ static void construct_b(vtx_data * graph, int n, double *b)
      * real laplacian L, but its negation: -L. 
      * So instead of solving Lx=b, we will solve -Lx=-b
      */
-    int i, j;
+    int i;
 
     double b_i = 0;
 
@@ -31,7 +32,7 @@ static void construct_b(vtx_data * graph, int n, double *b)
 	if (graph[0].edists == NULL) {
 	    continue;
 	}
-	for (j = 1; j < graph[i].nedges; j++) {	/* skip the self loop */
+	for (size_t j = 1; j < graph[i].nedges; j++) { // skip the self loop
 	    b_i += graph[i].ewgts[j] * graph[i].edists[j];
 	}
 	b[i] = b_i;
@@ -45,10 +46,10 @@ compute_y_coords(vtx_data * graph, int n, double *y_coords,
 		 int max_iterations)
 {
     /* Find y coords of a directed graph by solving L*x = b */
-    int i, j, rv = 0;
+    int i, rv = 0;
     double *b = gv_calloc(n, sizeof(double));
     double tol = hierarchy_cg_tol;
-    int nedges = 0;
+    size_t nedges = 0;
     float *old_ewgts = graph[0].ewgts;
 
     construct_b(graph, n, b);
@@ -65,7 +66,7 @@ compute_y_coords(vtx_data * graph, int n, double *y_coords,
     for (i = 0; i < n; i++) {
 	graph[i].ewgts = uniform_weights;
 	uniform_weights[0] = (float) -(graph[i].nedges - 1);
-	for (j = 1; j < graph[i].nedges; j++) {
+	for (size_t j = 1; j < graph[i].nedges; j++) {
 	    uniform_weights[j] = 1;
 	}
 	uniform_weights += graph[i].nedges;

lib/neatogen/bfs.c
@@ -25,12 +25,11 @@
 void bfs(int vertex, vtx_data *graph, int n, DistType *dist)
  /* compute vector 'dist' of distances of all nodes from 'vertex' */
 {
-    int i;
     int closestVertex, neighbor;
     DistType closestDist = INT_MAX;
 
     /* initial distances with edge weights: */
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
 	dist[i] = -1;
     dist[vertex] = 0;
 
@@ -41,7 +40,7 @@ void bfs(int vertex, vtx_data *graph, int n, DistType *dist)
     if (graph[0].ewgts == NULL) {
 	while (deQueue(&Q, &closestVertex)) {
 	    closestDist = dist[closestVertex];
-	    for (i = 1; i < graph[closestVertex].nedges; i++) {
+	    for (size_t i = 1; i < graph[closestVertex].nedges; i++) {
 		neighbor = graph[closestVertex].edges[i];
 		if (dist[neighbor] < -0.5) {	/* first time to reach neighbor */
 		    dist[neighbor] = closestDist + 1;
@@ -52,7 +51,7 @@ void bfs(int vertex, vtx_data *graph, int n, DistType *dist)
     } else {
 	while (deQueue(&Q, &closestVertex)) {
 	    closestDist = dist[closestVertex];
-	    for (i = 1; i < graph[closestVertex].nedges; i++) {
+	    for (size_t i = 1; i < graph[closestVertex].nedges; i++) {
 		neighbor = graph[closestVertex].edges[i];
 		if (dist[neighbor] < -0.5) {	/* first time to reach neighbor */
 		    dist[neighbor] =
@@ -65,7 +64,7 @@ void bfs(int vertex, vtx_data *graph, int n, DistType *dist)
     }
 
     /* For dealing with disconnected graphs: */
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
 	if (dist[i] < -0.5)	/* 'i' is not connected to 'vertex' */
 	    dist[i] = closestDist + 10;
 

lib/neatogen/closest.c
@@ -245,8 +245,7 @@ static void find_closest_pairs(double *place, size_t n, int num_pairs,
 
 static void add_edge(vtx_data * graph, int u, int v)
 {
-    int i;
-    for (i = 0; i < graph[u].nedges; i++) {
+    for (size_t i = 0; i < graph[u].nedges; i++) {
 	if (graph[u].edges[i] == v) {
 	    /* edge already exist */
 	    return;

lib/neatogen/constrained_majorization.c
@@ -47,7 +47,7 @@ int stress_majorization_with_hierarchy(vtx_data * graph,	/* Input graph in spars
 	** This function imposes HIERARCHY CONSTRAINTS  **
 	*************************************************/
 
-    int i, j, k;
+    int i, k;
     bool directionalityExist = false;
     float *lap1 = NULL;
     float *dist_accumulator = NULL;
@@ -82,7 +82,7 @@ int stress_majorization_with_hierarchy(vtx_data * graph,	/* Input graph in spars
 
     if (graph[0].edists != NULL) {
 	for (i = 0; i < n; i++) {
-	    for (j = 1; j < graph[i].nedges; j++) {
+	    for (size_t j = 1; j < graph[i].nedges; j++) {
 		directionalityExist |= graph[i].edists[j] != 0;
 	    }
 	}
@@ -142,7 +142,7 @@ int stress_majorization_with_hierarchy(vtx_data * graph,	/* Input graph in spars
 				      displacement -
 				      y[ordering[levels[i] - 1]]));
 		stop = i < num_levels - 1 ? levels[i + 1] : n;
-		for (j = levels[i]; j < stop; j++) {
+		for (int j = levels[i]; j < stop; j++) {
 		    y[ordering[j]] += displacement;
 		}
 	    }
@@ -221,12 +221,12 @@ int stress_majorization_with_hierarchy(vtx_data * graph,	/* Input graph in spars
 	/* No Jiggling, might conflict with constraints                  */
 	double max = 1;
 	for (i = 0; i < dim; i++) {
-	    for (j = 0; j < n; j++) {
+	    for (int j = 0; j < n; j++) {
 		max = fmax(max, fabs(d_coords[i][j]));
 	    }
 	}
 	for (i = 0; i < dim; i++) {
-	    for (j = 0; j < n; j++) {
+	    for (int j = 0; j < n; j++) {
 		d_coords[i][j] *= 10 / max;
 	    }
 	}
@@ -239,7 +239,7 @@ int stress_majorization_with_hierarchy(vtx_data * graph,	/* Input graph in spars
 	sum2 = 0;
 	for (count = 0, i = 0; i < n - 1; i++) {
 	    count++;		// skip self distance
-	    for (j = i + 1; j < n; j++, count++) {
+	    for (int j = i + 1; j < n; j++, count++) {
 		sum2 += distance_kD(d_coords, dim, i, j) / Dij[count];
 	    }
 	}
@@ -268,7 +268,7 @@ int stress_majorization_with_hierarchy(vtx_data * graph,	/* Input graph in spars
     f_storage = gv_calloc(dim * n, sizeof(float));
     for (i = 0; i < dim; i++) {
 	coords[i] = f_storage + i * n;
-	for (j = 0; j < n; j++) {
+	for (int j = 0; j < n; j++) {
 	    coords[i][j] = (float)d_coords[i][j];
 	}
     }
@@ -298,7 +298,7 @@ int stress_majorization_with_hierarchy(vtx_data * graph,	/* Input graph in spars
     for (i = 0; i < n - 1; i++) {
 	degree = 0;
 	count++;		// skip main diag entry
-	for (j = 1; j < n - i; j++, count++) {
+	for (int j = 1; j < n - i; j++, count++) {
 	    val = lap2[count];
 	    degree += val;
 	    degrees[i + j] -= val;
@@ -352,7 +352,7 @@ int stress_majorization_with_hierarchy(vtx_data * graph,	/* Input graph in spars
 	    /* convert to 1/d_{ij} */
 	    invert_sqrt_vec(len, dist_accumulator);
 	    /* detect overflows */
-	    for (j = 0; j < len; j++) {
+	    for (int j = 0; j < len; j++) {
 		if (dist_accumulator[j] >= FLT_MAX || dist_accumulator[j] < 0) {
 		    dist_accumulator[j] = 0;
 		}
@@ -360,7 +360,7 @@ int stress_majorization_with_hierarchy(vtx_data * graph,	/* Input graph in spars
 
 	    count++;		/* save place for the main diagonal entry */
 	    degree = 0;
-	    for (j = 0; j < len; j++, count++) {
+	    for (int j = 0; j < len; j++, count++) {
 		val = lap1[count] *= dist_accumulator[j];
 		degree += val;
 		degrees[i + j + 1] -= val;
@@ -435,7 +435,7 @@ int stress_majorization_with_hierarchy(vtx_data * graph,	/* Input graph in spars
 
     if (coords != NULL) {
 	for (i = 0; i < dim; i++) {
-	    for (j = 0; j < n; j++) {
+	    for (int j = 0; j < n; j++) {
 		d_coords[i][j] = coords[i][j];
 	    }
 	}

lib/neatogen/constrained_majorization_ipsep.c
@@ -70,7 +70,7 @@ int stress_majorization_cola(vtx_data * graph,	/* Input graph in sparse represen
 	** This function imposes HIERARCHY CONSTRAINTS  **
 	*************************************************/
 
-    int i, j, k;
+    int i, k;
     float *lap1 = NULL;
     float *dist_accumulator = NULL;
     float *tmp_coords = NULL;
@@ -105,7 +105,7 @@ int stress_majorization_cola(vtx_data * graph,	/* Input graph in sparse represen
 	return 0;
 
     for (i = 0; i < n; i++) {
-	for (j = 1; j < graph[i].nedges; j++) {
+	for (size_t j = 1; j < graph[i].nedges; j++) {
 	    maxEdgeLen = MAX(graph[i].ewgts[j], maxEdgeLen);
 	}
     }
@@ -161,14 +161,14 @@ int stress_majorization_cola(vtx_data * graph,	/* Input graph in sparse represen
     /* for numerical stability, scale down layout                */
     /* No Jiggling, might conflict with constraints                      */
     for (i = 0; i < dim; i++) {
-	for (j = 0; j < n; j++) {
+	for (int j = 0; j < n; j++) {
 	    if (fabs(d_coords[i][j]) > max) {
 		max = fabs(d_coords[i][j]);
 	    }
 	}
     }
     for (i = 0; i < dim; i++) {
-	for (j = 0; j < n; j++) {
+	for (int j = 0; j < n; j++) {
 	    d_coords[i][j] *= 10 / max;
 	}
     }
@@ -207,7 +207,7 @@ int stress_majorization_cola(vtx_data * graph,	/* Input graph in sparse represen
 	float v;
 	c0 = c1 = 0;
 	for (i = 0; i < nn; i++) {
-	    for (j = 0; j < nn - i; j++) {
+	    for (int j = 0; j < nn - i; j++) {
 		if (i < n && j < n - i) {
 		    v = lap2[c0++];
 		} else {
@@ -236,7 +236,7 @@ int stress_majorization_cola(vtx_data * graph,	/* Input graph in sparse represen
     for (i = 0; i < n - 1; i++) {
 	degree = 0;
 	count++;		/* skip main diag entry */
-	for (j = 1; j < n - i; j++, count++) {
+	for (int j = 1; j < n - i; j++, count++) {
 	    val = lap2[count];
 	    degree += val;
 	    degrees[i + j] -= val;
@@ -251,7 +251,7 @@ int stress_majorization_cola(vtx_data * graph,	/* Input graph in sparse represen
     f_storage = gv_calloc(dim * n, sizeof(float));
     for (i = 0; i < dim; i++) {
 	coords[i] = f_storage + i * n;
-	for (j = 0; j < n; j++) {
+	for (int j = 0; j < n; j++) {
 	    coords[i][j] = j < orig_n ? (float)d_coords[i][j] : 0;
 	}
     }
@@ -311,7 +311,7 @@ int stress_majorization_cola(vtx_data * graph,	/* Input graph in sparse represen
 	    /* convert to 1/d_{ij} */
 	    invert_sqrt_vec(len, dist_accumulator);
 	    /* detect overflows */
-	    for (j = 0; j < len; j++) {
+	    for (int j = 0; j < len; j++) {
 		if (dist_accumulator[j] >= FLT_MAX || dist_accumulator[j] < 0) {
 		    dist_accumulator[j] = 0;
 		}
@@ -319,7 +319,7 @@ int stress_majorization_cola(vtx_data * graph,	/* Input graph in sparse represen
 
 	    count++;		/* save place for the main diagonal entry */
 	    degree = 0;
-	    for (j = 0; j < len; j++, count++) {
+	    for (int j = 0; j < len; j++, count++) {
 		val = lap1[count] *= dist_accumulator[j];
 		degree += val;
 		degrees[i + j + 1] -= val;
@@ -441,7 +441,7 @@ int stress_majorization_cola(vtx_data * graph,	/* Input graph in sparse represen
 finish:
     if (coords != NULL) {
 	for (i = 0; i < dim; i++) {
-	    for (j = 0; j < orig_n; j++) {
+	    for (int j = 0; j < orig_n; j++) {
 		d_coords[i][j] = coords[i][j];
 	    }
 	}

lib/neatogen/dijkstra.c
@@ -140,7 +140,6 @@ increaseKey(heap * h, int increasedVertex, Word newDist, int index[],
 
 void dijkstra(int vertex, vtx_data * graph, int n, DistType * dist)
 {
-    int i;
     heap H;
     int closestVertex, neighbor;
     DistType closestDist, prevClosestDist = MAX_DIST;
@@ -148,10 +147,10 @@ void dijkstra(int vertex, vtx_data * graph, int n, DistType * dist)
     int *index = gv_calloc(n, sizeof(int));
 
     /* initial distances with edge weights: */
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
 	dist[i] = MAX_DIST;
     dist[vertex] = 0;
-    for (i = 1; i < graph[vertex].nedges; i++)
+    for (size_t i = 1; i < graph[vertex].nedges; i++)
 	dist[graph[vertex].edges[i]] = (DistType) graph[vertex].ewgts[i];
 
     initHeap(&H, vertex, index, dist, n);
@@ -160,7 +159,7 @@ void dijkstra(int vertex, vtx_data * graph, int n, DistType * dist)
 	closestDist = dist[closestVertex];
 	if (closestDist == MAX_DIST)
 	    break;
-	for (i = 1; i < graph[closestVertex].nedges; i++) {
+	for (size_t i = 1; i < graph[closestVertex].nedges; i++) {
 	    neighbor = graph[closestVertex].edges[i];
 	    increaseKey(&H, neighbor, closestDist +
 			(DistType)graph[closestVertex].ewgts[i], index, dist);
@@ -169,7 +168,7 @@ void dijkstra(int vertex, vtx_data * graph, int n, DistType * dist)
     }
 
     /* For dealing with disconnected graphs: */
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
 	if (dist[i] == MAX_DIST)	/* 'i' is not connected to 'vertex' */
 	    dist[i] = prevClosestDist + 10;
     freeHeap(&H);
@@ -259,17 +258,16 @@ increaseKey_f(heap * h, int increasedVertex, float newDist, int index[],
  */
 void dijkstra_f(int vertex, vtx_data * graph, int n, float *dist)
 {
-    int i;
     heap H;
     int closestVertex = 0, neighbor;
     float closestDist;
     int *index = gv_calloc(n, sizeof(int));
 
     /* initial distances with edge weights: */
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
 	dist[i] = FLT_MAX;
     dist[vertex] = 0;
-    for (i = 1; i < graph[vertex].nedges; i++)
+    for (size_t i = 1; i < graph[vertex].nedges; i++)
 	dist[graph[vertex].edges[i]] = graph[vertex].ewgts[i];
 
     initHeap_f(&H, vertex, index, dist, n);
@@ -278,7 +276,7 @@ void dijkstra_f(int vertex, vtx_data * graph, int n, float *dist)
 	closestDist = dist[closestVertex];
 	if (closestDist == FLT_MAX)
 	    break;
-	for (i = 1; i < graph[closestVertex].nedges; i++) {
+	for (size_t i = 1; i < graph[closestVertex].nedges; i++) {
 	    neighbor = graph[closestVertex].edges[i];
 	    increaseKey_f(&H, neighbor, closestDist + graph[closestVertex].ewgts[i],
 			  index, dist);

lib/neatogen/kkutils.c
@@ -16,13 +16,11 @@
 #include <stdlib.h>
 #include <math.h>
 
-int common_neighbors(vtx_data * graph, int u, int *v_vector)
-{
+size_t common_neighbors(vtx_data *graph, int u, int *v_vector) {
     // count number of common neighbors of 'v_vector' and 'u'
     int neighbor;
-    int num_shared_neighbors = 0;
-    int j;
-    for (j = 1; j < graph[u].nedges; j++) {
+    size_t num_shared_neighbors = 0;
+    for (size_t j = 1; j < graph[u].nedges; j++) {
 	neighbor = graph[u].edges[j];
 	if (v_vector[neighbor] > 0) {
 	    // a shared neighbor
@@ -35,18 +33,16 @@ void fill_neighbors_vec_unweighted(vtx_data * graph, int vtx, int *vtx_vec)
 {
     /* a node is NOT a neighbor of itself! */
     /* unlike the other version of this function */
-    int j;
-    for (j = 1; j < graph[vtx].nedges; j++) {
+    for (size_t j = 1; j < graph[vtx].nedges; j++) {
 	vtx_vec[graph[vtx].edges[j]] = 1;
     }
 }
 
 void empty_neighbors_vec(vtx_data * graph, int vtx, int *vtx_vec)
 {
-    int j;
     /* a node is NOT a neighbor of itself! */
     /* unlike the other version ofthis function */
-    for (j = 1; j < graph[vtx].nedges; j++) {
+    for (size_t j = 1; j < graph[vtx].nedges; j++) {
 	vtx_vec[graph[vtx].edges[j]] = 0;
     }
 }
@@ -170,10 +166,11 @@ void compute_new_weights(vtx_data * graph, int n)
 {
     /* Reweight graph so that high degree nodes will be separated */
 
-    int i, j;
-    int nedges = 0;
+    int i;
+    size_t nedges = 0;
     int *vtx_vec = gv_calloc(n, sizeof(int));
-    int deg_i, deg_j, neighbor;
+    size_t deg_i, deg_j;
+    int neighbor;
 
     for (i = 0; i < n; i++) {
 	nedges += graph[i].nedges;
@@ -184,7 +181,7 @@ void compute_new_weights(vtx_data * graph, int n)
 	graph[i].ewgts = weights;
 	fill_neighbors_vec_unweighted(graph, i, vtx_vec);
 	deg_i = graph[i].nedges - 1;
-	for (j = 1; j <= deg_i; j++) {
+	for (size_t j = 1; j <= deg_i; j++) {
 	    neighbor = graph[i].edges[j];
 	    deg_j = graph[neighbor].nedges - 1;
 	    weights[j] =

lib/neatogen/kkutils.h
@@ -10,6 +10,8 @@
 
 #pragma once
 
+#include <stddef.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -18,7 +20,7 @@ extern "C" {
 
     extern void fill_neighbors_vec_unweighted(vtx_data *, int vtx,
 					      int *vtx_vec);
-    extern int common_neighbors(vtx_data *, int u, int *);
+    extern size_t common_neighbors(vtx_data *, int u, int *);
     extern void empty_neighbors_vec(vtx_data * graph, int vtx,
 				    int *vtx_vec);
     extern DistType **compute_apsp(vtx_data *, int);

commit fd68eaf2e01c20fcdeea8c8761147dd374101fd8

neatogen compute_y_coords: fix incorrect sign calculation
3d72a3e5908340a3459bf4bb569aae7486721dee inadvertently introduced a bug here by
making `nedges` unsigned. MSVC notes:

  graphviz\lib\neatogen\opt_arrangement.c(68,31): warning C4146: unary minus
    operator applied to unsigned type, result still unsigned

lib/neatogen/opt_arrangement.c
@@ -65,7 +65,7 @@ compute_y_coords(vtx_data * graph, int n, double *y_coords,
     float *uniform_weights = gv_calloc(nedges, sizeof(float));
     for (i = 0; i < n; i++) {
 	graph[i].ewgts = uniform_weights;
-	uniform_weights[0] = (float) -(graph[i].nedges - 1);
+	uniform_weights[0] = -(float)(graph[i].nedges - 1);
 	for (size_t j = 1; j < graph[i].nedges; j++) {
 	    uniform_weights[j] = 1;
 	}