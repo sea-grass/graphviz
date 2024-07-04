e8f6d8a8fb1b8316723b68a052e072269c3355b8
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2024-07-04 22:50:09 +0200

    CMake: split discovery of Cairo for native Windows and MinGW
    This makes it clear which paths are valid for each platform and
    facilitates making platform specific changes in upcoming commits.
    
    This is currently a lot of duplication, but upcoming commits that will
    bring pkgconf into use also on these platforms, will reduce this
    substantially. The native Windows Cairo library must however first be
    updated to a version that supports pkgconf.
    
    GitLab: #2557

--- cmake/FindCAIRO.cmake
+++ cmake/FindCAIRO.cmake
@@ -1,6 +1,6 @@
 include(FindPackageHandleStandardArgs)
 
-if(WIN32)
+if(MINGW)
   find_path(
     CAIRO_INCLUDE_DIR cairo.h
     PATH_SUFFIXES cairo
@@ -8,11 +8,46 @@ if(WIN32)
 
   find_library(CAIRO_LIBRARY NAMES cairo)
 
-  find_program(CAIRO_RUNTIME_LIBRARY NAMES cairo.dll libcairo-2.dll)
-  find_program(EXPAT_RUNTIME_LIBRARY NAMES expat.dll libexpat-1.dll)
-  find_program(FONTCONFIG_RUNTIME_LIBRARY
-               NAMES fontconfig.dll libfontconfig-1.dll)
-  find_program(PIXMAN_RUNTIME_LIBRARY NAMES pixman-1.dll libpixman-1-0.dll)
+  find_program(CAIRO_RUNTIME_LIBRARY NAMES libcairo-2.dll)
+  find_program(EXPAT_RUNTIME_LIBRARY NAMES libexpat-1.dll)
+  find_program(FONTCONFIG_RUNTIME_LIBRARY NAMES libfontconfig-1.dll)
+  find_program(PIXMAN_RUNTIME_LIBRARY NAMES libpixman-1-0.dll)
+
+  find_package_handle_standard_args(CAIRO DEFAULT_MSG
+    CAIRO_INCLUDE_DIR
+
+    CAIRO_LIBRARY
+
+    CAIRO_RUNTIME_LIBRARY
+    EXPAT_RUNTIME_LIBRARY
+    FONTCONFIG_RUNTIME_LIBRARY
+    PIXMAN_RUNTIME_LIBRARY
+  )
+
+  set(CAIRO_INCLUDE_DIRS ${CAIRO_INCLUDE_DIR})
+
+  set(CAIRO_LIBRARIES ${CAIRO_LIBRARY})
+
+  set(CAIRO_LINK_LIBRARIES ${CAIRO_LIBRARY})
+
+  set(CAIRO_RUNTIME_LIBRARIES
+    ${CAIRO_RUNTIME_LIBRARY}
+    ${EXPAT_RUNTIME_LIBRARY}
+    ${FONTCONFIG_RUNTIME_LIBRARY}
+    ${PIXMAN_RUNTIME_LIBRARY}
+  )
+elseif(WIN32)
+  find_path(
+    CAIRO_INCLUDE_DIR cairo.h
+    PATH_SUFFIXES cairo
+  )
+
+  find_library(CAIRO_LIBRARY NAMES cairo)
+
+  find_program(CAIRO_RUNTIME_LIBRARY NAMES cairo.dll)
+  find_program(EXPAT_RUNTIME_LIBRARY NAMES expat.dll)
+  find_program(FONTCONFIG_RUNTIME_LIBRARY NAMES fontconfig.dll)
+  find_program(PIXMAN_RUNTIME_LIBRARY NAMES pixman-1.dll)
 
   find_package_handle_standard_args(CAIRO DEFAULT_MSG
     CAIRO_INCLUDE_DIR
