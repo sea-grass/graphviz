3591c1c03286e3796f2e4f1133ac1841d99f4315
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2024-07-04 20:01:12 +0200

    CMake: factor out discovery of Glib for native Windows in PangoCairo discovery
    GitLab: #2557

--- cmake/FindGLIB.cmake
+++ cmake/FindGLIB.cmake
@@ -14,4 +14,33 @@ if(MINGW)
   set(GLIB_RUNTIME_LIBRARIES
     ${GLIB_RUNTIME_LIBRARY}
   )
+elseif(WIN32)
+  find_path(GLIB_INCLUDE_DIR glib.h PATH_SUFFIXES glib-2.0)
+
+  find_library(GLIB_LIBRARY NAMES glib-2.0)
+
+  find_program(GLIB_RUNTIME_LIBRARY NAMES glib-2.dll)
+
+  find_package_handle_standard_args(GLIB DEFAULT_MSG
+    GLIB_INCLUDE_DIR
+    GLIB_LIBRARY
+    GLIB_LINK_LIBRARY
+    GLIB_RUNTIME_LIBRARY
+  )
+
+  set(GLIB_INCLUDE_DIRS
+    ${GLIB_INCLUDE_DIR}
+  )
+
+  set(GLIB_LIBRARIES
+    glib-2.0
+  )
+
+  set(GLIB_LINK_LIBRARIES
+    ${GLIB_LIBRARY}
+  )
+
+  set(GLIB_RUNTIME_LIBRARIES
+    ${GLIB_RUNTIME_LIBRARY}
+  )
 endif()
