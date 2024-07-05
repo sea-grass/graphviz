ad49f9fa4a3020e713081f1e7711d52e72c68ce5
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2024-07-05 20:51:09 +0200

    CMake: split discovery of GTS for native Windows and MinGW
    This makes it clear which paths are valid for each platform and
    facilitates making platform specific changes in upcoming commits.
    
    This is currently a lot of duplication, but upcoming commits that will
    bring pkgconf into use also on these platforms, will reduce this
    substantially. A GTS library that supports pkgconf must however first be
    installed for native Windows. We currently don't have any version of GTS
    installed in `windows/dependencies/libraries`, but the version that is
    possible to discover with the current dicovery mechanism presumably
    don't support pkgconf. We will therefore keep the current discovery
    until we upgrade.
    
    GitLab: #2557

--- cmake/FindGTS.cmake
+++ cmake/FindGTS.cmake
@@ -1,14 +1,51 @@
 include(FindPackageHandleStandardArgs)
-if(WIN32)
+if(MINGW)
   find_package(GLIB)
 
   find_path(GTS_INCLUDE_DIR gts.h)
   find_path(GLIBCONFIG_INCLUDE_DIR glibconfig.h
-            PATH_SUFFIXES glib-2.0/include lib/glib-2.0/include)
+            PATH_SUFFIXES lib/glib-2.0/include)
 
   find_library(GTS_LIBRARY NAMES gts)
 
-  find_program(GTS_RUNTIME_LIBRARY NAMES gts.dll libgts-0-7-5.dll)
+  find_program(GTS_RUNTIME_LIBRARY NAMES libgts-0-7-5.dll)
+
+  find_package_handle_standard_args(GTS DEFAULT_MSG
+    GTS_INCLUDE_DIR
+    GLIB_INCLUDE_DIRS
+    GLIBCONFIG_INCLUDE_DIR
+
+    GTS_LIBRARY
+    GLIB_LIBRARIES
+
+    GTS_RUNTIME_LIBRARY
+    GLIB_RUNTIME_LIBRARIES
+  )
+
+  set(GTS_INCLUDE_DIRS
+    ${GTS_INCLUDE_DIR}
+    ${GLIB_INCLUDE_DIRS}
+    ${GLIBCONFIG_INCLUDE_DIR}
+  )
+
+  set(GTS_LINK_LIBRARIES
+    ${GTS_LIBRARY}
+    ${GLIB_LIBRARIES}
+  )
+
+  set(GTS_RUNTIME_LIBRARIES
+    ${GTS_RUNTIME_LIBRARY}
+    ${GLIB_RUNTIME_LIBRARIES}
+  )
+elseif(WIN32)
+  find_package(GLIB)
+
+  find_path(GTS_INCLUDE_DIR gts.h)
+  find_path(GLIBCONFIG_INCLUDE_DIR glibconfig.h PATH_SUFFIXES glib-2.0/include)
+
+  find_library(GTS_LIBRARY NAMES gts)
+
+  find_program(GTS_RUNTIME_LIBRARY NAMES gts.dll)
 
   find_package_handle_standard_args(GTS DEFAULT_MSG
     GTS_INCLUDE_DIR
