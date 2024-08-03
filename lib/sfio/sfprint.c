sea-grass/graphviz

commit 8ce6b211ce7b9ebad3e30fa909effebf59955a93

replace use of SFIO in expr/gvpr with stdio
This change unfortunately needs to be done all at once because of some
intertwined I/O usage. Various things worth calling out in this change:

  * `sfprint` and `sfscanf` are adapted to operate on stdio `FILE`s instead of
    `Sfio_t`s. This is counter-intuitive (teaching SFIO components to operate
    on non-SFIO structures), but seems the only reasonable strategy.
    Unfortunately C stdio does not expose `printf`/`scanf` variants that let you
    control the handling of each format specifier.
  * String/temporary streams are now done with `tmpfile`. This seems the most
    portable replacement option (vs things like `fmemopen` and
    `open_memstream`).
  * When closing `opts.outFile`, we need to introduce an extra guard against
    null pointers because `fclose` unlike `sfclose` does not tolerate null
    pointer inputs.
  * All discipline tweaking of stout/stderr is dropped because this is not
    relevant under stdio.

This change squashes a huge number of compiler warnings.

Gitlab: #1998

lib/sfio/sfprint.c
@@ -12,6 +12,7 @@
 #include <inttypes.h>
 #include	<sfio/sfhdr.h>
 #include	<stddef.h>
+#include	<stdio.h>
 #include	<string.h>
 
 /*	The engine for formatting data
@@ -31,11 +32,11 @@
  * @param f file to print to
  * @param format Structure describing how to print
  */
-int sfprint(Sfio_t *f, Sffmt_t *format) {
+int sfprint(FILE *f, Sffmt_t *format) {
     int v = 0, n_s, base, fmt, flags;
     Sflong_t lv;
     char *sp, *ssp, *endsp, *ep, *endep;
-    int dot, width, precis, n, n_output;
+    int dot, width, precis, n, n_output = 0;
     int sign, decpt;
     ssize_t size;
     double dval;
@@ -51,53 +52,27 @@ int sfprint(Sfio_t *f, Sffmt_t *format) {
     int argp, argn;		/* arg position and number      */
 
 #define SLACK		1024
-    char buf[SF_MAXDIGITS + SLACK], data[SF_GRAIN];
+    char buf[SF_MAXDIGITS + SLACK];
     char decimal = 0, thousand = 0;
 
-    /* fast io system */
-    uchar *d, *endd;
-    int w;
-#define SFBUF(f)	(d = f->next, endd = f->endb)
-#define SFINIT(f)	(SFBUF(f), n_output = 0)
-#define SFEND(f)	((n_output += d - f->next), (f->next = d))
-#define SFputc(f,c) \
-	{ if(d < endd) 	{ *d++ = (uchar)c; } \
-	  else \
-	  { SFEND(f); n_output += (w = SFFLSBUF(f,c)) >= 0 ? 1 : 0; SFBUF(f); \
-	    if(w < 0) goto done; \
-	  } \
-	}
-#define SFnputc(f,c,n) \
-	{ if((endd-d) >= n) { while(n--) *d++ = (uchar)c; } \
-	  else \
-	  { SFEND(f); n_output += (w = SFNPUTC(f,c,n)) > 0 ? w : 0; SFBUF(f); \
-	    if(n != w) goto done; \
-	    n = 0; \
-	  } \
-	}
-#define SFwrite(f,s,n) \
-	{ if((endd-d) >= n) { memcpy(d, s, n); d += n; } \
-	  else \
-	  { SFEND(f); n_output += (w = SFWRITE(f,(void*)s,n)) > 0 ? w : 0; SFBUF(f); \
-	    if(n != w) goto done; \
-	  } \
-	}
-
-    SFCVINIT();			/* initialize conversion tables */
-
-    SFMTXSTART(f, -1);
-
-    /* make sure stream is in write mode and buffer is not NULL */
-    if (f->mode != SF_WRITE && _sfmode(f, SF_WRITE, 0) < 0)
-	SFMTXRETURN(f, -1);
-
-    SFLOCK(f, 0);
-
-    if (!f->data && !(f->flags & SF_STRING)) {
-	f->data = f->next = (uchar *) data;
-	f->endb = f->data + sizeof(data);
-    }
-    SFINIT(f);
+#define SFputc(f,c) do { \
+    if (putc((c), (f)) == EOF) { \
+      goto done; \
+    } \
+} while (0)
+#define SFnputc(f,c,n)  do { \
+    for (int i_ = 0; i_ < (n); ++i_) { \
+      if (putc((c), (f)) == EOF) { \
+        goto done; \
+      } \
+    } \
+    (n) = 0; \
+} while (0)
+#define SFwrite(f,s,n) do { \
+  if (fwrite((s), (n), 1, (f)) < 1) { \
+    goto done; \
+  } \
+} while (0)
 
     tls[1] = NULL;
 
@@ -386,11 +361,7 @@ int sfprint(Sfio_t *f, Sffmt_t *format) {
 	} else if (ft && ft->extf) {	/* extended processing */
 	    FMTSET(ft, form, args, fmt, size, flags, width, precis, base,
 		   t_str, n_str);
-	    SFEND(f);
-	    SFOPEN(f, 0);
 	    v = ft->extf(&argv, ft);
-	    SFLOCK(f, 0);
-	    SFBUF(f);
 
 	    if (v < 0)
 		goto pop_fmt;
@@ -915,19 +886,5 @@ int sfprint(Sfio_t *f, Sffmt_t *format) {
 	free(fm);
     }
 
-    SFEND(f);
-
-    n = f->next - f->data;
-    if ((d = f->data) == (uchar *) data)
-	f->endw = f->endr = f->endb = f->data = NULL;
-    f->next = f->data;
-
-    if ((((flags = f->flags) & SF_SHARE) && !(flags & SF_PUBLIC)) ||
-	(n > 0 && (d == (uchar *) data || (flags & SF_LINE))))
-	(void) SFWRITE(f, d, n);
-    else
-	f->next += n;
-
-    SFOPEN(f, 0);
-    SFMTXRETURN(f, n_output);
+    return n_output;
 }

lib/expr/exeval.c
@@ -364,24 +364,24 @@ prformat(void* vp, Sffmt_t* dp)
 /*
  * print a list of strings
  */
-static int prints(Expr_t *ex, Exnode_t *exnode, void *env, Sfio_t *sp) {
+static int prints(Expr_t *ex, Exnode_t *exnode, void *env, FILE *sp) {
     Extype_t v;
     Exnode_t *args;
 
     args = exnode->data.operand.left;
     while (args) {
 	v = eval(ex, args->data.operand.left, env);
-	sfputr(sp, v.string);
+	fputs(v.string, sp);
 	args = args->data.operand.right;
     }
-    sfputc(sp, '\n');
+    putc('\n', sp);
     return 0;
 }
 
 /*
  * do printf
  */
-static int print(Expr_t *ex, Exnode_t *exnode, void *env, Sfio_t *sp) {
+static int print(Expr_t *ex, Exnode_t *exnode, void *env, FILE *sp) {
 	Print_t*	x;
 	Extype_t		v;
 	Fmt_t			fmt;
@@ -391,7 +391,7 @@ static int print(Expr_t *ex, Exnode_t *exnode, void *env, Sfio_t *sp) {
 		v = eval(ex, exnode->data.print.descriptor, env);
 		if (v.integer < 0 || (long long unsigned)v.integer >= elementsof(ex->file) ||
 		    (!(sp = ex->file[v.integer]) &&
-		    !(sp = ex->file[v.integer] = sfnew(NULL, SF_UNBOUND, (int)v.integer, SF_READ|SF_WRITE))))
+		    !(sp = ex->file[v.integer] = tmpfile())))
 		{
 			exerror("printf: %" PRIdMAX ": invalid descriptor", (intmax_t)v.integer);
 			return -1;
@@ -412,7 +412,7 @@ static int print(Expr_t *ex, Exnode_t *exnode, void *env, Sfio_t *sp) {
 				sfprint(sp, &fmt.fmt);
 			}
 			else
-				sfputr(sp, x->format);
+				fputs(x->format, sp);
 		} while ((x = x->next));
 	else
 	{
@@ -499,7 +499,7 @@ scformat(void* vp, Sffmt_t* dp)
  * do scanf
  */
 
-static int scan(Expr_t *ex, Exnode_t *exnode, void *env, Sfio_t *sp) {
+static int scan(Expr_t *ex, Exnode_t *exnode, void *env, FILE *sp) {
 	Extype_t		v;
 	Extype_t		u;
 	Fmt_t			fmt;
@@ -515,7 +515,7 @@ static int scan(Expr_t *ex, Exnode_t *exnode, void *env, Sfio_t *sp) {
 		}
 		else
 			v.integer = 0;
-		if (v.integer < 0 || (size_t)v.integer >= elementsof(ex->file) || (!(sp = ex->file[v.integer]) && !(sp = ex->file[v.integer] = sfnew(NULL, SF_UNBOUND, (int)v.integer, SF_READ|SF_WRITE))))
+		if (v.integer < 0 || (size_t)v.integer >= elementsof(ex->file) || (!(sp = ex->file[v.integer]) && !(sp = ex->file[v.integer] = tmpfile())))
 		{
 			exerror("scanf: %" PRIdMAX ": invalid descriptor", (intmax_t)v.integer);
 			return 0;
@@ -529,7 +529,19 @@ static int scan(Expr_t *ex, Exnode_t *exnode, void *env, Sfio_t *sp) {
 	u = eval(ex, exnode->data.scan.format, env);
 	fmt.fmt.form = u.string;
 	fmt.actuals = exnode->data.scan.args;
-	n = sp ? sfscanf(sp, "%!", &fmt) : sfsscanf(v.string, "%!", &fmt);
+	if (sp == NULL) {
+		sp = tmpfile();
+		if (sp == NULL) {
+			exerror("scanf: failed to open temporary file");
+			return 0;
+		}
+		fputs(v.string, sp);
+		rewind(sp);
+		n = sfscanf(sp, "%!", &fmt);
+		fclose(sp);
+	} else {
+		n = sfscanf(sp, "%!", &fmt);
+	}
 	if (fmt.actuals && !*fmt.fmt.form)
 		exerror("scanf: %s: too many arguments", fmt.actuals->data.operand.left->data.variable.symbol->name);
 	return n;
@@ -1398,7 +1410,7 @@ static Extype_t eval(Expr_t *ex, Exnode_t *exnode, void *env) {
 		n = 1;
 		goto add;
 	case PRINT:
-		v.integer = prints(ex, exnode, env, sfstdout);
+		v.integer = prints(ex, exnode, env, stdout);
 		return v;
 	case PRINTF:
 		v.integer = print(ex, exnode, env, NULL);
@@ -1413,14 +1425,22 @@ static Extype_t eval(Expr_t *ex, Exnode_t *exnode, void *env) {
 		v.integer = scan(ex, exnode, env, NULL);
 		return v;
 	case SPRINTF: {
-		Sfio_t *buffer = sfstropen();
+		FILE *buffer = tmpfile();
 		if (buffer == NULL) {
 			fprintf(stderr, "out of memory\n");
 			graphviz_exit(EXIT_FAILURE);
 		}
 		print(ex, exnode, env, buffer);
-		v.string = exstash(buffer, ex->ve);
-		sfstrclose(buffer);
+		size_t size = (size_t)ftell(buffer);
+		rewind(buffer);
+		v.string = vmalloc(ex->ve, size + 1);
+		if (v.string == NULL) {
+			v.string = exnospace();
+		} else {
+			fread(v.string, size, 1, buffer);
+			v.string[size] = '\0';
+		}
+		fclose(buffer);
 		return v;
 	}
 	case '=':

lib/expr/exgram.h
@@ -966,7 +966,7 @@ exclose(Expr_t* p, int all)
 			size_t i;
 			for (i = 3; i < elementsof(p->file); i++)
 				if (p->file[i])
-					sfclose(p->file[i]);
+					fclose(p->file[i]);
 			if (p->vm)
 				vmclose(p->vm);
 			if (p->ve)

lib/expr/exopen.c
@@ -23,6 +23,7 @@
 
 #include <expr/exlib.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
 /*
@@ -48,9 +49,9 @@ exopen(Exdisc_t* disc)
 	program->id = "libexpr:expr";
 	program->disc = disc;
 	setcontext(program);
-	program->file[0] = sfstdin;
-	program->file[1] = sfstdout;
-	program->file[2] = sfstderr;
+	program->file[0] = stdin;
+	program->file[1] = stdout;
+	program->file[2] = stderr;
 	strcpy(program->main.name, "main");
 	program->main.lex = PROCEDURE;
 	program->main.index = PROCEDURE;

lib/expr/expr.3
@@ -43,9 +43,9 @@ Exnode_t*        exexpr(Expr_t*, const char*, Exid_t*, int);
 Exnode_t*        excast(Expr_t*, Exnode_t*, int, Exnode_t*, int);
 Exnode_t*        exnewnode(Expr_t*, int, int, int, Exnode_t*, Exnode_t*);
 void             exfreenode(Expr_t*, Exnode_t*);
-int              expush(Expr_t*, const char*, int, const char*, Sfio_t*);
+int              expush(Expr_t*, const char*, int, const char*, FILE*);
 int              expop(Expr_t*);
-int              excomp(Expr_t*, const char*, int, const char*, Sfio_t*);
+int              excomp(Expr_t*, const char*, int, const char*, FILE*);
 int              extoken(Expr_t*);
 char*            extype(long int);
 Extype_t         exzero(long int);

lib/expr/expr.h
@@ -224,7 +224,7 @@ struct Expr_s				/* ex program state		*/
 {
 	const char*	id;		/* library id			*/
 	Dt_t*		symbols;	/* symbol table			*/
-	Sfio_t*		file[10];	/* io streams			*/
+	FILE*		file[10];	/* io streams			*/
 	Vmalloc_t*	vm;		/* program store		*/
 
 #ifdef _EX_PROG_PRIVATE_

lib/gvpr/actions.c
@@ -34,15 +34,15 @@
 #define KINDS(p) ((AGTYPE(p) == AGRAPH) ? "graph" : (AGTYPE(p) == AGNODE) ? "node" : "edge")
 
 static int iofread(void *chan, char *buf, int bufsize) {
-  return (int)read(sffileno(chan), buf, bufsize);
+  return (int)read(fileno(chan), buf, bufsize);
 }
 
 static int ioputstr(void *chan, const char *str) {
-  return sfputr(chan, str);
+  return fputs(str, chan);
 }
 
 static int ioflush(void *chan) {
-  return sfsync(chan);
+  return fflush(chan);
 }
 
 static Agiodisc_t gprIoDisc = { iofread, ioputstr, ioflush };
@@ -594,7 +594,7 @@ int deleteObj(Agraph_t * g, Agobj_t * obj)
  * might not use sfio. In this case, we push an sfio discipline on
  * the graph, write it, and then pop it off.
  */
-int sfioWrite(Agraph_t *g, Sfio_t *fp) {
+int sfioWrite(Agraph_t *g, FILE *fp) {
     int rv;
 
     Agiodisc_t* saveio = g->clos->disc.io;
@@ -610,19 +610,18 @@ int sfioWrite(Agraph_t *g, Sfio_t *fp) {
  */
 int writeFile(Agraph_t *g, char *f) {
     int rv;
-    Sfio_t *fp;
 
     if (!f) {
 	exerror("NULL string passed to writeG");
 	return 1;
     }
-    fp = sfopen(f, "w");
+    FILE *fp = fopen(f, "w");
     if (!fp) {
 	exwarn("Could not open %s for writing in writeG", f);
 	return 1;
     }
     rv = sfioWrite(g, fp);
-    sfclose(fp);
+    fclose(fp);
     return rv;
 }
 
@@ -633,25 +632,24 @@ int writeFile(Agraph_t *g, char *f) {
 Agraph_t *readFile(char *f)
 {
     Agraph_t *gp;
-    Sfio_t *fp;
 
     if (!f) {
 	exerror("NULL string passed to readG");
 	return 0;
     }
-    fp = sfopen(f, "r");
+    FILE *fp = fopen(f, "r");
     if (!fp) {
 	exwarn("Could not open %s for reading in readG", f);
 	return 0;
     }
     gp = readG(fp);
-    sfclose(fp);
+    fclose(fp);
 
     return gp;
 }
 
 int fwriteFile(Expr_t *ex, Agraph_t *g, int fd) {
-    Sfio_t *sp;
+    FILE *sp;
 
     if (fd < 0 ||
         (elementsof(ex->file) <= INT_MAX && fd >= (int)elementsof(ex->file))
@@ -664,7 +662,7 @@ int fwriteFile(Expr_t *ex, Agraph_t *g, int fd) {
 
 Agraph_t *freadFile(Expr_t * ex, int fd)
 {
-    Sfio_t *sp;
+    FILE *sp;
 
     if (fd < 0 ||
         (elementsof(ex->file) <= INT_MAX && fd >= (int)elementsof(ex->file))
@@ -687,7 +685,7 @@ int openFile(Expr_t * ex, const char *fname, const char *mode)
 	exerror("openF: no available descriptors");
 	return -1;
     }
-    ex->file[idx] = sfopen(fname, mode);
+    ex->file[idx] = fopen(fname, mode);
     if (ex->file[idx])
 	return idx;
     else
@@ -706,7 +704,7 @@ int closeFile(Expr_t * ex, int fd)
 	exerror("closeF: stream %d not open", fd);
 	return -1;
     }
-    rv = sfclose(ex->file[fd]);
+    rv = fclose(ex->file[fd]);
     if (!rv)
 	ex->file[fd] = 0;
     return rv;
@@ -718,7 +716,7 @@ int closeFile(Expr_t * ex, int fd)
  */
 char *readLine(Expr_t * ex, int fd)
 {
-    Sfio_t *sp;
+    FILE *sp;
     int c;
     char *line;
 
@@ -728,7 +726,7 @@ char *readLine(Expr_t * ex, int fd)
     }
 
     agxbuf tmps = {0};
-    while ((c = sfgetc(sp)) > 0 && c != '\n')
+    while ((c = getc(sp)) > 0 && c != '\n')
 	agxbputc(&tmps, (char)c);
     if (c == '\n')
 	agxbputc(&tmps, (char)c);

lib/gvpr/actions.h
@@ -16,6 +16,7 @@ extern "C" {
 
 #include "cgraph.h"
 #include <expr/expr.h>
+#include <stdio.h>
 
     extern void nodeInduce(Agraph_t * selected);
     extern Agobj_t *cloneO(Agraph_t * g, Agobj_t * obj);
@@ -32,7 +33,7 @@ extern "C" {
     extern Agedge_t *addEdge(Agraph_t * g, Agedge_t * e, int doAdd);
     extern Agraph_t *sameG(void *p1, void *p2, char *fn, char *msg);
     extern int compare(Agobj_t *, Agobj_t *);
-    extern int sfioWrite(Agraph_t*, Sfio_t*);
+    extern int sfioWrite(Agraph_t*, FILE*);
     extern int writeFile(Agraph_t*, char*);
     extern int fwriteFile(Expr_t*, Agraph_t*, int);
     extern Agraph_t *readFile(char *);

commit dc4af38de72de49365dba4c696ff53906740cd33

fix gvpr’s handling of empty strings as 'printf' parameters
In transitioning from sfio’s I/O to libc stdio,
8ce6b211ce7b9ebad3e30fa909effebf59955a93 introduced a bug wherein printing
zero-length strings would be misinterpreted as an I/O error. This was mostly
ignored by libexpr and libgvpr and not visible to the user, except that it
terminated the current `[s]printf` call. So if there was anything following the
empty string, this would be truncated. E.g. `printf("hello%s world\n", "")` used
in one of the test cases fixed here would previously print just “hello”.

Gitlab: fixes #2577
Reported-by: phormio

lib/sfio/sfprint.c
@@ -68,7 +68,7 @@ int sfprint(FILE *f, Sffmt_t *format) {
     (n) = 0; \
 } while (0)
 #define SFwrite(f,s,n) do { \
-  if (fwrite((s), (n), 1, (f)) < 1) { \
+  if ((n) > 0 && fwrite((s), (n), 1, (f)) < 1) { \
     goto done; \
   } \
 } while (0)

tests/test_regression.py
@@ -4238,9 +4238,6 @@ def test_2572():
 
 
 @pytest.mark.skipif(which("gvpr") is None, reason="GVPR not available")
-@pytest.mark.xfail(
-    reason="https://gitlab.com/graphviz/graphviz/-/issues/2577", strict=True
-)
 def test_2577():
     """
     accessing an uninitialized string should not corrupt GVPR’s state
@@ -4261,9 +4258,6 @@ def test_2577():
 
 
 @pytest.mark.skipif(which("gvpr") is None, reason="GVPR not available")
-@pytest.mark.xfail(
-    reason="https://gitlab.com/graphviz/graphviz/-/issues/2577", strict=True
-)
 def test_2577_1():
     """
     a variant of `test_2577` that does not involve attribute access