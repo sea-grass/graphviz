7fc216a86a061cc52f5ab824dc173d29e112d3b4
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-08-03 13:48:12 -0700

    enable syntax highlighting of Pytest failure diffs
    As of Pytest 8.0.0, at `-vv` and above if Pygments is installed diffs will be
    syntax highlighted which makes them easier to read and interpret.
    
    Github: https://github.com/pytest-dev/pytest/issues/11520

--- ci/linux-test-job-script.sh
+++ ci/linux-test-job-script.sh
@@ -7,4 +7,5 @@ set -x
 
 ci/install-packages.sh
 export GV_VERSION=$( cat GRAPHVIZ_VERSION )
-python3 -m pytest --strict-markers --verbose --junit-xml=report.xml ci/tests.py tests
+python3 -m pytest --strict-markers --verbose --verbose --junit-xml=report.xml \
+  ci/tests.py tests
