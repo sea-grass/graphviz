81f6e30c55d1eaab9da7df57a6d234e6cc9bbf92
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-08-21 17:03:05 -0700

    edgepaint edge_distinct_coloring: lift comment to header docstring

--- lib/edgepaint/edge_distinct_coloring.c
+++ lib/edgepaint/edge_distinct_coloring.c
@@ -142,17 +142,6 @@ static int splines_intersect(size_t dim,
 Agraph_t *edge_distinct_coloring(const char *color_scheme, int *lightness,
                                  Agraph_t *g, double angle, double accuracy,
                                  int check_edges_with_same_endpoint, int seed) {
-  /* color the edges of a graph so that conflicting edges are as distinct in color as possible.
-     color_scheme: rgb, lab, gray, or a list of comma separated RGB colors in hex, like #ff0000,#00ff00
-     lightness: of the form 0,70, specifying the range of lightness of LAB color. Ignored if scheme is not COLOR_LAB.
-     g: the graph
-     angle: if two edges cross at an angle < "angle", consider they as conflict
-     accuracy: how accurate when finding  color of an edge to be as different from others
-     check_edges_with_same_endpoint: if TRUE, we will check edges with same end point and only consider them as conflict if
-     .                   their angle is very small. Edges that share an end point and is close to 180 degree
-     .                   are not consider conflict.
-     seed: random_seed. If negative, consider -seed as the number of random start iterations
-  */
   double *x = NULL;
   int dim = 2;
   SparseMatrix A, B, C;
--- lib/edgepaint/edge_distinct_coloring.h
+++ lib/edgepaint/edge_distinct_coloring.h
@@ -10,6 +10,24 @@
 
 #pragma once
 
+/// color the edges of a graph so that conflicting edges are as distinct in
+/// color as possible
+///
+/// @param color_scheme rgb, lab, gray, or a list of comma separated RGB colors
+///   in hex, like #ff0000,#00ff00
+/// @param lightness of the form 0,70, specifying the range of lightness of LAB
+///   color. Ignored if scheme is not COLOR_LAB.
+/// @param g the graph
+/// @param angle if two edges cross at an angle < "angle", consider they as
+///   conflict
+/// @param accuracy how accurate when finding  color of an edge to be as
+///   different from others
+/// @param check_edges_with_same_endpoint if TRUE, we will check edges with same
+///   end point and only consider them as conflict if their angle is very small.
+///   Edges that share an end point and is close to 180 degree are not consider
+///   conflict.
+/// @param seed random_seed. If negative, consider -seed as the number of random
+///   start iterations
 Agraph_t *edge_distinct_coloring(const char *color_scheme, int *lightness,
                                  Agraph_t *g, double angle, double accuracy,
                                  int check_edges_with_same_endpoint, int seed);
