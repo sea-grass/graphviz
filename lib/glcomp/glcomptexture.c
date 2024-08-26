ellson/MOTHBALLED-graphviz

commit 9a9cfd4e90775aa14a11a0db6745783f3a59ef56

smyrna changes

lib/glcomp/glcomptexture.c
@@ -14,68 +14,154 @@
 **********************************************************/
 
 #include "glcomptexture.h"
+#include "glpangofont.h"
 
-glCompTexture *glCompCreateTextureFromRaw(char *filename, int width,
-					  int height, int wrap)
+#include "memory.h"
+
+void glCompSetRemoveTexLabel(glCompSet* s,glCompFont* t)
 {
-    glCompTexture *t;
-#ifdef _WIN32
-    BYTE *data;
-#else
-    unsigned char *data;
-#endif
-    FILE *file;
-    t = malloc(sizeof(glCompTexture));
-    glGenTextures(1, &t->id);
-
-    // allocate buffer
-    data = malloc(width * height * 3);
-    // open and read texture data
-    file = fopen(filename, "rb");
-    fread(data, width * height * 3, 1, file);
-    fclose(file);
-
-
-// select our current texture
-    glBindTexture(GL_TEXTURE_2D, t->id);
-
-    // select modulate to mix texture with color for shading
-    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-
-    // when texture area is small, bilinear filter the closest mipmap
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
-		    GL_LINEAR_MIPMAP_NEAREST);
-    // when texture area is large, bilinear filter the first mipmap
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    // if wrap is true, the texture wraps over at the edges (repeat)
-    //       ... false, the texture ends at the edges (clamp)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,
-		    (GLfloat) wrap ? (GLfloat) GL_REPEAT : GL_CLAMP);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,
-		    (GLfloat) wrap ? (GLfloat) GL_REPEAT : GL_CLAMP);
-
-/*	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,GL_REPEAT );
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
-	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
-
-	
-	// build our texture mipmaps
-	glTexImage2D(GL_TEXTURE_2D, 0, 4, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE,data);*/
-
-    gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGB, width, height,
-		      GL_RGB, GL_UNSIGNED_BYTE, data);
-
-    // free buffer
-    free(data);
-    t->w = (float) width;
-    t->h = (float) height;
-    return t;
 }
 
-int glCompDeleteTexture(glCompTexture * t)
+
+static glCompTex* glCompSetAddNewTexture(glCompSet* s,int width,int height,unsigned char* data,int is2D)
 {
-    return 0;
+	int Er,offset,ind;
+	glCompTex* t;
+	unsigned char* tarData;
+	unsigned char* srcData;
+
+	if (!data)
+		return NULL;
+
+	Er=0;
+	t=NEW(glCompTex);
+	if (!is2D)	/*use opengl texture*/
+	{
+		glEnable(GL_TEXTURE_2D);
+		glShadeModel(GL_FLAT);
+		glEnable(GL_DEPTH_TEST);
+		glPixelStorei(GL_UNPACK_ALIGNMENT,1);
+		glGenTextures(1, &t->id); //get next id
+		if (t->id < 0 )	/*for some opengl based error , texture couldnt be created*/
+			Er=1;
+		else
+		{
+			glBindTexture(GL_TEXTURE_2D, t->id);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+			glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,width, height,0,GL_RGBA, GL_UNSIGNED_BYTE, data );
+			glDisable(GL_TEXTURE_2D);
+		}
+	}
+	if(is2D && !Er)
+	{
+		t->data=malloc (4 * width * height);
+		offset =4 ;//RGBA  mod,TO DO implement other modes 
+		/*data upside down because of pango gl coord system*/
+		for (ind=0;ind < height; ind ++)
+		{
+			srcData = data + (height - 1 -ind) * offset * width;
+			tarData = t->data+ind * offset * width;
+			memcpy(tarData,srcData,4*width);
+		}
+	}
+
+	if(Er)
+	{
+		free (data);
+		free (t);
+		return NULL;
+	}
+	t->userCount=1;
+	t->width=(GLfloat)width;
+	t->height=(GLfloat)height;
+	s->textureCount ++ ;
+	s->textures=realloc(s->textures,s->textureCount * sizeof(glCompTex*));
+	s->textures[s->textureCount-1]=t;
+	return t;
+
+
+}
+
+glCompTex* glCompSetAddNewTexImage(glCompSet* s,int width,int height,unsigned char* data,int is2D)
+{
+
+	glCompTex* t;
+	if (!data)
+		return NULL;
+	t=glCompSetAddNewTexture(s,width,height,data,is2D);
+	if (!t)
+		return NULL;
+	t->type=glTexImage;
+	return t;
+
+}
+
+
+
+
+glCompTex* glCompSetAddNewTexLabel(glCompSet* s, char* def,int fs,char* text,int is2D)
+{
+	int ind,Er,width,height;
+	int ind2=0;
+	glCompTex* t;
+	cairo_surface_t *surface=NULL;
+	unsigned char* data;
+	data=(unsigned char*)0;
+	Er=0;
+	if (!def)
+		return NULL;
+	/*first check if the same label with same font def created before
+		if it was , return its id
+	*/
+	for (ind =0 ; ind < s->textureCount ; ind ++)
+	{
+		if(s->textures[ind]->type==glTexLabel)
+		{
+			if ((strcmp(def,s->textures[ind]->def)==0)&&(s->textures[ind]->type == glTexLabel)&&(strcmp(text,s->textures[ind]->text)==0) )
+			{
+				s->textures[ind]->userCount ++;
+				return s->textures[ind];
+			}
+		}
+	}
+
+
+	data=	create_pango_texture(def,fs,text,surface,&width,&height);
+	if (!data)	/*pango error , */
+		Er=1;
+	t=glCompSetAddNewTexture(s,width,height,data,is2D);
+	if (!t) Er=1;
+    cairo_surface_destroy(surface);
+
+	if(Er)
+	{
+		free (data);
+		free (t);
+		return NULL;
+	}
+
+	t->def=strdup(def);
+	t->text=strdup(text);
+	t->type=glTexLabel;
+	return t;
+}
+
+void glCompDeleteTexture(glCompTex* t)
+{
+	if (!t)
+		return;
+	t->userCount --;
+	if (!t->userCount)
+	{
+		if (t->data)
+			free (t->data);
+		if (t->def)
+			free (t->def);
+		if (t->text)
+			free (t->text);
+		free(t); 
+	}
 }

cmd/smyrna/Makefile.am
@@ -30,14 +30,14 @@ noinst_HEADERS = btree.h draw.h filter.h glexpose.h glmotion.h gltemplate.h \
 	topfisheyeview.h viewportcamera.h gvprpipe.h \
 	gui/callbacks.h gui/datalistcallbacks.h gui/filterscallbacks.h \
 	gui/glmenu.h gui/gui.h gui/menucallbacks.h gui/toolboxcallbacks.h md5.h \
-	gui/topviewsettings.h gui/beacon.h drawxdot.h arcball.h
+	gui/topviewsettings.h gui/beacon.h drawxdot.h arcball.h gui/glcompui.h
 
 smyrna_SOURCES = btree.c draw.c filter.c glexpose.c glmotion.c gltemplate.c \
 	hier.c main.c selection.c support.c template.c topview.c topviewdata.c \
 	topfisheyeview.c trackball.c tvnodes.c viewport.c viewportcamera.c \
     gvprpipe.c gui/beacon.c gui/callbacks.c gui/datalistcallbacks.c \
     gui/filterscallbacks.c gui/glmenu.c gui/gui.c gui/menucallbacks.c \
-    gui/toolboxcallbacks.c gui/topviewsettings.c md5.c drawxdot.c arcball.c
+    gui/toolboxcallbacks.c gui/topviewsettings.c md5.c drawxdot.c arcball.c gui/glcompui.c
 
 smyrna_LDADD = $(top_builddir)/lib/cgraph/libcgraph_C.la \
 		$(top_builddir)/lib/cdt/libcdt_C.la \

cmd/smyrna/draw.c
@@ -323,7 +323,6 @@ void DrawEllipse(xdot_op * op, int param)
 	filled = 0;
     }
 
-    glLineWidth(view->LineWidth);
     if (!filled)
 	glBegin(GL_LINE_LOOP);
     else
@@ -464,7 +463,8 @@ void InsertImage(xdot_op * op, int param)
 }
 void EmbedText(xdot_op * op, int param)
 {
-    GLfloat x;
+#ifdef UNUSED
+	GLfloat x;
 //    SelectText((sdot_op *) op);
     set_options((sdot_op *) op, param);
     if (op->u.text.align == 1)
@@ -481,6 +481,7 @@ void EmbedText(xdot_op * op, int param)
 		  view->selectedNodeColor.B,1);
 	glprintf(view->widgets->fontset->fonts[view->widgets->fontset->activefont], (x - dx), (GLfloat)op->u.text.y -  dy,(GLfloat)0,
 		   (GLfloat)op->u.text.width, op->u.text.text);
+#endif
 }
 
 void draw_selection_box(ViewInfo * view)
@@ -745,7 +746,7 @@ static void scanXdot(xdot * xDot, void *p)
 		op->obj = p;
 		if (op->op.kind==xd_font)
 		{
-			add_font(view->widgets->fontset,op->op.u.font.name);//load or set active font
+//			add_font(view->widgets->fontset,op->op.u.font.name,op->op.u.font.size);//load or set active font
 		}
     }
 

cmd/smyrna/glexpose.c
@@ -19,7 +19,6 @@
 #include "glutils.h"
 #include "topview.h"
 #include "topfisheyeview.h"
-#include "glcomptext.h"
 #include "gui/toolboxcallbacks.h"
 void drawRotatingAxis(void);
 void draw_cube()
@@ -59,6 +58,56 @@ glBegin(GL_QUADS);		// Draw The Cube Using quads
   glEnd();	
 }
 
+
+
+void drawRotatingAxis(void)
+{
+	float x,y;
+	float x1,y1,z1;
+	float x2,y2,z2;
+	float R1,R2;
+	static GLUquadricObj *quadratic=(GLUquadricObj*)0;
+	if (!quadratic)
+	{
+		quadratic=gluNewQuadric();										// Create A Pointer To The Quadric Object
+		gluQuadricNormals(quadratic, GLU_SMOOTH);						// Create Smooth Normals
+//		gluQuadricTexture(quadratic, GL_TRUE);							// Create Texture Coords
+		gluQuadricDrawStyle (quadratic,GLU_LINE);
+
+
+	}
+
+	if ((view->mouse.mouse_mode == MM_ROTATE) && (view->active_camera >=0))
+	{
+		float AL=45;
+		glPushMatrix();
+		glLoadIdentity();
+		glMultMatrixf(view->arcball->Transform.M); /*arcball transformations , experimental*/
+		glLineWidth(3);
+		glBegin(GL_LINES);
+			glColor3f(1,1,0);
+	
+			glVertex3f(0,0,0);
+			glVertex3f(0,AL,0);
+
+			glVertex3f(0,0,0);
+			glVertex3f(AL,0,0);
+
+			glVertex3f(0,0,0);
+			glVertex3f(0,0,AL);
+
+		glEnd();
+		glColor4f(0,1,0,0.3);
+		gluSphere(quadratic,AL,20,20);
+		glLineWidth(1);
+		glPopMatrix();
+
+	}
+
+}
+
+
+
 /*
 	refreshes camera settings using view parameters such as pan zoom etc
 	if a camera is selected viewport is switched to 3D
@@ -67,28 +116,22 @@ glBegin(GL_QUADS);		// Draw The Cube Using quads
 */
 int glupdatecamera(ViewInfo * view)
 {
-
 	if (view->active_camera==-1)
-	{
-		gluLookAt(view->panx, view->pany, 20, view->panx,
-			view->pany, 0.0, 0.0, 1.0, 0.0);
-	}
+		glTranslatef(-view->panx,-view->pany,view->panz);
 
 
 	/*toggle to active camera*/
 	else
 	{
 		glMultMatrixf(view->arcball->Transform.M); /*arcball transformations , experimental*/
-/*		gluLookAt(view->cameras[view->active_camera]->targetx, view->cameras[view->active_camera]->targety, 20, view->cameras[view->active_camera]->targetx,
-		view->cameras[view->active_camera]->targety, 0.0, 0.0, 1.0, 0.0);*/
 		glTranslatef(-view->cameras[view->active_camera]->targetx,-view->cameras[view->active_camera]->targety,0);
-//		printf(" %f %f %f \n",view->panx,view->pany,view->
 	}
 
 	GetOGLPosRef(1, view->h - 5, &(view->clipX1), &(view->clipY1),
 		 &(view->clipZ1));
     GetOGLPosRef(view->w - 1, 1, &(view->clipX2), &(view->clipY2),
 		 &(view->clipZ2));
+
 	if (view->active_camera==-1)
 	{
 		glScalef(1/view->zoom*-1,1/view->zoom*-1,1/view->zoom*-1);
@@ -96,8 +139,8 @@ int glupdatecamera(ViewInfo * view)
 	else
 	{
 		glScalef(1/view->cameras[view->active_camera]->r,1/view->cameras[view->active_camera]->r,1/view->cameras[view->active_camera]->r);
-
 	}
+
 
 	return 1;
 }
@@ -169,13 +212,15 @@ void drawtestpoly(void)
 	glEnable(GL_TEXTURE_2D);
 	glTexEnvf (GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
 //	glTexEnvf(GL_TEXTURE_ENV,GL_TEXTURE_ENV_MODE , GL_DECAL);
-	glBindTexture(GL_TEXTURE_2D,view->widgets->fontset->fonts[view->widgets->fontset->activefont]->texId);
-	glColor3f(1,0,0);
-	glBegin(GL_POLYGON);
+//	glBindTexture(GL_TEXTURE_2D,view->widgets->fontset->fonts[view->widgets->fontset->activefont]->texId);
+	glBindTexture(GL_TEXTURE_2D,1);
+	glColor4f(1,1,1,1);
+	glBegin(GL_QUADS);
 		glTexCoord2f(0.0,0.0);glVertex3f(0.0,0.0,0.0);
 		glTexCoord2f(0.0,1.0);glVertex3f(0.0,256.0,0.0);
 		glTexCoord2f(1.0,1.0);glVertex3f(256.0,256.0,0.0);
 		glTexCoord2f(1.0,0.0);glVertex3f(256.0,0.0,0.0);
+		glTexCoord2f(0.0,0.0);glVertex3f(0.0,0.0,0.0);
 	glEnd();
 	glDisable(GL_TEXTURE_2D);
 
@@ -227,52 +272,6 @@ int glexpose_drawgraph(ViewInfo * view)
 		return 1;
     }
 	return 0;
-}
-
-void drawRotatingAxis(void)
-{
-	float x,y;
-	float x1,y1,z1;
-	float x2,y2,z2;
-	float R1,R2;
-	static GLUquadricObj *quadratic=(GLUquadricObj*)0;
-	if (!quadratic)
-	{
-		quadratic=gluNewQuadric();										// Create A Pointer To The Quadric Object
-		gluQuadricNormals(quadratic, GLU_SMOOTH);						// Create Smooth Normals
-//		gluQuadricTexture(quadratic, GL_TRUE);							// Create Texture Coords
-		gluQuadricDrawStyle (quadratic,GLU_LINE);
-
-
-	}
-
-	if ((view->mouse.mouse_mode == MM_ROTATE) && (view->active_camera >=0))
-	{
-		float AL=45;
-		glPushMatrix();
-		glLoadIdentity();
-		glMultMatrixf(view->arcball->Transform.M); /*arcball transformations , experimental*/
-		glLineWidth(3);
-		glBegin(GL_LINES);
-			glColor3f(1,1,0);
-	
-			glVertex3f(0,0,0);
-			glVertex3f(0,AL,0);
-
-			glVertex3f(0,0,0);
-			glVertex3f(AL,0,0);
-
-			glVertex3f(0,0,0);
-			glVertex3f(0,0,AL);
-
-		glEnd();
-		glColor4f(0,1,0,0.3);
-		gluSphere(quadratic,AL,20,20);
-		glLineWidth(1);
-		glPopMatrix();
-
-	}
-
 }
 
 

cmd/smyrna/glmotion.c
@@ -82,7 +82,7 @@ void glmotion_zoom_inc(int zoomin)
 	glexpose();
 
 }
-/*real zoom in out is done here, all other functions send this one what they desired, it is not guranteed,*/
+/*real zoom in out is done here, all other functions send this one what they desire, it is not guranteed,*/
 void graph_zoom(float real_zoom)
 {
 	float old_zoom;

cmd/smyrna/gltemplate.c
@@ -36,6 +36,20 @@ static float begin_y = 0.0;
 static float dx = 0.0;
 static float dy = 0.0;
 
+/*mouse mode mapping funvtion from gtk to glcomp*/
+static glMouseButtonType getGlCompMouseType(int n)
+{
+	switch (n)
+	{
+		case 1:
+			  return glMouseLeftButton;
+		case 3:
+			  return glMouseRightButton;
+	}
+}
+
+
+
 /*
 	test opengl parameters, configuration.Run this function to see machine's open gl capabilities
 	params:gtk gl config class ,gtk takes care of all these tests
@@ -134,11 +148,6 @@ static void realize(GtkWidget * widget, gpointer data)
 
     static char *smyrna_font;
 
-#ifdef WIN32
-    smyrna_font = "c:/pango_font.tga";
-#else
-    smyrna_font = smyrnaPath("arial.tga");
-#endif
 
 	/*** OpenGL BEGIN ***/
     if (!gdk_gl_drawable_gl_begin(gldrawable, glcontext))
@@ -257,23 +266,6 @@ gboolean expose_event(GtkWidget * widget, GdkEventExpose * event,
 	return TRUE;
 }
 
-#ifdef UNUSED
-static gboolean key_release_event(GtkWidget * widget,
-				     GdkEventButton * event, gpointer data)
-{
-	printf ("a key is released\n");
-
-}
-
-static gboolean key_press_event(GtkWidget * widget, GdkEventKey * event,
-				gpointer data)
-{
-	printf ("a key is pressed\n");
-
-}
-#endif
-
-
 /*
 	when a mouse button is clicked this function is called
 	params:gtk opgn gl canvas , GdkEventButton object and custom data
@@ -282,15 +274,10 @@ static gboolean key_press_event(GtkWidget * widget, GdkEventKey * event,
 static gboolean button_press_event(GtkWidget * widget,
 				   GdkEventButton * event, gpointer data)
 {
-	if (view->graphCount)
-	{
-		if (glCompSetClick(view->widgets, (int) event->x, (int) event->y))
-	    expose_event(view->drawing_area, NULL, NULL);
-    }
-
     begin_x = (float) event->x;
     begin_y = (float) event->y;
-	//update mouse coordinates of view if there is left or right click
+	view->widgets->common.functions.mousedown(view->widgets,(GLfloat)event->x,(GLfloat)event->y,getGlCompMouseType(event->button));
+
 	if ((event->button == 1) || (event->button == 3))
 	{
 		GetOGLPosRef    ((int) begin_x, (int) begin_y, &(view->GLx), &(view->GLy),&(view->GLz));
@@ -319,7 +306,8 @@ static gboolean button_press_event(GtkWidget * widget,
 			expose_event(view->drawing_area, NULL, NULL);
 		}
     }
-	/*experimental code for arcball, init first click */
+	expose_event(view->drawing_area, NULL, NULL);
+
     return FALSE;
 }
 
@@ -333,14 +321,12 @@ static gboolean button_release_event(GtkWidget * widget,
 {
 	view->FontSizeConst=GetOGLDistance(14);
 	view->arcball->isDragging=0;
+	view->widgets->common.functions.mouseup(view->widgets,(GLfloat)event->x,(GLfloat)event->y,getGlCompMouseType(event->button));
+
 
 	if (event->button == 1)	//left click release
     {
-		if (glCompSetRelease(view->widgets, (int) event->x_root,(int) event->y_root))		
-		{
-			 expose_event(view->drawing_area, NULL, NULL);	
-		}
-		view->mouse.mouse_down = 0;
+   		view->mouse.mouse_down = 0;
 		if ((view->mouse.mouse_mode == MM_RECTANGULAR_SELECT)
 			|| (view->mouse.mouse_mode == MM_RECTANGULAR_X_SELECT)) 
 		{
@@ -431,9 +417,12 @@ static gboolean motion_notify_event(GtkWidget * widget,
     /* float h = (float)widget->allocation.height; */
     float x = (float) event->x;
     float y = (float) event->y;
+    gboolean redraw = FALSE;
+
+	view->widgets->common.functions.mouseover(view->widgets,(GLfloat)x,(GLfloat)y);
+
 	
 
-    gboolean redraw = FALSE;
 
 	dx = x - begin_x;
     dy = y - begin_y;

cmd/smyrna/gui/beacon.c
@@ -234,7 +234,7 @@ int draw_node_hint_boxes(void)
     double dx, dy, dz;
 	char buf[512];  /* FIX!!! static buffer */
 
-    view->widgets->fontset->fonts[view->widgets->fontset->activefont]->fontheight=fs;
+//    view->widgets->fontset->fonts[view->widgets->fontset->activefont]->fontheight=fs;
 
 
 	for (ind = 0; ind < view->Topview->picked_node_count; ind++) 
@@ -285,8 +285,8 @@ int draw_node_hint_boxes(void)
 #endif
 
     }
-	glLineWidth(5);
 	glColor4f(0, 1, 0, 0.5);
+	glLineWidth(2);
 	glBegin(GL_LINES);
 	for (ind = 0; ind < view->Topview->picked_edge_count; ind++) 
 	{
@@ -310,8 +310,7 @@ int draw_node_hint_boxes(void)
 
     }
 	glEnd();
-	glLineWidth(0);
-
+	glLineWidth(1);
 	for (ind = 0; ind < view->Topview->picked_edge_count; ind++) {
 		float x1,x2,y1,y2,z1,z2;
 		char* s;

cmd/smyrna/gui/gui.c
@@ -311,11 +311,7 @@ void load_attributes(void)
     static char* smyrna_attrs;
 
     if (!smyrna_attrs) {
-#if WIN32
-	smyrna_attrs = SMYRNA_ATTRS;
-#else
 	smyrna_attrs = smyrnaPath ("attrs.txt");
-#endif
     }
 
     //loads attributes from a text file

sea-grass/graphviz

commit 58eb6828185cf0ba5ac6be83859a1ddeb2e89759

glcomp glCompSetAddNewTexture: fix 'free' of incorrect pointer
The `data` pointer passed into this function originates from Cairo and is not
owned by us. It should not be freed. From the surrounding context, it seems this
intended to free `t->data`. This bug was introduced in
9a9cfd4e90775aa14a11a0db6745783f3a59ef56.

lib/glcomp/glcomptexture.c
@@ -64,7 +64,7 @@ static glCompTex *glCompSetAddNewTexture(glCompSet *s, int width, int height,
     }
 
     if (Er) {
-	free(data);
+	free(t->data);
 	free(t);
 	return NULL;
     }