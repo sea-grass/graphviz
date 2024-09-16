751c0822c3d0d121e6339ed3b69ced8531338282
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-09-16 17:02:55 +1000

    add a test case for Tclpathplan reading out of bounds
    The xfail for this is not marked strict because it only deterministically fails
    when running under a memory checker like ASan or Valgrind. In other situations,
    the string being over-read can be coincidentally followed by a null byte causing
    a benign result.
    
    Gitlab: #2596

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -4284,6 +4284,76 @@ def test_2591():
     assert gray_svg != rgb_svg, "edgepaint --color_scheme had no effect"
 
 
+@pytest.mark.skipif(shutil.which("tclsh") is None, reason="tclsh not available")
+@pytest.mark.skipif(
+    platform.system() == "Windows",
+    reason="pexpect.spawn is not available on Windows "
+    "(https://pexpect.readthedocs.io/en/stable/overview.html#pexpect-on-windows)",
+)
+@pytest.mark.xfail(
+    is_autotools() and is_macos(),
+    reason="Autotools on macOS does not detect TCL",
+    strict=True,
+)
+@pytest.mark.xfail(
+    is_cmake() and is_ubuntu_2004(),
+    reason="TCL packages are not built on Ubuntu 20.04 with CMake < 3.18",
+    strict=True,
+)
+@pytest.mark.xfail(reason="https://gitlab.com/graphviz/graphviz/-/issues/2596")
+def test_2596():
+    """
+    running Tclpathplan `triangulate` with a malformed callback script should not read
+    out-of-bounds
+    https://gitlab.com/graphviz/graphviz/-/issues/2596
+    """
+
+    # if this appears to be an ASan-enabled CI job, teach `tclsh` to load ASan’s
+    # supporting library because it is otherwise unaware that Tcldot depends on this
+    # being loaded first
+    env = os.environ.copy()
+    if re.search(r"\basan\b", os.environ.get("CI_JOB_NAME", "").lower()):
+        cc = os.environ.get("CC", "gcc")
+        libasan = subprocess.check_output(
+            [cc, "-print-file-name=libasan.so"], universal_newlines=True
+        ).strip()
+        print(f"setting LD_PRELOAD={libasan}")
+        env["LD_PRELOAD"] = libasan
+
+    # startup TCL and load the pathplan module
+    proc = pexpect.spawn("tclsh", timeout=1, env=env)
+    proc.expect("% ")
+    proc.sendline("package require Tclpathplan")
+    proc.expect("% ")
+
+    # Create a pane. We assume the first created pane will be index 0, though
+    # this is not technically required.
+    proc.sendline("vgpane")
+    proc.expect("vgpane0")
+    proc.expect("% ")
+
+    # bind the triangulation callback to something ending in a trailing '%'
+    proc.sendline("vgpane0 bind triangle %")
+    proc.expect("% ")
+
+    # add a triangular polygon
+    proc.sendline("vgpane0 insert 1 1 2 2 1 2")
+    proc.expect("1")
+    proc.expect("% ")
+
+    # attempt triangulation on this polygon
+    proc.sendline("vgpane0 triangulate 1")
+    proc.expect("% ")
+
+    # delete the pane to clean up
+    proc.sendline("vgpane0 delete")
+    proc.expect("% ")
+
+    # tell TCL to exit
+    proc.sendeof()
+    proc.wait()
+
+
 @pytest.mark.skipif(which("acyclic") is None, reason="acyclic not available")
 def test_2600():
     """
