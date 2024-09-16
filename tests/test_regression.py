242d7067905c26112b70b03e2627fdea1a960554
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-09-16 17:02:55 +1000

    add test case for incorrect triangulation error handling

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -4403,6 +4403,47 @@ def test_2591():
     reason="pexpect.spawn is not available on Windows "
     "(https://pexpect.readthedocs.io/en/stable/overview.html#pexpect-on-windows)",
 )
+@pytest.mark.xfail(strict=True)
+def test_vgpane_bad_triangulation():
+    """
+    running Tclpathplan `triangulate` with incorrect arguments should be rejected
+    """
+
+    # startup TCL and load the pathplan module
+    proc = pexpect.spawn("tclsh", timeout=1)
+    proc.expect("% ")
+    proc.sendline("package require Tclpathplan")
+    proc.expect("% ")
+
+    # Create a pane. We assume the first created pane will be index 0, though
+    # this is not technically required.
+    proc.sendline("vgpane")
+    proc.expect("vgpane0")
+    proc.expect("% ")
+
+    # bind the triangulation callback to something ending in a trailing '%'
+    proc.sendline("vgpane0 bind triangle %")
+    proc.expect("% ")
+
+    # run triangulation with no polygon ID, which should be rejected
+    proc.sendline("vgpane0 triangulate")
+    proc.expect("wrong # args")
+
+    # delete the pane to clean up
+    proc.sendline("vgpane0 delete")
+    proc.expect("% ")
+
+    # tell TCL to exit
+    proc.sendeof()
+    proc.wait()
+
+
+@pytest.mark.skipif(shutil.which("tclsh") is None, reason="tclsh not available")
+@pytest.mark.skipif(
+    platform.system() == "Windows",
+    reason="pexpect.spawn is not available on Windows "
+    "(https://pexpect.readthedocs.io/en/stable/overview.html#pexpect-on-windows)",
+)
 def test_vgpane_delete():
     """
     it should be possible to delete an existing `vgpane`
