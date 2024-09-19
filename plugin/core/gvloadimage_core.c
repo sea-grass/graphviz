c16e5df76abed2c264741a66bf00a886ab4e2af4
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-09-19 17:03:37 +1000

    core plugin core_loadimage_ps: do not share memory mapping
    It is not anticipated that another process is writing to the file we are
    reading. Therefore there is no need to use a shared mapping here instead of a
    private mapping that is cheaper.

--- plugin/core/gvloadimage_core.c
+++ plugin/core/gvloadimage_core.c
@@ -169,7 +169,7 @@ static void core_loadimage_ps(GVJ_t * job, usershape_t *us, boxf b, bool filled)
 		fstat(fd, &statbuf);
 		us->datasize = (size_t)statbuf.st_size;
 #ifdef HAVE_SYS_MMAN_H
-		us->data = mmap(0, us->datasize, PROT_READ, MAP_SHARED, fd, 0);
+		us->data = mmap(0, us->datasize, PROT_READ, MAP_PRIVATE, fd, 0);
 		if (us->data == MAP_FAILED)
 			us->data = NULL;
 #else
