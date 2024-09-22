ellson/MOTHBALLED-graphviz

commit bcf867e081c8f0bfc41f443b0f00824b0aa8d12b

additonal fixes from Alex Poylisher <sher@komkon.org>

plugin/rsvg/gvloadimage_rsvg.c
@@ -19,6 +19,7 @@
 #endif
 
 #include <stdlib.h>
+#include <sys/stat.h>
 
 #include "gvplugin_loadimage.h"
 
@@ -26,6 +27,7 @@
 #ifdef HAVE_RSVG
 #include <librsvg/rsvg.h>
 #include <librsvg/rsvg-cairo.h>
+#include <cairo/cairo-svg.h>
 
 #ifdef WIN32
 #define NUL_FILE "nul"
@@ -37,23 +39,22 @@ typedef enum {
     FORMAT_SVG_CAIRO,
 } format_type;
 
-static cairo_status_t
-reader (void *closure, unsigned char *data, unsigned int length)
+
+static void gvloadimage_rsvg_free(usershape_t *us)
 {
-    if (length == fread(data, 1, length, (FILE *)closure)
-     || feof((FILE *)closure))
-        return CAIRO_STATUS_SUCCESS;
-    return CAIRO_STATUS_READ_ERROR;
+    rsvg_handle_close((RsvgHandle*)us->data, NULL);
 }
 
-static void cairo_freeimage(usershape_t *us)
-{
-    cairo_destroy((cairo_t*)us->data);
-}
-
-static RsvgHandle* cairo_loadimage(GVJ_t * job, usershape_t *us)
+static RsvgHandle* gvloadimage_rsvg_load(GVJ_t * job, usershape_t *us)
 {
     RsvgHandle* rsvgh = NULL;
+    guchar *fileBuf = NULL;
+    GError *err = NULL;
+    gsize fileSize;
+    gint result;
+
+    int fd;
+    struct stat stbuf;
 
     assert(job);
     assert(us);
@@ -61,7 +62,7 @@ static RsvgHandle* cairo_loadimage(GVJ_t * job, usershape_t *us)
     assert(us->f);
 
     if (us->data) {
-        if (us->datafree == cairo_freeimage)
+        if (us->datafree == gvloadimage_rsvg_free)
              rsvgh = (RsvgHandle*)(us->data); /* use cached data */
         else {
              us->datafree(us);        /* free incompatible cache data */
@@ -75,12 +76,46 @@ static RsvgHandle* cairo_loadimage(GVJ_t * job, usershape_t *us)
             case FT_SVG:
 
 		rsvg_init();
-       		rsvgh = rsvg_handle_new_from_file(us->name, NULL);
+
+      		rsvgh = rsvg_handle_new();
 		
 		if (rsvgh == NULL) {
+			fprintf(stderr, "rsvg_handle_new_from_file returned an error: %s\n", err->message);
 			rsvg_term();
+			return NULL;
+		} 
+
+	 	fd = fileno(us->f);
+		fstat(fd, &stbuf);
+  		fileSize = stbuf.st_size;	
+
+		fileBuf = calloc(fileSize + 1, sizeof(guchar));
+
+		if (fileBuf == NULL) {
+			rsvg_handle_free(rsvgh);
+			rsvg_term();
+			return NULL;
 		}
 	
+		rewind(us->f);
+
+		if ((result = fread(fileBuf, 1, fileSize, us->f)) < fileSize) {
+			rsvg_handle_free(rsvgh);
+			rsvg_term();
+			return NULL;
+		}
+
+		if (rsvg_handle_write(rsvgh, (const guchar *)fileBuf, (gsize)fileSize, &err) == FALSE) {
+			fprintf(stderr, "rsvg_handle_write returned an error: %s\n", err->message);
+			free(fileBuf);
+			rsvg_handle_free(rsvgh);
+			rsvg_term();
+			return NULL;
+		} 
+
+		free(fileBuf);
+
+		rsvg_handle_close(rsvgh, &err);
 		rsvg_handle_set_dpi(rsvgh, POINTS_PER_INCH);
 
                 break;
@@ -90,15 +125,16 @@ static RsvgHandle* cairo_loadimage(GVJ_t * job, usershape_t *us)
 
         if (rsvgh) {
             us->data = (void*)rsvgh;
-            us->datafree = cairo_freeimage;
+            us->datafree = gvloadimage_rsvg_free;
         }
     }
+
     return rsvgh;
 }
 
-static void rsvg_loadimage_cairo(GVJ_t * job, usershape_t *us, boxf b, boolean filled)
+static void gvloadimage_rsvg_cairo(GVJ_t * job, usershape_t *us, boxf b, boolean filled)
 {
-    RsvgHandle* rsvgh = cairo_loadimage(job, us);
+    RsvgHandle* rsvgh = gvloadimage_rsvg_load(job, us);
 
     cairo_t *cr = (cairo_t *) job->context; /* target context */
     cairo_surface_t *surface;	 /* source surface */
@@ -116,7 +152,6 @@ static void rsvg_loadimage_cairo(GVJ_t * job, usershape_t *us, boxf b, boolean f
         cairo_scale(cr, (b.UR.x - b.LL.x) / us->w,
                        (b.UR.y - b.LL.y) / us->h);
 
-
 	rsvg_handle_render_cairo(rsvgh, cr);
 
         cairo_paint (cr);
@@ -125,7 +160,7 @@ static void rsvg_loadimage_cairo(GVJ_t * job, usershape_t *us, boxf b, boolean f
 }
 
 static gvloadimage_engine_t engine_cairo = {
-    rsvg_loadimage_cairo
+    gvloadimage_rsvg_cairo
 };
 #endif
 #endif

sea-grass/graphviz

commit d9af9c722ac4eb3205dac71ab368e66addc2f2fc

rsvg plugin: partially port librsvg interaction to newer API
The 2.45.1 release of librsvg deprecated a number of functions being called by
this plugin. Previously, the need to support CentOS 7 prevented forward-porting
this code to the newer API. With CentOS 7 support dropped in
5e1e316900d67cc40dbb35eb3743ecbbca732517, the new baseline librsvg floor is set
by Rocky Linux 8 which has librsvg 2.42.7. Though the deprecation warnings did
not arrive until 2.45.1, many of the replacement functions are already available
in 2.42. This change takes advantage of this and uses them where possible.

This change does not fully complete #2206 because a call to the deprecated
`rsvg_handle_render_cairo` remains. Its replacement,
`rsvg_handle_render_document`, is not available until librsvg 2.46.

Aside from addressing deprecation issues, this change fixes numerous other
problems:
  • The prior code was determining the size of the input image file and then
    reading its entire content into memory. For large image files, this required
    allocating enough heap memory to store its content. This was unnecessary,
    even before the present change. A `BUFSIZ`-sized chunk-by-chunk loop could
    have been used instead. Switching to `rsvg_handle_new_from_file` removes the
    need to think about this and lets librsvg handle streaming.
  • The `fstat` call was not being checked for failure.
  • One more byte than the file size was being allocated but then unused.
    Presumably this was copied from elsewhere in the code base where text files
    are read in and then a null byte is appended. There is no null byte appended
    here, so this extra byte was unnecessary.
  • The `rewind` call was unnecessary. The preceding call to
    `gvusershape_file_access` has already rewound the file handle. Presumably
    this was copied from elsewhere in the code base where
    `fseek(…, 0, SEEK_END); ftell; rewind` is used to determine the size of the
    open file, but a different technique is used here that does not move the
    file handle position.
  • `err` in the first error handling path was being dereferenced, despite being
    null at this point. This would have caused a crash if this path was ever
    exercised.
  • bcf867e081c8f0bfc41f443b0f00824b0aa8d12b switched from calling
    `rsvg_handle_new_from_file` to calling `rsvg_handle_new`, presumably due to
    a need to support librsvg <2.14 at the time. While this change in itself was
    fine, it also introduces an error message erroneously referring to the call
    being removed, `rsvg_handle_new_from_file`.
  • The error handling code path that used `err` when it was not null failed to
    deallocate this object, causing a memory leak. This, like many of the above
    problems, was also introduced in bcf867e081c8f0bfc41f443b0f00824b0aa8d12b.

plugin/rsvg/gvloadimage_rsvg.c
@@ -8,9 +8,11 @@
  * Contributors: Details at https://graphviz.org
  *************************************************************************/
 
+#include <assert.h>
+#include <common/render.h>
+#include <common/utils.h>
 #include <stdbool.h>
-#include <stdlib.h>
-#include <sys/stat.h>
+#include <stddef.h>
 
 #include <gvc/gvplugin_loadimage.h>
 
@@ -27,18 +29,13 @@ typedef enum {
 
 static void gvloadimage_rsvg_free(usershape_t *us)
 {
-    rsvg_handle_close(us->data, NULL);
+  g_object_unref(us->data);
 }
 
 static RsvgHandle* gvloadimage_rsvg_load(GVJ_t * job, usershape_t *us)
 {
     RsvgHandle* rsvgh = NULL;
-    unsigned char *fileBuf = NULL;
     GError *err = NULL;
-    size_t fileSize;
-
-    int fd;
-    struct stat stbuf;
 
     assert(job);
     assert(us);
@@ -58,46 +55,22 @@ static RsvgHandle* gvloadimage_rsvg_load(GVJ_t * job, usershape_t *us)
 	if (!gvusershape_file_access(us))
 	    return NULL;
         switch (us->type) {
-            case FT_SVG:
-      		rsvgh = rsvg_handle_new();
+            case FT_SVG: {
+      		const char *const safe_path = safefile(us->name);
+      		assert(safe_path != NULL &&
+      		       "gvusershape_file_access did not validate file path");
+      		rsvgh = rsvg_handle_new_from_file(safe_path, &err);
 		
 		if (rsvgh == NULL) {
 			fprintf(stderr, "rsvg_handle_new_from_file returned an error: %s\n", err->message);
+			g_error_free(err);
 			return NULL;
 		} 
 
-	 	fd = fileno(us->f);
-		fstat(fd, &stbuf);
-  		fileSize = (size_t)stbuf.st_size;	
-
-		fileBuf = calloc(fileSize + 1, sizeof(unsigned char));
-
-		if (fileBuf == NULL) {
-			g_object_unref(rsvgh);
-			return NULL;
-		}
-	
-		rewind(us->f);
-
-		if (fread(fileBuf, 1, fileSize, us->f) < fileSize) {
-			free(fileBuf);
-			g_object_unref(rsvgh);
-			return NULL;
-		}
-
-		if (!rsvg_handle_write(rsvgh, fileBuf, (gsize)fileSize, &err)) {
-			fprintf(stderr, "rsvg_handle_write returned an error: %s\n", err->message);
-			free(fileBuf);
-			g_object_unref(rsvgh);
-			return NULL;
-		} 
-
-		free(fileBuf);
-
-		rsvg_handle_close(rsvgh, &err);
 		rsvg_handle_set_dpi(rsvgh, POINTS_PER_INCH);
 
                 break;
+            }
             default:
                 rsvgh = NULL;
         }