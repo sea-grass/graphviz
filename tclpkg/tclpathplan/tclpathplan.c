sea-grass/graphviz

commit 38d0aa9d44a0863590821cdd9102dbf50c27a94b

tclpathplan: replace tclhandle allocator with generic list
The tclhandle allocator is a general purpose allocator that handed out pointers
for use. By replacing it with the generic list we get a number of benefits:
  • Type safety. The tclhandle allocator deals in `void *`s whereas this use of
    the generic list deals exclusively in `vgpane_t *`s.
  • Reduced heap fragmentation. Instead of allocating pointers and then
    separately allocating `vgpane_t` objects, a single array of `vgpane_t`
    objects is now allocated/resized.
  • Removing alignment concerns/complications. tclhandle performs some
    non-trivial calculations to try to comply with platform alignment
    constraints. As d0963b7a27ac7a13815af78c4233414db0872a2e noted, despite
    being elaborate, these are probably still not fully correct. When using the
    generic list interface, the compiler ensures all of this for us.
  • Fixing Undefined Behavior. `tclhandleXlateIndex` and `tclhandleFreeIndex`
    construct `entryHeader_pt`s and only _then_ check whether the index being
    looked up is within bounds. While it is relatively unlikely a compiler would
    exploit this, technically it is Undefined Behavior to even construct an
    invalid pointer like this.
  • Fixing unchecked allocations. `tclhandleExpandTable` calls `malloc` and
    assumes success. The generic list checks any allocation it makes.
  • Improved performance. tclhandle uses a pointer-chasing linked-list-style
    data structure, whereas the generic list lays out its backing data as a
    contiguous array. The latter is more cache- and branch-predictor-friendly on
    contemporary platforms.

The switch from `uint64_t` to `size_t` for indexing here is inessential. But it
more naturally aligns with platform properties and expectations, leading to a
more readable/intuitive implementation.

tclpkg/tclpathplan/tclpathplan.c
@@ -34,8 +34,8 @@
 #include <pathplan/tri.h>
 #include "Plegal_arrangement.h"
 #include <tcl.h>
-#include "tclhandle.h"
 #include <util/alloc.h>
+#include <util/prisize_t.h>
 
 #if ((TCL_MAJOR_VERSION == 8) && (TCL_MINOR_VERSION >= 6)) || ( TCL_MAJOR_VERSION > 8)
 #else
@@ -53,15 +53,21 @@ typedef struct poly_s {
 
 DEFINE_LIST(polys, poly)
 
+/// printf format for TCL handles
+#define HANDLE_FORMAT ("vgpane%" PRISIZE_T)
+
 typedef struct {
     polys_t poly; // set of polygons
     vconfig_t *vc;		/* visibility graph handle */
     Tcl_Interp *interp;		/* interpreter that owns the binding */
     char *triangle_cmd;		/* why is this here any more */
-    uint64_t index; ///< position within allocated handles list
+    size_t index; ///< position within allocated handles list
+    bool valid; ///< is this pane currently allocated?
 } vgpane_t;
 
-static tblHeader_pt vgpaneTable;
+DEFINE_LIST(vgpanes, vgpane_t)
+
+static vgpanes_t vgpaneTable;
 
 static int polyid = 0;		/* unique and unchanging id for each poly */
 
@@ -119,7 +125,7 @@ static void dgsprintxy(agxbuf *result, int npts, const point p[]) {
 /// @param npts Number of coordinates
 /// @param ppos Coordinates to substitute for %t
 static void expandPercentsEval(Tcl_Interp *interp, char *before,
-                               uint64_t vgcanvasHandle, int npts,
+                               size_t vgcanvasHandle, int npts,
                                const point *ppos) {
     agxbuf scripts = {0};
 
@@ -167,7 +173,7 @@ static void triangle_callback(void *vgparg, const point pqr[]) {
     vgp = vgparg;
 
     if (vgp->triangle_cmd) {
-	const uint64_t vgcanvasHandle = vgp->index;
+	const size_t vgcanvasHandle = vgp->index;
 	expandPercentsEval(vgp->interp, vgp->triangle_cmd, vgcanvasHandle, 3, pqr);
     }
 }
@@ -354,6 +360,58 @@ static void appendpoint(Tcl_Interp * interp, point p)
     Tcl_AppendElement(interp, buf);
 }
 
+/// lookup an existing pane
+///
+/// @param panes Collection to search
+/// @param handle Text descriptor of the pane
+/// @return A pointer to the pane or `NULL` if there is no such pane
+static vgpane_t *find_vgpane(vgpanes_t *panes, const char *handle) {
+  assert(panes != NULL);
+  assert(handle != NULL);
+
+  size_t index;
+  if (sscanf(handle, HANDLE_FORMAT, &index) != 1) {
+    return NULL;
+  }
+  if (index >= vgpanes_size(panes)) {
+    return NULL;
+  }
+  vgpane_t *const pane = vgpanes_at(panes, index);
+  if (!pane->valid) {
+    return NULL;
+  }
+
+  return pane;
+}
+
+/// cleanup unused panes where possible
+///
+/// Panes need to have a stable identifier (index) for their lifetime. Thus they
+/// cannot be removed from a `vgpanes_t` when deallocated because removing them
+/// would alter the index of any still-live panes that comes after them.
+/// Instead, panes are marked `->valid = false` when deallocated and then later
+/// removed here.
+///
+/// It is only safe to remove deallocated panes that have no live panes
+/// following them, which is exactly what this function does.
+///
+/// @param panes Collection to sweep
+static void garbage_collect_vgpanes(vgpanes_t *panes) {
+  assert(panes != NULL);
+
+  // shrink list, discarding previously deallocated entries
+  while (!vgpanes_is_empty(panes)) {
+    if (!vgpanes_back(panes)->valid) {
+      (void)vgpanes_pop_back(panes);
+    }
+  }
+
+  // if this left us with none, fully deallocate to make leak checkers happy
+  if (vgpanes_is_empty(panes)) {
+    vgpanes_free(panes);
+  }
+}
+
 /* process vgpane methods */
 static int
 vgpanecmd(ClientData clientData, Tcl_Interp * interp, int argc,
@@ -364,7 +422,7 @@ vgpanecmd(ClientData clientData, Tcl_Interp * interp, int argc,
     int vargc, result;
     char vbuf[30];
     const char **vargv;
-    vgpane_t *vgp, **vgpp;
+    vgpane_t *vgp;
     point p, q, *ps;
     double alpha, gain;
     Pvector_t slopes[2];
@@ -376,11 +434,10 @@ vgpanecmd(ClientData clientData, Tcl_Interp * interp, int argc,
 			 " ", argv[0], " method ?arg arg ...?\"", NULL);
 	return TCL_ERROR;
     }
-    if (!(vgpp = (vgpane_t **) tclhandleXlate(vgpaneTable, argv[0]))) {
+    if (!(vgp = find_vgpane(&vgpaneTable, argv[0]))) {
 	Tcl_AppendResult(interp, "Invalid handle: \"", argv[0], "\"", NULL);
 	return TCL_ERROR;
     }
-    vgp = *vgpp;
 
     if (strcmp(argv[1], "coords") == 0) {
 	if (argc < 3) {
@@ -446,7 +503,8 @@ vgpanecmd(ClientData clientData, Tcl_Interp * interp, int argc,
 	    Pobsclose(vgp->vc);
 	polys_free(&vgp->poly);
 	Tcl_DeleteCommand(interp, argv[0]);
-	free(tclhandleFree(vgpaneTable, argv[0]));
+	vgp->valid = false;
+	garbage_collect_vgpanes(&vgpaneTable);
 	return TCL_OK;
 
     } else if (strcmp(argv[1], "find") == 0) {
@@ -807,21 +865,18 @@ vgpane(ClientData clientData, Tcl_Interp * interp, int argc, const char *argv[])
     (void)argc;
     (void)argv;
 
-    char *vbuf = NULL;
-    vgpane_t *vgp = gv_alloc(sizeof(vgpane_t));
-    uint64_t index;
-    *(vgpane_t **)tclhandleAlloc(vgpaneTable, &vbuf, &index) = vgp;
-    assert(vbuf != NULL);
+    const vgpane_t vg = {.interp = interp};
+    vgpanes_append(&vgpaneTable, vg);
+    vgpane_t *const vgp = vgpanes_back(&vgpaneTable);
+    vgp->index = vgpanes_size(&vgpaneTable) - 1;
 
-    vgp->vc = NULL;
-    vgp->poly = (polys_t){0};
-    vgp->interp = interp;
-    vgp->triangle_cmd = NULL;
-    vgp->index = index;
+    agxbuf buffer = {0};
+    agxbprint(&buffer, HANDLE_FORMAT, vgp->index);
+    const char *const vbuf = agxbuse(&buffer);
 
     Tcl_CreateCommand(interp, vbuf, vgpanecmd, NULL, NULL);
     Tcl_AppendResult(interp, vbuf, NULL);
-    free(vbuf);
+    agxbfree(&buffer);
     return TCL_OK;
 }
 
@@ -851,8 +906,6 @@ int Tclpathplan_Init(Tcl_Interp * interp)
     }
 
     Tcl_CreateCommand(interp, "vgpane", vgpane, NULL, NULL);
-
-    vgpaneTable = tclhandleInit(sizeof(vgpane_t), 10);
 
     return TCL_OK;
 }

configure.ac
@@ -2179,7 +2179,6 @@ AC_CONFIG_FILES(Makefile
 	cmd/gvedit/images/Makefile
 	tclpkg/Makefile
 	tclpkg/tclstubs/Makefile
-	tclpkg/tclhandle/Makefile
 	tclpkg/gdtclft/Makefile
 	tclpkg/gdtclft/demo/Makefile
 	tclpkg/tcldot/Makefile

tclpkg/CMakeLists.txt
@@ -12,7 +12,6 @@ endif()
 if(ENABLE_TCL)
   add_subdirectory(gdtclft)
   add_subdirectory(tcldot)
-  add_subdirectory(tclhandle)
   add_subdirectory(tclpathplan)
   add_subdirectory(tclstubs)
 endif()

tclpkg/Makefile.am
@@ -18,7 +18,7 @@ if WITH_TCL
 pkgindex_DATA = pkgIndex.tcl
 endif
 
-SUBDIRS = tclstubs tclhandle gdtclft tcldot tclpathplan gv
+SUBDIRS = tclstubs gdtclft tcldot tclpathplan gv
 
 pkgIndex.tcl: $(TCL_PKGINDEX) $(TCL_PKGINDEX_GD) $(TCL_PKGINDEX_SWIG)
 	cat $(TCL_PKGINDEX) $(TCL_PKGINDEX_GD) $(TCL_PKGINDEX_SWIG) >$@

tclpkg/tclhandle/CMakeLists.txt
@@ -1,14 +1,0 @@
-add_library(tclhandle OBJECT
-  tclhandle.c
-  tclhandle.h
-)
-
-target_include_directories(tclhandle
-  PRIVATE
-    ../../lib
-  PUBLIC
-    ${CMAKE_CURRENT_SOURCE_DIR}
-)
-
-target_include_directories(tclhandle SYSTEM PUBLIC ${TCL_INCLUDE_PATH})
-target_link_libraries(tclhandle PUBLIC ${TCL_LIBRARY})

tclpkg/tclhandle/Makefile.am
@@ -1,11 +1,0 @@
-## Process this file with automake to produce Makefile.in
-
-AM_CPPFLAGS = -I$(top_srcdir)/lib ${TCL_INCLUDES}
-
-noinst_HEADERS = tclhandle.h
-if WITH_TCL
-noinst_LTLIBRARIES = libtclhandle_C.la
-endif
-
-libtclhandle_C_la_SOURCES = tclhandle.c
-libtclhandle_C_la_LDFLAGS = -module -avoid-version

tclpkg/tclhandle/tclhandle.c
@@ -1,293 +1,0 @@
-/*************************************************************************
- * Copyright (c) 2011 AT&T Intellectual Property
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * https://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors: Details at https://graphviz.org
- *************************************************************************/
-
-/*
- * tclhandle.c --
- *
- * adapted from tclXhandles.c tclXutil.c and tclExtend.h
- */
-
-/*
- *
- * tclXhandles.c --
- *
- * Tcl handles.  Provides a mechanism for managing expandable tables that are
- * addressed by textual handles.
- *-----------------------------------------------------------------------------
- * Copyright 1991-1994 Karl Lehenbauer and Mark Diekhans.
- *
- * Permission to use, copy, modify, and distribute this software and its
- * documentation for any purpose and without fee is hereby granted, provided
- * that the above copyright notice appear in all copies.  Karl Lehenbauer and
- * Mark Diekhans make no representations about the suitability of this
- * software for any purpose.  It is provided "as is" without express or
- * implied warranty.
- *-----------------------------------------------------------------------------
- * $Id$
- *-----------------------------------------------------------------------------
- */
-
-#include "tclhandle.h"
-#include <cgraph/agxbuf.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-/*
- * Alignment factor (in bytes) for this machine.
- */
-enum {
-  tclhandleEntryAlignment =
-      sizeof(void *) > sizeof(uint64_t)
-          ? (sizeof(double) > sizeof(void *) ? sizeof(double) : sizeof(void *))
-          : (sizeof(double) > sizeof(uint64_t) ? sizeof(double)
-                                               : sizeof(uint64_t))
-};
-
-/*=============================================================================
- * tclhandleLinkInNewEntries --
- *   Build free links through the newly allocated part of a table.
- *
- * Parameters:
- *   o tblHdrPtr (I) - A pointer to the table header.
- *   o newIdx (I) - Index of the first new entry.
- *   o numEntries (I) - The number of new entries.
- *-----------------------------------------------------------------------------
- */
-static void tclhandleLinkInNewEntries(tblHeader_pt tblHdrPtr, uint64_t newIdx,
-                                      uint64_t numEntries) {
-  uint64_t entIdx, lastIdx;
-  entryHeader_pt entryPtr;
-
-  lastIdx = newIdx + numEntries - 1;
-
-  for (entIdx = newIdx; entIdx < lastIdx; entIdx++) {
-    entryPtr = TBL_INDEX(tblHdrPtr, entIdx);
-    entryPtr->freeLink = entIdx + 1;
-  }
-  entryPtr = TBL_INDEX(tblHdrPtr, lastIdx);
-  entryPtr->freeLink = tblHdrPtr->freeHeadIdx;
-  tblHdrPtr->freeHeadIdx = newIdx;
-}
-
-/*=============================================================================
- * tclhandleExpandTable --
- *   Expand a handle table, doubling its size.
- * Parameters:
- *   o tblHdrPtr (I) - A pointer to the table header.
- *   o neededIdx (I) - If positive, then the table will be expanded so that
- *     this entry is available.  If -1, then just expand by the number of
- *     entries specified on table creation.  MUST be smaller than this size.
- *-----------------------------------------------------------------------------
- */
-static void tclhandleExpandTable(tblHeader_pt tblHdrPtr, uint64_t neededIdx) {
-  ubyte_pt oldbodyPtr = tblHdrPtr->bodyPtr;
-  uint64_t numNewEntries;
-  uint64_t newSize;
-
-  if (neededIdx == NULL_IDX || neededIdx == ALLOCATED_IDX)
-    numNewEntries = tblHdrPtr->tableSize;
-  else
-    numNewEntries = neededIdx - tblHdrPtr->tableSize + 1;
-  newSize = (tblHdrPtr->tableSize + numNewEntries) * tblHdrPtr->entrySize;
-
-  tblHdrPtr->bodyPtr = malloc(newSize);
-  memcpy(tblHdrPtr->bodyPtr, oldbodyPtr,
-         tblHdrPtr->tableSize * tblHdrPtr->entrySize);
-  tclhandleLinkInNewEntries(tblHdrPtr, tblHdrPtr->tableSize, numNewEntries);
-  tblHdrPtr->tableSize += numNewEntries;
-  free(oldbodyPtr);
-}
-
-/*=============================================================================
- * tclhandleAlloc --
- *   Allocate a table entry, expanding if necessary.
- *
- * Parameters:
- *   o headerPtr (I) - A pointer to the table header.
- *   o handle (O) - If not NULL, the handle is returned here.
- *   o entryIdxPtr (O) - If not NULL, the index is returned here.
- * Returns:
- *    The a pointer to the entry.
- *-----------------------------------------------------------------------------
- */
-entryHeader_pt tclhandleAlloc(tblHeader_pt headerPtr, char **handle,
-                              uint64_t *entryIdxPtr) {
-  tblHeader_pt tblHdrPtr = headerPtr;
-  entryHeader_pt entryPtr;
-  uint64_t entryIdx;
-
-  if (tblHdrPtr->freeHeadIdx == NULL_IDX)
-    tclhandleExpandTable(tblHdrPtr, NULL_IDX);
-
-  entryIdx = tblHdrPtr->freeHeadIdx;
-  entryPtr = TBL_INDEX(tblHdrPtr, entryIdx);
-  tblHdrPtr->freeHeadIdx = entryPtr->freeLink;
-  entryPtr->freeLink = ALLOCATED_IDX;
-
-  if (handle) {
-    agxbuf buf = {0};
-    agxbprint(&buf, HANDLE_FORMAT, entryIdx);
-    *handle = agxbdisown(&buf);
-  }
-  if (entryIdxPtr)
-    *entryIdxPtr = entryIdx;
-  return USER_AREA(entryPtr);
-}
-
-/*=============================================================================
- * tclhandleInit --
- *   Create and initialize a Tcl dynamic handle table.
- * Parameters:
- *   o entrySize (I) - The size of an entry, in bytes.
- *   o initEntries (I) - Initial size of the table, in entries.
- * Returns:
- *   A pointer to the table header.
- *-----------------------------------------------------------------------------
- */
-tblHeader_pt tclhandleInit(uint64_t entrySize, uint64_t initEntries) {
-  tblHeader_pt tblHdrPtr;
-
-  /*
-   * Set up the table entry.
-   */
-  tblHdrPtr = malloc(sizeof(tblHeader_t));
-
-  /*
-   * Calculate entry size, including header, rounded up to sizeof (void *).
-   */
-  tblHdrPtr->entrySize = ENTRY_HEADER_SIZE + ROUND_ENTRY_SIZE(entrySize);
-  tblHdrPtr->freeHeadIdx = NULL_IDX;
-  tblHdrPtr->tableSize = initEntries;
-  tblHdrPtr->bodyPtr = malloc(initEntries * tblHdrPtr->entrySize);
-  tclhandleLinkInNewEntries(tblHdrPtr, 0, initEntries);
-
-  return tblHdrPtr;
-}
-
-/*=============================================================================
- * tclhandleIndex --
- *   Translate a string handle to a handleTable Index.
- *
- * Parameters:
- *   o headerPtr (I) - A pointer to the table header.
- *   o handle (I) - The handle string -- <prefix><int>
- *   o entryIdxPtr (O) - If not NULL, the index is returned here.
- * Returns:
- *   TCL_OK if the handle was valid.
- *   TCL_ERROR if the handle was invalid.
- *-----------------------------------------------------------------------------
- */
-int tclhandleIndex(tblHeader_pt tblHdrPtr, const char *handle,
-                   uint64_t *entryIdxPtr) {
-  uint64_t entryIdx;
-
-  if (sscanf(handle, HANDLE_FORMAT, &entryIdx) != 1)
-    return TCL_ERROR;
-  if (entryIdxPtr)
-    *entryIdxPtr = entryIdx;
-  return TCL_OK;
-}
-
-/*=============================================================================
- * tclhandleXlateIndex --
- *   Translate an index to an entry pointer.
- *
- * Parameters:
- *   o headerPtr (I) - A pointer to the table header.
- *   o index (I) - The index of an entry.
- * Returns:
- *   A pointer to the entry, or NULL if an error occurred.
- *-----------------------------------------------------------------------------
- */
-void *tclhandleXlateIndex(tblHeader_pt headerPtr, uint64_t entryIdx) {
-  tblHeader_pt tblHdrPtr = headerPtr;
-  entryHeader_pt entryPtr;
-
-  entryPtr = TBL_INDEX(tblHdrPtr, entryIdx);
-
-  if (entryIdx >= tblHdrPtr->tableSize || entryPtr->freeLink != ALLOCATED_IDX) {
-    return NULL;
-  }
-
-  return USER_AREA(entryPtr);
-}
-
-/*=============================================================================
- * tclhandleXlate --
- *   Translate a string handle to an entry pointer.
- *
- * Parameters:
- *   o headerPtr (I) - A pointer to the table header.
- *   o handle (I) - The handle string -- <prefix><int>
- * Returns:
- *   A pointer to the entry, or NULL if an error occurred.
- *-----------------------------------------------------------------------------
- */
-void *tclhandleXlate(tblHeader_pt tblHdrPtr, const char *handle) {
-  uint64_t entryIdx;
-
-  if (tclhandleIndex(tblHdrPtr, handle, &entryIdx) != TCL_OK)
-    return NULL;
-  return tclhandleXlateIndex(tblHdrPtr, entryIdx);
-}
-
-/*============================================================================
- * tclhandleFreeIndex --
- *   Frees a handle table entry by index.
- *
- * Parameters:
- *   o headerPtr (I) - A pointer to the table header.
- *   o index (I) - The index of an entry.
- *----------------------------------------------------------------------------
- * Returns:
- *   The contents of the entry, if success, or NULL if an error occurred.
- *----------------------------------------------------------------------------
- */
-void *tclhandleFreeIndex(tblHeader_pt headerPtr, uint64_t entryIdx) {
-  tblHeader_pt tblHdrPtr = headerPtr;
-  entryHeader_pt entryPtr, freeentryPtr;
-
-  entryPtr = TBL_INDEX(tblHdrPtr, entryIdx);
-
-  if (entryIdx >= tblHdrPtr->tableSize || entryPtr->freeLink != ALLOCATED_IDX) {
-    return NULL;
-  }
-
-  entryPtr = USER_AREA(entryPtr);
-  freeentryPtr = HEADER_AREA(entryPtr);
-  freeentryPtr->freeLink = tblHdrPtr->freeHeadIdx;
-  tblHdrPtr->freeHeadIdx =
-      ((uint64_t)((uintptr_t)entryPtr - (uintptr_t)tblHdrPtr->bodyPtr)) /
-      tblHdrPtr->entrySize;
-
-  return entryPtr;
-}
-
-/*============================================================================
- * tclhandleFree --
- *   Frees a handle table entry by handle.
- *
- * Parameters:
- *   o headerPtr (I) - A pointer to the table header.
- *   o handle (I) - The handle of an entry.
- *----------------------------------------------------------------------------
- * Returns:
- *   The contents of the entry, if success, or NULL if an error occurred.
- *----------------------------------------------------------------------------
- */
-void *tclhandleFree(tblHeader_pt tblHdrPtr, const char *handle) {
-  uint64_t entryIdx;
-
-  if (tclhandleIndex(tblHdrPtr, handle, &entryIdx) != TCL_OK)
-    return NULL;
-  return tclhandleFreeIndex(tblHdrPtr, entryIdx);
-}

tclpkg/tclhandle/tclhandle.h
@@ -1,87 +1,0 @@
-/*************************************************************************
- * Copyright (c) 2011 AT&T Intellectual Property
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * https://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors: Details at https://graphviz.org
- *************************************************************************/
-
-#include <inttypes.h>
-#include <stdint.h>
-#include <tcl.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-/*
- * Macro to rounded up a size to be a multiple of (void *).  This is required
- * for systems that have alignment restrictions on pointers and data.
- */
-#define ROUND_ENTRY_SIZE(size)                                                 \
-  ((((size) + tclhandleEntryAlignment - 1) / tclhandleEntryAlignment) *        \
-   tclhandleEntryAlignment)
-
-#define NULL_IDX UINT64_MAX
-#define ALLOCATED_IDX (UINT64_MAX - 1)
-
-typedef unsigned char ubyte_t;
-typedef ubyte_t *ubyte_pt;
-
-/*
- * This is the table header.  It is separately allocated from the table body,
- * since it must keep track of a table body that might move.  Each entry in the
- * table is preceded with a header which has the free list link, which is a
- * entry index of the next free entry.  Special values keep track of allocated
- * table is preceded with a header which has the free list link, which is a
- * entry index of the next free entry.  Special values keep track of allocated
- * entries.
- */
-
-typedef struct {
-  uint64_t entrySize;   /* Entry size in bytes, including overhead */
-  uint64_t tableSize;   /* Current number of entries in the table  */
-  uint64_t freeHeadIdx; /* Index of first free entry in the table  */
-  ubyte_pt bodyPtr;     /* Pointer to table body                   */
-} tblHeader_t;
-typedef tblHeader_t *tblHeader_pt;
-
-typedef struct {
-  uint64_t freeLink;
-} entryHeader_t;
-typedef entryHeader_t *entryHeader_pt;
-
-#define ENTRY_HEADER_SIZE (ROUND_ENTRY_SIZE(sizeof(entryHeader_t)))
-
-/*
- * This macro is used to return a pointer to an entry, given its index.
- */
-#define TBL_INDEX(hdrPtr, idx)                                                 \
-  ((entryHeader_pt)(hdrPtr->bodyPtr + (hdrPtr->entrySize * idx)))
-
-/*
- * This macros to convert between pointers to the user and header area of
- * an table entry.
- */
-#define USER_AREA(entryPtr) (void *)(((ubyte_pt)entryPtr) + ENTRY_HEADER_SIZE);
-#define HEADER_AREA(entryPtr)                                                  \
-  (entryHeader_pt)(((ubyte_pt)entryPtr) - ENTRY_HEADER_SIZE);
-
-void *tclhandleFreeIndex(tblHeader_pt headerPtr, uint64_t entryIdx);
-void *tclhandleFree(tblHeader_pt headerPtr, const char *handle);
-tblHeader_pt tclhandleInit(uint64_t entrySize, uint64_t initEntries);
-void *tclhandleXlateIndex(tblHeader_pt headerPtr, uint64_t entryIdx);
-void *tclhandleXlate(tblHeader_pt headerPtr, const char *handle);
-entryHeader_pt tclhandleAlloc(tblHeader_pt tblHdrPtr, char **handle,
-                              uint64_t *entryIdxPtr);
-int tclhandleIndex(tblHeader_pt tblHdrPtr, const char *handle,
-                   uint64_t *entryIdxPtr);
-
-/// printf format for TCL handles
-#define HANDLE_FORMAT ("vgpane%" PRIu64)
-
-#ifdef __cplusplus
-}
-#endif

commit 8c840ec249adb0feada6559e09520177abd2ab4b

Tclpathplan: fix: allow created vgpanes to be found
38d0aa9d44a0863590821cdd9102dbf50c27a94b introduced a bug wherein created
vgpanes were considered invalid. This meant any attempt to run a command on them
would fail, claiming the passed handle was invalid. Because this also prevented
deleting created handles, this also caused unavoidable memory leaks.

tclpkg/tclpathplan/tclpathplan.c
@@ -866,6 +866,7 @@ vgpane(ClientData clientData, Tcl_Interp * interp, int argc, const char *argv[])
     vgpanes_append(&vgpaneTable, vg);
     vgpane_t *const vgp = vgpanes_back(&vgpaneTable);
     vgp->index = vgpanes_size(&vgpaneTable) - 1;
+    vgp->valid = true;
 
     agxbuf buffer = {0};
     agxbprint(&buffer, HANDLE_FORMAT, vgp->index);

tests/test_regression.py
@@ -4403,7 +4403,6 @@ def test_2591():
     reason="pexpect.spawn is not available on Windows "
     "(https://pexpect.readthedocs.io/en/stable/overview.html#pexpect-on-windows)",
 )
-@pytest.mark.xfail(strict=True)
 def test_vgpane_delete():
     """
     it should be possible to delete an existing `vgpane`